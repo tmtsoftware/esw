ce025a2a9c63cb3ee82e57e10d847f37bfc4753b
Pritam Kadam <kpritam@thoughtworks.com>
2019-06-19 12:23:12 +0530

    #ESW-90: Pritam|Bilal: Add tests for FunctionBuilder
    Co-authored-by: Bilal Fazlani <bilal.m.fazlani@gmail.com>

---
+++ ocs/ocs-framework-tests/src/main/scala/esw/ocs/framework/dsl/FunctionBuilderTest.scala
@@ -0,0 +1,57 @@
+package esw.ocs.framework.dsl
+
+import esw.ocs.framework.BaseTestSuite
+import esw.ocs.framework.dsl.internal.FunctionBuilder
+
+import scala.util.{Failure, Success}
+
+class FunctionBuilderTest extends BaseTestSuite {
+
+  "build" when {
+    "handlers are empty" should {
+      "execute default case and return Success" in {
+        val functionBuilder = new FunctionBuilder[Int, Int]
+        functionBuilder.build(default = _ + 10)(10) shouldBe Success(20)
+      }
+    }
+    "handlers are added" when {
+      case class Command(name: String, value: Int)
+      val functionBuilder = new FunctionBuilder[Command, Int]
+
+      functionBuilder.addHandler[Command](cmd ⇒ cmd.value * cmd.value)(_.name == "square")
+      functionBuilder.addHandler[Command](cmd ⇒ Math.abs(cmd.value))(_.name == "abs")
+      functionBuilder.addHandler[Command](cmd ⇒ 1 / cmd.value)(_.name == "reciprocal")
+      val builtFunction = functionBuilder.build {
+        case Command("die", _) ⇒ throw new RuntimeException("dead")
+        case _                 ⇒ 0
+      }
+
+      "found matching handler" when {
+        "there is no exception" should {
+          "execute matched handler and return Success" in {
+            builtFunction(Command("square", 10)) shouldBe Success(100)
+            builtFunction(Command("abs", 25)) shouldBe Success(25)
+          }
+        }
+        "there is an exception" should {
+          "execute matched handler and return Failure" in {
+            builtFunction(Command("reciprocal", 0)) shouldBe a[Failure[ArithmeticException]]
+          }
+        }
+      }
+      "not found matching handler" when {
+        "there is no exception" should {
+          "execute default handler and return Success" in {
+            builtFunction(Command("absent", 25)) shouldBe Success(0)
+
+          }
+        }
+        "there is an exception" should {
+          "execute default handler and return Failure" in {
+            builtFunction(Command("die", 10)) shouldBe a[Failure[RuntimeException]]
+          }
+        }
+      }
+    }
+  }
+}
--- ocs/ocs-framework/src/main/scala/esw/ocs/framework/dsl/internal/FunctionBuilder.scala
+++ ocs/ocs-framework/src/main/scala/esw/ocs/framework/dsl/internal/FunctionBuilder.scala
@@ -2,16 +2,17 @@ package esw.ocs.framework.dsl.internal
 
 import scala.collection.mutable
 import scala.reflect.ClassTag
+import scala.util.Try
 
 private[framework] class FunctionBuilder[I, O] {
 
   private val handlers: mutable.Buffer[PartialFunction[I, O]] = mutable.Buffer.empty
 
-  def combinedHandler: PartialFunction[I, O] = handlers.foldLeft(PartialFunction.empty[I, O])(_ orElse _)
+  private def combinedHandler: PartialFunction[I, O] = handlers.foldLeft(PartialFunction.empty[I, O])(_ orElse _)
 
   def addHandler[T <: I: ClassTag](handler: T => O)(iff: T => Boolean): Unit = handlers += {
     case input: T if iff(input) => handler(input)
   }
 
-  def build(default: I => O): I => O = input => combinedHandler.lift(input).getOrElse(default(input))
+  def build(default: I => O): I => Try[O] = input => Try(combinedHandler.lift(input).getOrElse(default(input)))
 }
