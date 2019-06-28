78e7eab809946b32186e79b4f886f7c9fa1fc10b
Pritam Kadam <kpritam@thoughtworks.com>
2019-06-28 12:04:55 +0530

    #ESW-90: Pritam: Exercise Sequence.add(other: Sequence) in test

--- ocs/ocs-framework-api/src/test/scala/esw/ocs/framework/api/models/SequenceTest.scala
+++ ocs/ocs-framework-api/src/test/scala/esw/ocs/framework/api/models/SequenceTest.scala
@@ -23,16 +23,6 @@ class SequenceTest extends BaseTestSuite {
   }
 
   "add" must {
-    "allow adding command to existing sequence" in {
-      val setup    = Setup(Prefix("test1"), CommandName("setup-test"), None)
-      val observe  = Observe(Prefix("test2"), CommandName("observe-test"), None)
-      val sequence = Sequence(setup, observe)
-
-      val newSetup       = Setup(Prefix("test3"), CommandName("setup-test"), None)
-      val updateSequence = sequence.add(newSetup)
-      updateSequence.commands shouldBe List(setup, observe, newSetup)
-    }
-
     "allow adding list of commands to existing sequence" in {
       val setup    = Setup(Prefix("test1"), CommandName("setup-test"), None)
       val observe  = Observe(Prefix("test2"), CommandName("observe-test"), None)
@@ -44,5 +34,19 @@ class SequenceTest extends BaseTestSuite {
       val updateSequence = sequence.add(newSetup, newObserve)
       updateSequence.commands shouldBe List(setup, observe, newSetup, newObserve)
     }
+
+    "allow adding new sequence to existing sequence" in {
+      val setup    = Setup(Prefix("test1"), CommandName("setup-test"), None)
+      val observe  = Observe(Prefix("test2"), CommandName("observe-test"), None)
+      val sequence = Sequence(setup, observe)
+
+      val newSetup    = Setup(Prefix("test3"), CommandName("setup-test"), None)
+      val newObserve  = Observe(Prefix("test4"), CommandName("observe-test"), None)
+      val newSequence = Sequence(newSetup, newObserve)
+
+      val updateSequence = sequence.add(newSequence)
+      updateSequence.commands shouldBe List(setup, observe, newSetup, newObserve)
+    }
+
   }
 }
