1270b47144bc416caa9f179e4bd7fb2468d8ee37
Poorva Gokhale <poorvasgokhale@gmail.com>
2019-07-11 23:15:14 +0530

    #ESW-103 : Shubham | Poorva : Add SequencerApp. Refine gateway integration test

--- esw-integration-test/src/test/scala/esw/integration/test/gatway/server/MainTest.scala
+++ esw-integration-test/src/test/scala/esw/integration/test/gatway/server/MainTest.scala
@@ -6,15 +6,18 @@ import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.adapter.TypedActorSystemOps
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.Uri.{Path, Query}
-import akka.http.scaladsl.model.{HttpRequest, StatusCodes, Uri}
+import akka.http.scaladsl.model.Uri.Path
+import akka.http.scaladsl.model._
 import akka.stream.typed.scaladsl.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 import csw.location.api.models.Connection.HttpConnection
 import csw.location.api.models.{ComponentId, ComponentType}
 import csw.location.api.scaladsl.LocationService
 import csw.location.client.scaladsl.HttpLocationServiceFactory
-import csw.testkit.LocationTestKit
+import csw.params.core.formats.JsonSupport
+import csw.params.core.models.Prefix
+import csw.params.events.{EventName, SystemEvent}
+import csw.testkit.{EventTestKit, LocationTestKit}
 import esw.gateway.server.Main
 import esw.template.http.server.BaseTestSuite
 import esw.template.http.server.TestFutureExtensions.RichFuture
@@ -22,7 +25,11 @@ import esw.template.http.server.TestFutureExtensions.RichFuture
 import scala.concurrent.duration.DurationInt
 
 class MainTest extends BaseTestSuite {
-  private val testKit = LocationTestKit()
+
+  import JsonSupport._
+
+  private val locationTestKit = LocationTestKit()
+  private val eventTestKit    = EventTestKit()
 
   implicit val system: ActorSystem[_]                = ActorSystem(Behaviors.empty, "test")
   implicit val untypedActorSystem: actor.ActorSystem = system.toUntyped
@@ -30,17 +37,19 @@ class MainTest extends BaseTestSuite {
   private val testLocationService: LocationService   = HttpLocationServiceFactory.makeLocalClient
 
   override def beforeAll(): Unit = {
-    testKit.startLocationServer()
+    locationTestKit.startLocationServer()
+    eventTestKit.startEventService()
   }
 
   override def afterAll(): Unit = {
     Http().shutdownAllConnectionPools().await
-    testKit.shutdownLocationServer()
+    locationTestKit.shutdownLocationServer()
+    eventTestKit.shutdown()
     system.terminate()
     system.whenTerminated.await
   }
 
-  "should start Gateway server and register with location service | ESW-92" in {
+  "should start Gateway server and register with location service and publish event | ESW-92" in {
     val httpService = Main.start(Array("--port", "9806"), startLogging = false).get
     val connection  = HttpConnection(ComponentId("GatewayServer", ComponentType.Service))
     val expectedConnection = HttpConnection(
@@ -51,11 +60,14 @@ class MainTest extends BaseTestSuite {
       val gatewayServiceLocation = testLocationService.resolve(connection, 5.seconds).await.get
 
       gatewayServiceLocation.connection shouldBe expectedConnection
-      val uri = Uri(gatewayServiceLocation.uri.toString).withPath(Path / "event").withQuery(Query(("key", "tcs.test.gateway")))
+      val uri       = Uri(gatewayServiceLocation.uri.toString).withPath(Path / "event")
+      val event     = SystemEvent(Prefix("tcs.test.gateway"), EventName("event"))
+      val eventJson = HttpEntity(ContentTypes.`application/json`, writeEvent(event).toString())
 
-      val request  = HttpRequest(uri = uri)
+      val request  = HttpRequest(uri = uri).withMethod(HttpMethods.POST).withEntity(eventJson)
       val response = Http().singleRequest(request).await
-      response.status shouldBe StatusCodes.InternalServerError
+      response.status shouldBe StatusCodes.OK
+
     } finally {
       httpService.shutdown(UnknownReason).await
     }
