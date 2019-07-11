cac9eb5870592bedd69da885d43745ce4bec54f8
Shubham Chauhan <shubham25may1998@gmail.com>
2019-07-11 16:03:35 +0530

    #000 : Dolly | Chauhan : Add tests for SequencerBehavior and refactor
    Co-authored-by: Dolly <10329479+Dollyg@users.noreply.github.com>

--- esw-ocs/esw-ocs-framework/src/main/scala/esw/ocs/framework/core/SequencerBehavior.scala
+++ esw-ocs/esw-ocs-framework/src/main/scala/esw/ocs/framework/core/SequencerBehavior.scala
@@ -1,19 +1,14 @@
 package esw.ocs.framework.core
 
-import akka.Done
-import akka.actor.typed.ActorRef
 import akka.actor.typed.scaladsl.Behaviors
+import esw.ocs.framework.api.models.messages.SequencerMsg
 import esw.ocs.framework.api.models.messages.SequencerMsg._
-import esw.ocs.framework.api.models.messages.{SequencerMsg, StepListError}
 import esw.ocs.framework.dsl.ScriptDsl
 
 object SequencerBehavior {
   def behavior(sequencer: Sequencer, script: ScriptDsl): Behaviors.Receive[SequencerMsg] =
     Behaviors.receive[SequencerMsg] { (ctx, msg) =>
       import ctx.executionContext
-
-      def send[L <: StepListError, R](stepListResponse: Either[L, R], replyTo: ActorRef[Either[L, Done]]): Unit =
-        replyTo ! stepListResponse.map(_ ⇒ akka.Done)
 
       msg match {
         // ===== External Lifecycle =====
@@ -25,16 +20,16 @@ object SequencerBehavior {
         case Available(replyTo)                 ⇒ sequencer.isAvailable.foreach(replyTo.tell)
         case GetSequence(replyTo)               ⇒ sequencer.getSequence.foreach(replyTo.tell)
         case GetPreviousSequence(replyTo)       ⇒ sequencer.getPreviousSequence.foreach(replyTo.tell)
-        case Add(commands, replyTo)             ⇒ sequencer.add(commands).foreach(send(_, replyTo))
-        case Pause(replyTo)                     ⇒ sequencer.pause.foreach(send(_, replyTo))
-        case Resume(replyTo)                    ⇒ sequencer.resume.foreach(send(_, replyTo))
-        case Reset(replyTo)                     ⇒ sequencer.reset().foreach(send(_, replyTo))
-        case Replace(id, commands, replyTo)     ⇒ sequencer.replace(id, commands).foreach(send(_, replyTo))
-        case Prepend(commands, replyTo)         ⇒ sequencer.prepend(commands).foreach(send(_, replyTo))
-        case Delete(id, replyTo)                ⇒ sequencer.delete(id).foreach(send(_, replyTo))
-        case InsertAfter(id, commands, replyTo) ⇒ sequencer.insertAfter(id, commands).foreach(send(_, replyTo))
-        case AddBreakpoint(id, replyTo)         ⇒ sequencer.addBreakpoint(id).foreach(send(_, replyTo))
-        case RemoveBreakpoint(id, replyTo)      ⇒ sequencer.removeBreakpoint(id).foreach(send(_, replyTo))
+        case Add(commands, replyTo)             ⇒ sequencer.add(commands).foreach(replyTo.tell)
+        case Pause(replyTo)                     ⇒ sequencer.pause.foreach(replyTo.tell)
+        case Resume(replyTo)                    ⇒ sequencer.resume.foreach(replyTo.tell)
+        case Reset(replyTo)                     ⇒ sequencer.reset().foreach(replyTo.tell)
+        case Replace(id, commands, replyTo)     ⇒ sequencer.replace(id, commands).foreach(replyTo.tell)
+        case Prepend(commands, replyTo)         ⇒ sequencer.prepend(commands).foreach(replyTo.tell)
+        case Delete(id, replyTo)                ⇒ sequencer.delete(id).foreach(replyTo.tell)
+        case InsertAfter(id, commands, replyTo) ⇒ sequencer.insertAfter(id, commands).foreach(replyTo.tell)
+        case AddBreakpoint(id, replyTo)         ⇒ sequencer.addBreakpoint(id).foreach(replyTo.tell)
+        case RemoveBreakpoint(id, replyTo)      ⇒ sequencer.removeBreakpoint(id).foreach(replyTo.tell)
 
         // ===== Internal =====
         case PullNext(replyTo)              ⇒ sequencer.pullNext().foreach(replyTo.tell)
--- esw-ocs/esw-ocs-framework/src/test/scala/esw/ocs/framework/core/SequencerBehaviorTest.scala
+++ esw-ocs/esw-ocs-framework/src/test/scala/esw/ocs/framework/core/SequencerBehaviorTest.scala
@@ -1,15 +1,18 @@
 package esw.ocs.framework.core
 
+import akka.Done
 import akka.actor.testkit.typed.scaladsl.{ActorTestKitBase, TestProbe}
-import csw.params.commands.CommandResponse.{Completed, SubmitResponse}
+import csw.params.commands.CommandResponse.{Completed, Error, SubmitResponse}
 import csw.params.commands.{CommandName, Setup}
 import csw.params.core.models.{Id, Prefix}
 import esw.ocs.framework.BaseTestSuite
+import esw.ocs.framework.api.models.SequenceEditor.EditorResponse
 import esw.ocs.framework.api.models.messages.ProcessSequenceError
-import esw.ocs.framework.api.models.messages.SequencerMsg.{Available, GetSequence, ProcessSequence}
-import esw.ocs.framework.api.models.{Sequence, StepList}
+import esw.ocs.framework.api.models.messages.SequencerMsg._
+import esw.ocs.framework.api.models.messages.StepListError._
+import esw.ocs.framework.api.models.{Sequence, Step, StepList}
 import esw.ocs.framework.dsl.ScriptDsl
-import org.mockito.Mockito.when
+import org.mockito.Mockito.{verify, when}
 import org.scalatestplus.mockito.MockitoSugar
 
 import scala.concurrent.Future
@@ -57,5 +60,166 @@ class SequencerBehaviorTest extends ActorTestKitBase with BaseTestSuite with Moc
       testProbe.expectMessage(stepListResponse)
     }
 
+    "GetPreviousSequence" in {
+      val testProbe: TestProbe[Option[StepList]] = TestProbe()
+
+      val stepListResponse: Option[StepList] = Some(StepList.empty)
+      when(sequencer.getPreviousSequence).thenReturn(Future.successful(stepListResponse))
+
+      sequencerActor ! GetPreviousSequence(testProbe.ref)
+      testProbe.expectMessage(stepListResponse)
+    }
+
+    "Add" in {
+      val testProbe: TestProbe[EditorResponse[AddError]] = TestProbe()
+      val command1                                       = Setup(Prefix("test"), CommandName("command-1"), None)
+
+      val addResponse: EditorResponse[AddError] = Right(Done)
+      when(sequencer.add(List(command1))).thenReturn(Future.successful(addResponse))
+
+      sequencerActor ! Add(List(command1), testProbe.ref)
+      testProbe.expectMessage(addResponse)
+    }
+
+    "Pause" in {
+      val testProbe: TestProbe[EditorResponse[PauseError]] = TestProbe()
+
+      val pauseResponse: EditorResponse[PauseError] = Right(Done)
+      when(sequencer.pause).thenReturn(Future.successful(pauseResponse))
+
+      sequencerActor ! Pause(testProbe.ref)
+      testProbe.expectMessage(pauseResponse)
+    }
+
+    "Resume" in {
+      val testProbe: TestProbe[EditorResponse[ResumeError]] = TestProbe()
+
+      val resumeResponse: EditorResponse[ResumeError] = Right(Done)
+      when(sequencer.resume).thenReturn(Future.successful(resumeResponse))
+
+      sequencerActor ! Resume(testProbe.ref)
+      testProbe.expectMessage(resumeResponse)
+    }
+
+    "Reset" in {
+      val testProbe: TestProbe[EditorResponse[ResetError]] = TestProbe()
+
+      val resetResponse: EditorResponse[ResetError] = Right(Done)
+      when(sequencer.reset()).thenReturn(Future.successful(resetResponse))
+
+      sequencerActor ! Reset(testProbe.ref)
+      testProbe.expectMessage(resetResponse)
+    }
+
+    "Replace" in {
+      val testProbe: TestProbe[EditorResponse[ReplaceError]] = TestProbe()
+      val runId                                              = Id()
+      val command1                                           = Setup(Prefix("test"), CommandName("command-1"), None)
+
+      val replaceResponse: EditorResponse[ReplaceError] = Right(Done)
+      when(sequencer.replace(runId, List(command1))).thenReturn(Future.successful(replaceResponse))
+
+      sequencerActor ! Replace(runId, List(command1), testProbe.ref)
+      testProbe.expectMessage(replaceResponse)
+    }
+
+    "Prepend" in {
+      val testProbe: TestProbe[EditorResponse[PrependError]] = TestProbe()
+      val command1                                           = Setup(Prefix("test"), CommandName("command-1"), None)
+
+      val prependResponse: EditorResponse[PrependError] = Right(Done)
+      when(sequencer.prepend(List(command1))).thenReturn(Future.successful(prependResponse))
+
+      sequencerActor ! Prepend(List(command1), testProbe.ref)
+      testProbe.expectMessage(prependResponse)
+    }
+
+    "Delete" in {
+      val testProbe: TestProbe[EditorResponse[DeleteError]] = TestProbe()
+      val runId                                             = Id()
+
+      val deleteResponse: EditorResponse[DeleteError] = Right(Done)
+      when(sequencer.delete(runId)).thenReturn(Future.successful(deleteResponse))
+
+      sequencerActor ! Delete(runId, testProbe.ref)
+      testProbe.expectMessage(deleteResponse)
+    }
+
+    "InsertAfter" in {
+      val testProbe: TestProbe[EditorResponse[InsertError]] = TestProbe()
+      val runId                                             = Id()
+      val command1                                          = Setup(Prefix("test"), CommandName("command-1"), None)
+
+      val insertResponse: EditorResponse[InsertError] = Right(Done)
+      when(sequencer.insertAfter(runId, List(command1))).thenReturn(Future.successful(insertResponse))
+
+      sequencerActor ! InsertAfter(runId, List(command1), testProbe.ref)
+      testProbe.expectMessage(insertResponse)
+    }
+
+    "AddBreakpoint" in {
+      val testProbe: TestProbe[EditorResponse[AddBreakpointError]] = TestProbe()
+      val runId                                                    = Id()
+
+      val addBreakpointResponse: EditorResponse[AddBreakpointError] = Right(Done)
+      when(sequencer.addBreakpoint(runId)).thenReturn(Future.successful(addBreakpointResponse))
+
+      sequencerActor ! AddBreakpoint(runId, testProbe.ref)
+      testProbe.expectMessage(addBreakpointResponse)
+    }
+
+    "RemoveBreakpoint" in {
+      val testProbe: TestProbe[EditorResponse[RemoveBreakpointError]] = TestProbe()
+      val runId                                                       = Id()
+
+      val removeBreakpointResponse: EditorResponse[RemoveBreakpointError] = Right(Done)
+      when(sequencer.removeBreakpoint(runId)).thenReturn(Future.successful(removeBreakpointResponse))
+
+      sequencerActor ! RemoveBreakpoint(runId, testProbe.ref)
+      testProbe.expectMessage(removeBreakpointResponse)
+    }
+
+    "PullNext" in {
+      val testProbe: TestProbe[Step] = TestProbe()
+      val command1                   = Setup(Prefix("test"), CommandName("command-1"), None)
+
+      val pullNextResponse = Step(command1)
+
+      when(sequencer.pullNext()).thenReturn(Future.successful(pullNextResponse))
+
+      sequencerActor ! PullNext(testProbe.ref)
+      testProbe.expectMessage(pullNextResponse)
+    }
+
+    "MaybeNext" in {
+      val testProbe: TestProbe[Option[Step]] = TestProbe()
+      val command1                           = Setup(Prefix("test"), CommandName("command-1"), None)
+
+      val mayBeNextResponse = Some(Step(command1))
+
+      when(sequencer.mayBeNext).thenReturn(Future.successful(mayBeNextResponse))
+
+      sequencerActor ! MaybeNext(testProbe.ref)
+      testProbe.expectMessage(mayBeNextResponse)
+    }
+
+    "ReadyToExecuteNext" in {
+      val testProbe: TestProbe[Done] = TestProbe()
+
+      val ReadyToExecuteNextResponse = Done
+
+      when(sequencer.readyToExecuteNext()).thenReturn(Future.successful(ReadyToExecuteNextResponse))
+
+      sequencerActor ! ReadyToExecuteNext(testProbe.ref)
+      testProbe.expectMessage(ReadyToExecuteNextResponse)
+    }
+
+    "UpdateFailure" in {
+      val errorResponse: SubmitResponse = Error(Id(), "")
+
+      sequencerActor ! UpdateFailure(errorResponse)
+
+      verify(sequencer).updateFailure(errorResponse)
+    }
   }
 }
