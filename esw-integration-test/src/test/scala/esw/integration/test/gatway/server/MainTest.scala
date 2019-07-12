8323eb030712a4ebbcbc3400fe28a5d9583d6441
Poorva Gokhale <poorvasgokhale@gmail.com>
2019-07-12 12:27:16 +0530

    #ESW-103 : Shubham | Poorva : Test if published event can be get via gateway.
    Co-authored-by: Shubham Jaybhaye <34642890+shubhamJay@users.noreply.github.com>

--- esw-integration-test/src/test/scala/esw/integration/test/gatway/server/MainTest.scala
+++ esw-integration-test/src/test/scala/esw/integration/test/gatway/server/MainTest.scala
@@ -6,8 +6,9 @@ import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.adapter.TypedActorSystemOps
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.Uri.Path
+import akka.http.scaladsl.model.Uri.{Path, Query}
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.typed.scaladsl.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 import csw.location.api.models.Connection.HttpConnection
@@ -15,18 +16,18 @@ import csw.location.api.models.{ComponentId, ComponentType}
 import csw.location.api.scaladsl.LocationService
 import csw.location.client.scaladsl.HttpLocationServiceFactory
 import csw.params.core.formats.JsonSupport
+import csw.params.core.generics.Parameter
 import csw.params.core.models.Prefix
-import csw.params.events.{EventName, SystemEvent}
+import csw.params.events.{Event, EventName, SystemEvent}
 import csw.testkit.{EventTestKit, LocationTestKit}
+import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
 import esw.gateway.server.Main
 import esw.template.http.server.BaseTestSuite
 import esw.template.http.server.TestFutureExtensions.RichFuture
 
 import scala.concurrent.duration.DurationInt
 
-class MainTest extends BaseTestSuite {
-
-  import JsonSupport._
+class MainTest extends BaseTestSuite with JsonSupport with PlayJsonSupport {
 
   private val locationTestKit = LocationTestKit()
   private val eventTestKit    = EventTestKit()
@@ -61,12 +62,25 @@ class MainTest extends BaseTestSuite {
 
       gatewayServiceLocation.connection shouldBe expectedConnection
       val uri       = Uri(gatewayServiceLocation.uri.toString).withPath(Path / "event")
-      val event     = SystemEvent(Prefix("tcs.test.gateway"), EventName("event"))
-      val eventJson = HttpEntity(ContentTypes.`application/json`, writeEvent(event).toString())
+      val event     = SystemEvent(Prefix("tcs.test.gateway"), EventName("event"), Set.empty[Parameter[_]])
+      val jsObject  = eventFormat.writes(event).toString()
+      val eventJson = HttpEntity(ContentTypes.`application/json`, jsObject)
 
-      val request  = HttpRequest(uri = uri).withMethod(HttpMethods.POST).withEntity(eventJson)
+      //Publish event
+      val request  = HttpRequest(uri = uri, method = HttpMethods.POST, entity = eventJson)
       val response = Http().singleRequest(request).await
+
+      //assert if event is successfully published
       response.status shouldBe StatusCodes.OK
+
+      //Get event by specifying event key
+      val getRequest  = HttpRequest(uri = uri.withQuery(Query(("key", "tcs.test.gateway.event"))))
+      val getResponse = Http().singleRequest(getRequest).await
+
+      //assert on response of getEvent call
+      getResponse.status shouldBe StatusCodes.OK
+      val actualEvent = Unmarshal(getResponse.entity).to[Set[Event]].await
+      actualEvent shouldEqual Set(event)
 
     } finally {
       httpService.shutdown(UnknownReason).await
