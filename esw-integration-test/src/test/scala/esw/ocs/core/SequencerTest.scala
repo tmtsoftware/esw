fc3606e581f31b8576627765744153f7658100b1
Dollyg <dollyg@thoughtworks.com>
2019-07-23 12:42:24 +0530

    #ESW-190 : Shubham | Dolly : Modify Sequencer Integration Test to resolve sequencer from remote location service
    Co-authored-by: Shubham Chauhan <shubham25may1998@gmail.com>

--- esw-integration-test/src/test/scala/esw/ocs/core/SequencerTest.scala
+++ esw-integration-test/src/test/scala/esw/ocs/core/SequencerTest.scala
@@ -1,21 +1,56 @@
 package esw.ocs.core
 
 import akka.Done
+import akka.actor.Scheduler
+import akka.actor.typed.scaladsl.AskPattern._
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.stream.typed.scaladsl.ActorMaterializer
+import akka.util.Timeout
+import csw.command.client.messages.{ProcessSequence, ProcessSequenceResponse, SequencerMsg}
+import csw.location.api.extensions.URIExtension.RichURI
+import csw.location.api.scaladsl.LocationService
+import csw.location.client.ActorSystemFactory
+import csw.location.client.scaladsl.HttpLocationServiceFactory
+import csw.location.models.Connection.AkkaConnection
+import csw.location.models.{ComponentId, ComponentType}
 import csw.params.commands.CommandResponse.Completed
 import csw.params.commands.{CommandName, Sequence, Setup}
 import csw.params.core.models.Prefix
-import csw.testkit.scaladsl.ScalaTestFrameworkTestKit
+import csw.testkit.LocationTestKit
 import esw.ocs.BaseTestSuite
-import esw.ocs.api.models.StepStatus.Finished
 import esw.ocs.api.models.Step
+import esw.ocs.api.models.StepStatus.Finished
+import esw.ocs.api.models.messages.EditorResponse
+import esw.ocs.api.models.messages.SequencerMessages.{Add, GetSequence}
 import esw.ocs.internal.SequencerWiring
 
-class SequencerTest extends ScalaTestFrameworkTestKit with BaseTestSuite {
-  private var wiring: SequencerWiring = _
+import scala.concurrent.Future
+import scala.concurrent.duration.DurationLong
+
+class SequencerTest extends BaseTestSuite {
+
+  private implicit val sys: ActorSystem[SequencerMsg] = ActorSystemFactory.remote(Behavior.empty)
+  private implicit val mat: ActorMaterializer         = ActorMaterializer()
+  private implicit val timeout: Timeout               = Timeout(5.seconds)
+  private implicit val scheduler: Scheduler           = sys.scheduler
+
+  private val sequencerId   = "testSequencerId1"
+  private val observingMode = "testObservingMode1"
+
+  private val locationTestKit                   = LocationTestKit()
+  private var locationService: LocationService  = _
+  private var wiring: SequencerWiring           = _
+  private var sequencer: ActorRef[SequencerMsg] = _
+
+  override def beforeAll(): Unit = {
+    locationTestKit.startLocationServer()
+    locationService = HttpLocationServiceFactory.makeLocalClient
+  }
 
   override protected def beforeEach(): Unit = {
     wiring = new SequencerWiring("testSequencerId1", "testObservingMode1")
     wiring.start()
+    sequencer = resolveSequencer()
   }
 
   override protected def afterEach(): Unit = {
@@ -27,11 +62,11 @@ class SequencerTest extends ScalaTestFrameworkTestKit with BaseTestSuite {
       val command3 = Setup(Prefix("test"), CommandName("command-3"), None)
       val sequence = Sequence(command3)
 
-      val processSeqResponse = wiring.sequencer.processSequence(sequence)
+      val processSeqResponse: Future[ProcessSequenceResponse] = sequencer ? (ProcessSequence(sequence, _))
 
       processSeqResponse.futureValue.response.rightValue should ===(Completed(sequence.runId))
 
-      wiring.sequencer.getSequence.futureValue.steps should ===(
+      (sequencer ? GetSequence).futureValue.steps should ===(
         List(Step(command3, Finished.Success(Completed(command3.runId)), hasBreakpoint = false))
       )
     }
@@ -40,16 +75,16 @@ class SequencerTest extends ScalaTestFrameworkTestKit with BaseTestSuite {
       val command1 = Setup(Prefix("test"), CommandName("command-1"), None)
       val command2 = Setup(Prefix("test"), CommandName("command-2"), None)
       val command3 = Setup(Prefix("test"), CommandName("command-3"), None)
-
       val sequence = Sequence(command1, command2)
 
-      val processSeqResponse = wiring.sequencer.processSequence(sequence)
+      val processSeqResponse: Future[ProcessSequenceResponse] = sequencer ? (ProcessSequence(sequence, _))
 
-      wiring.sequenceEditorClient.add(List(command3)).futureValue.response.rightValue should ===(Done)
+      val addResponse: Future[EditorResponse] = sequencer ? (Add(List(command3), _))
+      addResponse.futureValue.response.rightValue should ===(Done)
 
       processSeqResponse.futureValue.response.rightValue should ===(Completed(sequence.runId))
 
-      wiring.sequencer.getSequence.futureValue.steps should ===(
+      (sequencer ? GetSequence).futureValue.steps should ===(
         List(
           Step(command1, Finished.Success(Completed(command1.runId)), hasBreakpoint = false),
           Step(command2, Finished.Success(Completed(command2.runId)), hasBreakpoint = false),
@@ -58,4 +93,13 @@ class SequencerTest extends ScalaTestFrameworkTestKit with BaseTestSuite {
       )
     }
   }
+
+  private def resolveSequencer(): ActorRef[SequencerMsg] =
+    locationService
+      .resolve(AkkaConnection(ComponentId(s"$sequencerId@$observingMode", ComponentType.Sequencer)), 5.seconds)
+      .futureValue
+      .value
+      .uri
+      .toActorRef
+      .unsafeUpcast[SequencerMsg]
 }
