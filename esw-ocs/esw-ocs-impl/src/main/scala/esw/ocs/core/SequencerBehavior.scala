bc76f0687a07339e4feff185b5647d003efd74c4
Dollyg <dollyg@thoughtworks.com>
2019-07-28 17:15:33 +0530

    #ESW-194 : Dolly : fix formatting and add comments

--- esw-ocs/esw-ocs-impl/src/main/scala/esw/ocs/core/SequencerBehavior.scala
+++ esw-ocs/esw-ocs-impl/src/main/scala/esw/ocs/core/SequencerBehavior.scala
@@ -30,32 +30,28 @@ class SequencerBehavior(
     msg match {
       // ===== External Lifecycle =====
       case Shutdown(replyTo)  => shutdown(replyTo)(ctx); Behaviors.same
-      case GoOnline(replyTo)  => goOnline(replyTo); Behaviors.same // should this be removed
       case GoOffline(replyTo) => goOffline(replyTo)(ctx); intermediateBehavior
       case Abort(replyTo) => abort(replyTo); Behaviors.same
+      // GoOnline message is not handled since sequencer is by default in online mode
 
       // ===== External Editor =====
       case LoadSequence(sequence, replyTo)         => sequencer.load(sequence).foreach(replyTo.tell); Behaviors.same;
       case StartSequence(replyTo)                  => sequencer.start().foreach(replyTo.tell); Behaviors.same
       case LoadAndStartSequence(sequence, replyTo) => sequencer.loadAndStart(sequence).foreach(replyTo.tell); Behaviors.same
 
-      case Available(replyTo)   => sequencer.isAvailable.foreach(replyTo.tell); Behaviors.same
-      case GetSequence(replyTo) => sequencer.getSequence.foreach(replyTo.tell); Behaviors.same
-      case GetPreviousSequence(replyTo) =>
-        sequencer.getPreviousSequence.foreach(replyTo ! StepListResponse(_)); Behaviors.same
-      case Add(commands, replyTo) => sequencer.add(commands).foreach(replyTo ! EditorResponse(_)); Behaviors.same
-      case Pause(replyTo)         => sequencer.pause.foreach(replyTo ! EditorResponse(_)); Behaviors.same
-      case Resume(replyTo)        => sequencer.resume.foreach(replyTo ! EditorResponse(_)); Behaviors.same
-      case Reset(replyTo)         => sequencer.reset().foreach(replyTo ! EditorResponse(_)); Behaviors.same
-      case Replace(id, commands, replyTo) =>
-        sequencer.replace(id, commands).foreach(replyTo ! EditorResponse(_)); Behaviors.same
-      case Prepend(commands, replyTo) => sequencer.prepend(commands).foreach(replyTo ! EditorResponse(_)); Behaviors.same
-      case Delete(id, replyTo)        => sequencer.delete(id).foreach(replyTo ! EditorResponse(_)); Behaviors.same
-      case InsertAfter(id, commands, replyTo) =>
-        sequencer.insertAfter(id, commands).foreach(replyTo ! EditorResponse(_)); Behaviors.same
-      case AddBreakpoint(id, replyTo) => sequencer.addBreakpoint(id).foreach(replyTo ! EditorResponse(_)); Behaviors.same
-      case RemoveBreakpoint(id, replyTo) =>
-        sequencer.removeBreakpoint(id).foreach(replyTo ! EditorResponse(_)); Behaviors.same
+      case Available(replyTo)             => sequencer.isAvailable.foreach(replyTo.tell); Behaviors.same
+      case GetSequence(replyTo)           => sequencer.getSequence.foreach(replyTo.tell); Behaviors.same
+      case GetPreviousSequence(replyTo)   => sequencer.getPreviousSequence.foreach(replyTo ! StepListResponse(_)); Behaviors.same
+      case Add(commands, replyTo)         => sequencer.add(commands).foreach(replyTo ! EditorResponse(_)); Behaviors.same
+      case Pause(replyTo)                 => sequencer.pause.foreach(replyTo ! EditorResponse(_)); Behaviors.same
+      case Resume(replyTo)                => sequencer.resume.foreach(replyTo ! EditorResponse(_)); Behaviors.same
+      case Reset(replyTo)                 => sequencer.reset().foreach(replyTo ! EditorResponse(_)); Behaviors.same
+      case Replace(id, commands, replyTo) => sequencer.replace(id, commands).foreach(replyTo ! EditorResponse(_)); Behaviors.same
+      case Prepend(commands, replyTo)     => sequencer.prepend(commands).foreach(replyTo ! EditorResponse(_)); Behaviors.same
+      case Delete(id, replyTo)            => sequencer.delete(id).foreach(replyTo ! EditorResponse(_)); Behaviors.same
+      case InsertAfter(id, cmds, replyTo) => sequencer.insertAfter(id, cmds).foreach(replyTo ! EditorResponse(_)); Behaviors.same
+      case AddBreakpoint(id, replyTo)     => sequencer.addBreakpoint(id).foreach(replyTo ! EditorResponse(_)); Behaviors.same
+      case RemoveBreakpoint(id, replyTo) => sequencer.removeBreakpoint(id).foreach(replyTo ! EditorResponse(_)); Behaviors.same
 
       // ===== Internal =====
       case PullNext(replyTo)              => sequencer.pullNext().foreach(replyTo.tell); Behaviors.same
@@ -68,16 +64,16 @@ class SequencerBehavior(
   private def offlineBehavior: Behavior[SequencerMsg] = Behaviors.receive[SequencerMsg] { (ctx, msg) =>
     import ctx.executionContext
     msg match {
-      case Shutdown(replyTo) => shutdown(replyTo)(ctx); Behaviors.same
+      case Shutdown(replyTo) => shutdown(replyTo)(ctx); Behaviors.same // fixme: should not receive any messages
       case GoOnline(replyTo) => goOnline(replyTo); defaultBehavior
-      case _                 => Behaviors.same // what to do on offline behavior
+      case _                 => Behaviors.same // reject all other commands in offline state
     }
   }
 
   private def intermediateBehavior: Behavior[SequencerMsg] = Behaviors.receiveMessage[SequencerMsg] {
     case ChangeBehaviorToOffline => offlineBehavior
     case ChangeBehaviorToDefault => defaultBehavior
-    case _                       => Behaviors.same
+    case _                       => Behaviors.same // do not receive any other commands in transition from online to offline
   }
 
   private def shutdown(replyTo: ActorRef[LifecycleResponse])(implicit ctx: ActorContext[SequencerMsg]): Unit = {
