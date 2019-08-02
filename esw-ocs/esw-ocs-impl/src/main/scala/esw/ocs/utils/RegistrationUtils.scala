4285771ff56b5eec424ef94ad13a07cba800ded2
Poorva Gokhale <poorvasgokhale@gmail.com>
2019-08-02 15:14:28 +0530

    #ESW-144: Poorva | Puneet : Add registerWithRetry utility
    Co-authored-by: Puneet Patwari <puneet.patwari@thoughtworks.com>

--- esw-ocs/esw-ocs-impl/src/main/scala/esw/ocs/utils/RegistrationUtils.scala
+++ esw-ocs/esw-ocs-impl/src/main/scala/esw/ocs/utils/RegistrationUtils.scala
@@ -1,6 +1,7 @@
 package esw.ocs.utils
 
 import akka.actor.CoordinatedShutdown
+import csw.location.api.exceptions.OtherLocationIsRegistered
 import csw.location.api.scaladsl.{LocationService, RegistrationResult}
 import csw.location.models.{AkkaLocation, AkkaRegistration}
 import esw.ocs.api.models.messages.RegistrationError
@@ -10,25 +11,35 @@ import scala.util.control.NonFatal
 
 object RegistrationUtils {
 
+  private def addCoordinatedShutdownTask(
+      coordinatedShutdown: CoordinatedShutdown,
+      registrationResult: RegistrationResult
+  ): Unit = {
+    coordinatedShutdown.addTask(
+      CoordinatedShutdown.PhaseBeforeServiceUnbind,
+      s"unregistering-${registrationResult.location}"
+    )(() => registrationResult.unregister())
+  }
+
   def register(locationService: LocationService, akkaRegistration: AkkaRegistration)(
       coordinatedShutdown: CoordinatedShutdown
+  )(implicit ec: ExecutionContext): Future[Either[RegistrationError, AkkaLocation]] =
+    registerWithRetry(locationService, akkaRegistration, 0)(coordinatedShutdown)
+
+  def registerWithRetry(locationService: LocationService, akkaRegistration: AkkaRegistration, retryCount: Int)(
+      coordinatedShutdown: CoordinatedShutdown
   )(implicit ec: ExecutionContext): Future[Either[RegistrationError, AkkaLocation]] = {
-
-    def addCoordinatedShutdownTask(registrationResult: RegistrationResult): Unit = {
-      coordinatedShutdown.addTask(
-        CoordinatedShutdown.PhaseBeforeServiceUnbind,
-        s"unregistering-${registrationResult.location}"
-      )(() => registrationResult.unregister())
-    }
-
     locationService
       .register(akkaRegistration)
       .map { result =>
-        addCoordinatedShutdownTask(result)
+        addCoordinatedShutdownTask(coordinatedShutdown, result)
         Right(result.location.asInstanceOf[AkkaLocation])
       }
-      .recover {
-        case NonFatal(e) => Left(RegistrationError(e.getMessage))
+      .recoverWith {
+        case OtherLocationIsRegistered(_) if retryCount > 0 =>
+          registerWithRetry(locationService, akkaRegistration, retryCount - 1)(coordinatedShutdown)
+        case NonFatal(e) => Future.successful(Left(RegistrationError(e.getMessage)))
       }
   }
+
 }
--- esw-ocs/esw-ocs-impl/src/test/scala/esw/ocs/utils/RegistrationUtilsTest.scala
+++ esw-ocs/esw-ocs-impl/src/test/scala/esw/ocs/utils/RegistrationUtilsTest.scala
@@ -5,7 +5,7 @@ import java.net.URI
 import akka.Done
 import akka.actor.CoordinatedShutdown.UnknownReason
 import akka.actor.{ActorSystem, CoordinatedShutdown}
-import csw.location.api.exceptions.OtherLocationIsRegistered
+import csw.location.api.exceptions.{OtherLocationIsRegistered, RegistrationFailed}
 import csw.location.api.scaladsl.{LocationService, RegistrationResult}
 import csw.location.models.ComponentType.Sequencer
 import csw.location.models.Connection.AkkaConnection
@@ -46,10 +46,86 @@ class RegistrationUtilsTest extends BaseTestSuite {
       val coordinatedShutdown = CoordinatedShutdown(system)
       val errorMsg            = "error message"
       when(locationService.register(registration)).thenReturn(Future.failed(OtherLocationIsRegistered(errorMsg)))
+
       RegistrationUtils.register(locationService, registration)(coordinatedShutdown).leftValue should ===(
         RegistrationError(errorMsg)
       )
       system.terminate().futureValue
     }
+  }
+
+  "registerWithRetry" must {
+    "return successful RegistrationResult | ESW-144" in {
+      val system              = ActorSystem("test")
+      val coordinatedShutdown = CoordinatedShutdown(system)
+      val retryCount          = 2
+      val registrationResult  = mock[RegistrationResult]
+
+      when(registrationResult.location).thenReturn(akkaLocation)
+      when(registrationResult.unregister()).thenReturn(Future.successful(Done))
+      when(locationService.register(registration)).thenReturn(Future(registrationResult))
+
+      RegistrationUtils.registerWithRetry(locationService, registration, retryCount)(coordinatedShutdown).rightValue should ===(
+        akkaLocation
+      )
+      coordinatedShutdown.run(UnknownReason).futureValue
+      verify(registrationResult).unregister()
+    }
+
+    "retry if OtherLocationIsRegistered | ESW-144" in {
+      val system              = ActorSystem("test")
+      val coordinatedShutdown = CoordinatedShutdown(system)
+      val errorMsg            = "error message"
+      val retryCount          = 1
+      val registrationResult  = mock[RegistrationResult]
+
+      when(registrationResult.location).thenReturn(akkaLocation)
+      when(registrationResult.unregister()).thenReturn(Future.successful(Done))
+      when(locationService.register(registration))
+        .thenReturn(Future.failed(OtherLocationIsRegistered(errorMsg)), Future(registrationResult))
+
+      RegistrationUtils.registerWithRetry(locationService, registration, retryCount)(coordinatedShutdown).rightValue should ===(
+        akkaLocation
+      )
+      coordinatedShutdown.run(UnknownReason).futureValue
+      verify(registrationResult).unregister()
+    }
+
+    "not retry if RegistrationFailed | ESW-144" in {
+      val system              = ActorSystem("test")
+      val coordinatedShutdown = CoordinatedShutdown(system)
+      val errorMsg            = "error message"
+      val retryCount          = 3
+      val registrationResult  = mock[RegistrationResult]
+
+      when(registrationResult.location).thenReturn(akkaLocation)
+      when(registrationResult.unregister()).thenReturn(Future.successful(Done))
+      when(locationService.register(registration))
+        .thenReturn(Future.failed(RegistrationFailed(errorMsg)), Future(registrationResult))
+
+      RegistrationUtils.registerWithRetry(locationService, registration, retryCount)(coordinatedShutdown).leftValue should ===(
+        RegistrationError(errorMsg)
+      )
+      system.terminate().futureValue
+    }
+
+    "map location service registration failure to RegistrationError if could not register after retry attempts | ESW-144" in {
+      val system              = ActorSystem("test")
+      val coordinatedShutdown = CoordinatedShutdown(system)
+      val errorMsg            = "error message"
+      val retryCount          = 2
+      when(locationService.register(registration))
+        .thenReturn(
+          Future.failed(OtherLocationIsRegistered(errorMsg)),
+          Future.failed(OtherLocationIsRegistered(errorMsg)),
+          Future.failed(OtherLocationIsRegistered(errorMsg))
+        )
+
+      RegistrationUtils.registerWithRetry(locationService, registration, retryCount)(coordinatedShutdown).leftValue should ===(
+        RegistrationError(errorMsg)
+      )
+      system.terminate().futureValue
+    }
+
   }
 }
