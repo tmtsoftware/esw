93af62617447c575b80c22c83948cfb03ebb35cb
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-08-05 17:49:02 +0530

    #ESW-194: Bilal | Shubham : added GoOffline and LoadSequence Messages

--- esw-ocs/esw-ocs-api/src/main/scala/esw/ocs/api/models/messages/FSM/SequencerResponse.scala
+++ esw-ocs/esw-ocs-api/src/main/scala/esw/ocs/api/models/messages/FSM/SequencerResponse.scala
@@ -1,15 +1,27 @@
 package esw.ocs.api.models.messages.FSM
 
 sealed trait SequencerResponse
-sealed trait GoOnlineResponse extends SequencerResponse
-sealed trait ShutdownResponse extends SequencerResponse
+sealed trait GoOnlineResponse     extends SequencerResponse
+sealed trait GoOfflineResponse    extends SequencerResponse
+sealed trait ShutdownResponse     extends SequencerResponse
+sealed trait LoadSequenceResponse extends SequencerResponse
 
-case class Unhandled(state: String, messageType: String) extends GoOnlineResponse with ShutdownResponse {
+case class Unhandled(state: String, messageType: String)
+    extends GoOnlineResponse
+    with ShutdownResponse
+    with GoOfflineResponse
+    with LoadSequenceResponse {
   val description = s"Sequencer can not handle '$messageType' message in '$state' state"
 }
 
-case object EswSuccess                                              extends GoOnlineResponse with ShutdownResponse
-case class EswError(error: String, cause: Option[Throwable] = None) extends GoOnlineResponse with ShutdownResponse
+case object EswSuccess extends GoOnlineResponse with ShutdownResponse with GoOfflineResponse with LoadSequenceResponse
+
+case class EswError(error: String, cause: Option[Throwable] = None)
+    extends GoOnlineResponse
+    with ShutdownResponse
+    with GoOfflineResponse
+    with LoadSequenceResponse
+
 object EswError {
-  def apply(cause: Throwable) = EswError(cause.getMessage, Some(cause))
+  def apply(cause: Throwable): EswError = EswError(cause.getMessage, Some(cause))
 }
