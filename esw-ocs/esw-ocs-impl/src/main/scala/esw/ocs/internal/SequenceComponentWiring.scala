a43845c22f7299827195ba0c3b645c34cc3375ec
Puneet Patwari <puneet.patwari@thoughtworks.com>
2019-08-08 16:12:09 +0530

    #ESW-215: Poorva | Puneet: Extract SequenceComponentRegistration. Add tests.
    Co-authored-by: Poorva Gokhale <13188905+Poorva17@users.noreply.github.com>

--- esw-ocs/esw-ocs-impl/src/main/scala/esw/ocs/internal/SequenceComponentWiring.scala
+++ esw-ocs/esw-ocs-impl/src/main/scala/esw/ocs/internal/SequenceComponentWiring.scala
@@ -1,26 +1,12 @@
 package esw.ocs.internal
 
-import akka.actor.CoordinatedShutdown
-import akka.actor.typed.SpawnProtocol.Spawn
-import akka.actor.typed.scaladsl.AskPattern.Askable
-import akka.actor.typed.scaladsl.adapter.TypedActorSystemOps
-import akka.actor.typed.{ActorRef, ActorSystem}
-import csw.location.api.exceptions.OtherLocationIsRegistered
-import csw.location.api.extensions.ActorExtension.RichActor
-import csw.location.api.extensions.URIExtension._
-import csw.location.api.scaladsl.{LocationService, RegistrationResult}
+import csw.location.api.scaladsl.LocationService
 import csw.location.client.scaladsl.HttpLocationServiceFactory
-import csw.location.models.Connection.AkkaConnection
-import csw.location.models.{AkkaLocation, AkkaRegistration, ComponentId, ComponentType}
+import csw.location.models.AkkaLocation
 import csw.params.core.models.Prefix
-import esw.ocs.api.models.messages.SequenceComponentMsg.Stop
-import esw.ocs.api.models.messages.{RegistrationError, SequenceComponentMsg}
-import esw.ocs.core.SequenceComponentBehavior
+import esw.ocs.api.models.messages.RegistrationError
 import esw.ocs.syntax.FutureSyntax.FutureOps
 import esw.utils.csw.LocationServiceUtils
-
-import scala.concurrent.{ExecutionContext, Future}
-import scala.util.control.NonFatal
 
 // $COVERAGE-OFF$
 private[ocs] class SequenceComponentWiring(prefix: Prefix) {
@@ -32,60 +18,11 @@ private[ocs] class SequenceComponentWiring(prefix: Prefix) {
   private lazy val locationService: LocationService = HttpLocationServiceFactory.makeLocalClient
 
   private lazy val locationServiceUtils: LocationServiceUtils = new LocationServiceUtils(locationService)
-
-  private def addCoordinatedShutdownTask(
-      coordinatedShutdown: CoordinatedShutdown,
-      registrationResult: RegistrationResult
-  ): Unit = {
-    coordinatedShutdown.addTask(
-      CoordinatedShutdown.PhaseBeforeServiceUnbind,
-      s"unregistering-${registrationResult.location}"
-    )(() => registrationResult.unregister())
-  }
-
-  private def registerWithRetry(akkaRegistration: => AkkaRegistration, retryCount: Int)(
-      implicit actorSystem: ActorSystem[_]
-  ): Future[Either[RegistrationError, AkkaLocation]] = {
-    implicit val ec: ExecutionContext = actorSystem.executionContext
-    locationService
-      .register(akkaRegistration)
-      .map { result =>
-        addCoordinatedShutdownTask(CoordinatedShutdown(actorSystem.toUntyped), result)
-        Right(result.location.asInstanceOf[AkkaLocation])
-      }
-      .recoverWith {
-        case OtherLocationIsRegistered(_) if retryCount > 0 =>
-          //kill actor ref if registration fails. Retry attempt will create new actor ref
-          akkaRegistration.actorRefURI.toActorRef.unsafeUpcast[SequenceComponentMsg] ! Stop
-          registerWithRetry(akkaRegistration, retryCount - 1)
-        case NonFatal(e) => Future.successful(Left(RegistrationError(e.getMessage)))
-      }
-  }
-
-  private def generateSequenceComponentName(): String = {
-    val subsystem = prefix.subsystem
-    locationServiceUtils
-      .listBy(subsystem, ComponentType.SequenceComponent)
-      .map { sequenceComponents =>
-        val uniqueId = s"${sequenceComponents.length + 1}"
-        s"${subsystem}_$uniqueId"
-      }
-      .block
-  }
-
-  private def registration(): AkkaRegistration = {
-    val sequenceComponentName = generateSequenceComponentName()
-    val sequenceComponentRef: ActorRef[SequenceComponentMsg] =
-      (typedSystem ? Spawn(SequenceComponentBehavior.behavior(sequenceComponentName), sequenceComponentName)).block
-    AkkaRegistration(
-      AkkaConnection(ComponentId(sequenceComponentName, ComponentType.SequenceComponent)),
-      prefix,
-      sequenceComponentRef.toURI
-    )
-  }
+  private lazy val sequenceComponentRegistration =
+    new SequenceComponentRegistration(prefix, locationService, locationServiceUtils)
 
   def start(): Either[RegistrationError, AkkaLocation] =
-    registerWithRetry(registration(), registrationRetryCount).block
+    sequenceComponentRegistration.registerWithRetry(registrationRetryCount).block
 
 }
 // $COVERAGE-ON$
