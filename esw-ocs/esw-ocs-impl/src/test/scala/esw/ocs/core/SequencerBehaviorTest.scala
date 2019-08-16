085e1596eaf92016c8bbec9a94ee273184374b5b
Dollyg <dollyg@thoughtworks.com>
2019-08-16 16:31:55 +0530

    #ESW-194: Pritam | Dolly : Add tests for Add api
    Co-authored-by: Pritam Kadam <phkadam2008@gmail.com>

--- esw-ocs/esw-ocs-impl/src/test/scala/esw/ocs/core/SequencerBehaviorTest.scala
+++ esw-ocs/esw-ocs-impl/src/test/scala/esw/ocs/core/SequencerBehaviorTest.scala
@@ -2,8 +2,9 @@ package esw.ocs.core
 
 import java.util.concurrent.CountDownLatch
 
-import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.testkit.typed.scaladsl.{ScalaTestWithActorTestKit, TestProbe}
 import akka.actor.typed.ActorRef
+import akka.stream.TLSClientAuth
 import csw.command.client.CommandResponseManager
 import csw.command.client.messages.sequencer.{LoadAndStartSequence, SequencerMsg}
 import csw.location.api.scaladsl.LocationService
@@ -12,9 +13,9 @@ import csw.params.commands.CommandResponse.{Completed, Error, SubmitResponse}
 import csw.params.commands.{CommandName, CommandResponse, Sequence, Setup}
 import csw.params.core.models.{Id, Prefix}
 import esw.ocs.api.BaseTestSuite
-import esw.ocs.api.models.StepStatus.Finished
-import esw.ocs.api.models.messages.SequencerMessages.{GetPreviousSequence, GetSequence, LoadSequence, StartSequence}
-import esw.ocs.api.models.messages._
+import esw.ocs.api.models.StepStatus.{Finished, InFlight}
+import esw.ocs.api.models.messages.SequencerMessages.{Add, GetPreviousSequence, GetSequence, LoadSequence, StartSequence}
+import esw.ocs.api.models.messages.{StepListResponse, _}
 import esw.ocs.api.models.{Step, StepList}
 import esw.ocs.dsl.Script
 import org.mockito.Mockito.when
@@ -47,19 +48,47 @@ class SequencerBehaviorTest extends ScalaTestWithActorTestKit with BaseTestSuite
       if (latch.getCount == 0) completionPromise.complete(Success(CommandResponse.withRunId(sequence.runId, submitResponse)))
       submitResponse
     }
-  }
 
-  def assertSequencerIsLoaded(sequencerActor: ActorRef[SequencerMsg], sequence: Sequence): Unit = {
-    val probe = createTestProbe[LoadSequenceResponse]()
-    sequencerActor ! LoadSequence(sequence, probe.ref)
-    probe.expectMessage(Ok)
+    private def assertStepListResponse(
+        expected: StepListResponse,
+        msg: ActorRef[StepListResponse] => SequencerMsg
+    ): Unit = {
+      val probe: TestProbe[StepListResponse] = createTestProbe[StepListResponse]()
+      sequencerActor ! msg(probe.ref)
+      probe.expectMessage(expected)
+    }
+
+    def assertCurrentSequence(expected: StepListResponse): Unit =
+      assertStepListResponse(expected, GetSequence)
+
+    def assertPreviousSequence(expected: StepListResponse): Unit =
+      assertStepListResponse(expected, GetPreviousSequence)
+
+    def assertSequencerIsLoaded(sequence: Sequence, expected: LoadSequenceResponse): Unit = {
+      val probe = createTestProbe[LoadSequenceResponse]()
+      sequencerActor ! LoadSequence(sequence, probe.ref)
+      probe.expectMessage(expected)
+    }
+
+    def assertSequencerIsInProgress(sequence: Sequence): TestProbe[SubmitResponse] = {
+      val probe = createTestProbe[SubmitResponse]()
+      sequencerActor ! LoadAndStartSequence(sequence, probe.ref)
+      val p: TestProbe[StepListResponse] = createTestProbe[StepListResponse]()
+
+      eventually {
+        sequencerActor ! GetSequence(p.ref)
+        val result = p.expectMessageType[StepListResult]
+        result.stepList.isDefined shouldBe true
+      }
+      probe
+    }
   }
 
   "LoadSequence" must {
     "load the given sequence in idle state" in {
       val sequencerSetup = new SequencerSetup(sequence1)
       import sequencerSetup._
-      assertSequencerIsLoaded(sequencerActor, sequence1)
+      assertSequencerIsLoaded(sequence1, Ok)
     }
 
     "fail when given sequence contains duplicate Ids" in {
@@ -68,9 +97,7 @@ class SequencerBehaviorTest extends ScalaTestWithActorTestKit with BaseTestSuite
       val sequencerSetup = new SequencerSetup(invalidSequence)
       import sequencerSetup._
 
-      val loadSeqResProbe = createTestProbe[LoadSequenceResponse]()
-      sequencerActor ! LoadSequence(invalidSequence, loadSeqResProbe.ref)
-      loadSeqResProbe.expectMessage(DuplicateIdsFound)
+      assertSequencerIsLoaded(invalidSequence, DuplicateIdsFound)
     }
   }
 
@@ -83,7 +110,7 @@ class SequencerBehaviorTest extends ScalaTestWithActorTestKit with BaseTestSuite
       val cmd1Response = Completed(command1.runId)
       when(crm.queryFinal(command1.runId)).thenAnswer(_ => queryResponse(cmd1Response, latch))
 
-      assertSequencerIsLoaded(sequencerActor, sequence1)
+      assertSequencerIsLoaded(sequence1, Ok)
 
       val seqResProbe = createTestProbe[SequenceResponse]()
       sequencerActor ! StartSequence(seqResProbe.ref)
@@ -121,24 +148,16 @@ class SequencerBehaviorTest extends ScalaTestWithActorTestKit with BaseTestSuite
   }
 
   "GetSequence" must {
-    val command1       = Setup(Prefix("esw.test"), CommandName("command-1"), None)
-    val sequence       = Sequence(Id(), Seq(command1))
-    val sequencerSetup = new SequencerSetup(sequence)
+    val sequencerSetup = new SequencerSetup(sequence1)
     import sequencerSetup._
 
     "return None when in Idle state" in {
-      val probe = createTestProbe[StepListResponse]()
-      sequencerActor ! GetSequence(probe.ref)
-      probe.expectMessage(StepListResult(None))
+      assertCurrentSequence(StepListResult(None))
     }
 
     "return sequence when in loaded state" in {
-      val probe             = createTestProbe[StepListResponse]()
-      val loadedSeqResProbe = createTestProbe[LoadSequenceResponse]
-      sequencerActor ! LoadSequence(sequence, loadedSeqResProbe.ref)
-      loadedSeqResProbe.expectMessage(Ok)
-      sequencerActor ! GetSequence(probe.ref)
-      probe.expectMessage(StepListResult(StepList(sequence).toOption))
+      assertSequencerIsLoaded(sequence1, Ok)
+      assertCurrentSequence(StepListResult(StepList(sequence1).toOption))
     }
   }
 
@@ -146,17 +165,11 @@ class SequencerBehaviorTest extends ScalaTestWithActorTestKit with BaseTestSuite
     "return None when sequencer has not started executing any sequence" in {
       val sequencerSetup = new SequencerSetup(sequence1)
       import sequencerSetup._
-
-      val stepListResProbe = createTestProbe[StepListResponse]()
-      sequencerActor ! GetPreviousSequence(stepListResProbe.ref)
-      stepListResProbe.expectMessage(StepListResult(None))
-
-      val loadedSeqResProbe = createTestProbe[LoadSequenceResponse]
-      sequencerActor ! LoadSequence(sequence1, loadedSeqResProbe.ref)
-      loadedSeqResProbe.expectMessage(Ok)
-
-      sequencerActor ! GetPreviousSequence(stepListResProbe.ref)
-      stepListResProbe.expectMessage(StepListResult(None))
+      // in idle state
+      assertPreviousSequence(StepListResult(None))
+      assertSequencerIsLoaded(sequence1, Ok)
+      // in loaded state
+      assertPreviousSequence(StepListResult(None))
     }
 
     "return previous sequence after new sequence is loaded" in {
@@ -172,19 +185,45 @@ class SequencerBehaviorTest extends ScalaTestWithActorTestKit with BaseTestSuite
       sequencerActor ! LoadAndStartSequence(sequence1, loadAndStartResProbe.ref)
       loadAndStartResProbe.expectMessage(Completed(sequence1.runId))
 
-      val loadedSeqResProbe = createTestProbe[LoadSequenceResponse]
-      sequencerActor ! LoadSequence(sequence2, loadedSeqResProbe.ref)
-      loadedSeqResProbe.expectMessage(Ok)
+      assertSequencerIsLoaded(sequence2, Ok)
 
-      val stepListResProbe = createTestProbe[StepListResponse]()
-      sequencerActor ! GetPreviousSequence(stepListResProbe.ref)
+      val expectedPreviousSequence = StepListResult(
+        Some(StepList(sequence1.runId, List(Step(command1, Finished.Success(Completed(command1.runId)), hasBreakpoint = false))))
+      )
 
-      stepListResProbe.expectMessage(
-        StepListResult(
-          Some(
-            StepList(sequence1.runId, List(Step(command1, Finished.Success(Completed(command1.runId)), hasBreakpoint = false)))
-          )
-        )
+      assertPreviousSequence(expectedPreviousSequence)
+    }
+  }
+
+  "Add" must {
+    "add commands when sequence is loaded" in {
+      val sequencerSetup = new SequencerSetup(sequence1)
+      import sequencerSetup._
+
+      assertSequencerIsLoaded(sequence1, Ok)
+      val probe = createTestProbe[OkOrUnhandledResponse]()
+      sequencerActor ! Add(List(command2), probe.ref)
+      probe.expectMessage(Ok)
+
+      val updatedSequence = sequence1.copy(commands = Seq(command1, command2))
+      assertCurrentSequence(StepListResult(StepList(updatedSequence).toOption))
+    }
+
+    "add commands when sequence is in progress" in {
+      val sequencerSetup = new SequencerSetup(sequence1)
+      import sequencerSetup._
+
+      val submitResponsePromise = Promise[SubmitResponse]()
+      when(crm.queryFinal(command1.runId)).thenReturn(submitResponsePromise.future)
+
+      assertSequencerIsInProgress(sequence1)
+
+      val probe = createTestProbe[OkOrUnhandledResponse]()
+      sequencerActor ! Add(List(command2), probe.ref)
+      probe.expectMessage(Ok)
+
+      assertCurrentSequence(
+        StepListResult(Some(StepList(sequence1.runId, List(Step(command1, InFlight, hasBreakpoint = false), Step(command2)))))
       )
     }
   }
