4460c6fa06e2edb62dd91cae3cf8e229c43eef26
Dollyg <dollyg@thoughtworks.com>
2019-08-16 15:16:44 +0530

    #ESW-194: Pritam | Dolly : Add tests for start and load sequence
    Co-authored-by: Pritam Kadam <phkadam2008@gmail.com>

--- esw-ocs/esw-ocs-impl/src/test/scala/esw/ocs/core/SequencerBehaviorTest.scala
+++ esw-ocs/esw-ocs-impl/src/test/scala/esw/ocs/core/SequencerBehaviorTest.scala
@@ -8,13 +8,13 @@ import csw.command.client.CommandResponseManager
 import csw.command.client.messages.sequencer.{LoadAndStartSequence, SequencerMsg}
 import csw.location.api.scaladsl.LocationService
 import csw.location.models.{ComponentId, ComponentType}
-import csw.params.commands.CommandResponse.{Completed, SubmitResponse}
+import csw.params.commands.CommandResponse.{Completed, Error, SubmitResponse}
 import csw.params.commands.{CommandName, CommandResponse, Sequence, Setup}
 import csw.params.core.models.{Id, Prefix}
 import esw.ocs.api.BaseTestSuite
 import esw.ocs.api.models.StepStatus.Finished
-import esw.ocs.api.models.messages.SequencerMessages.{GetPreviousSequence, GetSequence, LoadSequence}
-import esw.ocs.api.models.messages.{LoadSequenceResponse, Ok, StepListResponse, StepListResult}
+import esw.ocs.api.models.messages.SequencerMessages.{GetPreviousSequence, GetSequence, LoadSequence, StartSequence}
+import esw.ocs.api.models.messages._
 import esw.ocs.api.models.{Step, StepList}
 import esw.ocs.dsl.Script
 import org.mockito.Mockito.when
@@ -49,17 +49,45 @@ class SequencerBehaviorTest extends ScalaTestWithActorTestKit with BaseTestSuite
     }
   }
 
-  "LoadSequence" must {
-    "load sequence in idle state" in {
-      val command1 = Setup(Prefix("esw.test"), CommandName("command-1"), None)
-      val sequence = Sequence(Id(), Seq(command1))
+  def assertSequencerIsLoaded(sequencerActor: ActorRef[SequencerMsg], sequence: Sequence): Unit = {
+    val probe = createTestProbe[LoadSequenceResponse]()
+    sequencerActor ! LoadSequence(sequence, probe.ref)
+    probe.expectMessage(Ok)
+  }
 
-      val sequencerSetup = new SequencerSetup(sequence)
+  "LoadSequence" must {
+    "load the given sequence in idle state" in {
+      val sequencerSetup = new SequencerSetup(sequence1)
+      import sequencerSetup._
+      assertSequencerIsLoaded(sequencerActor, sequence1)
+    }
+
+    "fail when given sequence contains duplicate Ids" in {
+      val invalidSequence = Sequence(Id(), Seq(command1, command1))
+
+      val sequencerSetup = new SequencerSetup(invalidSequence)
       import sequencerSetup._
 
-      val probe = createTestProbe[LoadSequenceResponse]()
-      sequencerActor ! LoadSequence(sequence, probe.ref)
-      probe.expectMessage(Ok)
+      val loadSeqResProbe = createTestProbe[LoadSequenceResponse]()
+      sequencerActor ! LoadSequence(invalidSequence, loadSeqResProbe.ref)
+      loadSeqResProbe.expectMessage(DuplicateIdsFound)
+    }
+  }
+
+  "StartSequence" must {
+    "start executing a sequence when sequencer is loaded" in {
+      val latch          = new CountDownLatch(1)
+      val sequencerSetup = new SequencerSetup(sequence1)
+      import sequencerSetup._
+
+      val cmd1Response = Completed(command1.runId)
+      when(crm.queryFinal(command1.runId)).thenAnswer(_ => queryResponse(cmd1Response, latch))
+
+      assertSequencerIsLoaded(sequencerActor, sequence1)
+
+      val seqResProbe = createTestProbe[SequenceResponse]()
+      sequencerActor ! StartSequence(seqResProbe.ref)
+      seqResProbe.expectMessage(SequenceResult(Completed(sequence1.runId)))
     }
   }
 
@@ -78,6 +106,17 @@ class SequencerBehaviorTest extends ScalaTestWithActorTestKit with BaseTestSuite
       val probe = createTestProbe[SubmitResponse]()
       sequencerActor ! LoadAndStartSequence(sequence, probe.ref)
       probe.expectMessage(Completed(sequence.runId))
+    }
+
+    "fail when given sequence contains duplicate Ids" in {
+      val invalidSequence = Sequence(Id(), Seq(command1, command1))
+
+      val sequencerSetup = new SequencerSetup(invalidSequence)
+      import sequencerSetup._
+
+      val loadSeqResProbe = createTestProbe[SubmitResponse]()
+      sequencerActor ! LoadAndStartSequence(invalidSequence, loadSeqResProbe.ref)
+      loadSeqResProbe.expectMessage(Error(invalidSequence.runId, DuplicateIdsFound.description))
     }
   }
 
