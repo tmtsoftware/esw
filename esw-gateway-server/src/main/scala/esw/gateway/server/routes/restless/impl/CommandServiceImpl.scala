49d96da053216c6e6045d6b921dfb2579e992686
Pritam Kadam <kpritam@thoughtworks.com>
2019-08-16 21:38:24 +0530

    #000: Remove unused imports

--- esw-gateway-server/src/main/scala/esw/gateway/server/routes/restless/impl/CommandServiceImpl.scala
+++ esw-gateway-server/src/main/scala/esw/gateway/server/routes/restless/impl/CommandServiceImpl.scala
@@ -1,20 +1,13 @@
 package esw.gateway.server.routes.restless.impl
 
-import akka.stream.OverflowStrategy
-import akka.stream.scaladsl.Source
 import akka.util.Timeout
-import csw.command.api.CurrentStateSubscription
 import csw.params.commands.CommandResponse
-import csw.params.core.states.CurrentState
 import esw.gateway.server.routes.restless.api.CommandServiceApi
-import esw.gateway.server.routes.restless.codecs.RestlessCodecs
 import esw.gateway.server.routes.restless.messages.CommandAction.{Oneway, Submit, Validate}
-import esw.gateway.server.routes.restless.messages.ErrorResponseMsg.{InvalidComponent, InvalidMaxFrequency}
+import esw.gateway.server.routes.restless.messages.ErrorResponseMsg.InvalidComponent
 import esw.gateway.server.routes.restless.messages.RequestMsg.CommandMsg
-import esw.gateway.server.routes.restless.messages.WebSocketMsg.CurrentStateSubscriptionCommandMsg
 import esw.gateway.server.routes.restless.messages.{ErrorResponseMsg, WebSocketMsg}
 import esw.http.core.utils.CswContext
-import msocket.core.api.Payload
 
 import scala.concurrent.Future
 import scala.concurrent.duration.DurationLong
