a45b37a43329d14c558757d9a96c5b0dee8b5702
Pritam Kadam <kpritam@thoughtworks.com>
2019-08-30 15:36:16 +0530

    add fatJar taks in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.jvm.tasks.Jar
 
 plugins {
     java
     kotlin("jvm") version "1.3.50"
 }
 
-group = "asda"
+group = "com.github.tmtsoftware.esw"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -15,13 +16,6 @@ repositories {
 }
 
 dependencies {
-    /*
-    implementation "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0"
-    compile group: 'org.jetbrains.kotlinx', name: 'kotlinx-coroutines-jdk8', version: '1.3.0'
-    implementation 'com.github.tmtsoftware.csw:csw-params_2.13:1c2c2c4baaed0e14ae6b2dc579787b8f4b38616c'
-    implementation 'com.github.tmtsoftware.esw:esw-ocs-impl_2.13:c964c43911dfbf88d0ffff10f9507f859d6eec2e'
-     */
-
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.github.tmtsoftware.esw:esw-ocs-impl_2.13:c964c43911dfbf88d0ffff10f9507f859d6eec2e")
     implementation("com.github.tmtsoftware.csw:csw-params_2.13:1c2c2c4baaed0e14ae6b2dc579787b8f4b38616c")
@@ -32,6 +26,25 @@ dependencies {
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
+
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+
+val fatJar = task("fatJar", type = Jar::class) {
+    baseName = "${project.name}-fat"
+    manifest {
+        attributes["Implementation-Title"] = "Gradle Jar File Example"
+        attributes["Implementation-Version"] = version
+        attributes["Main-Class"] = "esw.ocs.dsl.MainKt"
+    }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    with(tasks.jar.get() as CopySpec)
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
 }
---
+++ src/main/kotlin/esw/ocs/dsl/Main.kt
@@ -0,0 +1,5 @@
+package esw.ocs.dsl
+
+fun main() {
+    println("Hello world")
+}
