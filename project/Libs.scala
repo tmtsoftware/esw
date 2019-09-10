4e9cc012394633b9f5a6d7802faa7a9d8a96f3ba
Scala Steward <me@scala-steward.org>
2019-09-10 12:28:14 +0200

    Update mockito-scala to 1.5.16

--- project/Libs.scala
+++ project/Libs.scala
@@ -11,7 +11,7 @@ object Libs {
   val scalatest          = "org.scalatest" %% "scalatest" % "3.0.8" //Apache License 2.0
   val scopt              = "com.github.scopt" %% "scopt" % "4.0.0-RC2" //MIT License
   val `scala-async`      = "org.scala-lang.modules" %% "scala-async" % "0.10.0" //BSD 3-clause "New" or "Revised" License
-  val `mockito-scala`    = "org.mockito" %% "mockito-scala" % "1.5.13" // MIT License
+  val `mockito-scala`    = "org.mockito" %% "mockito-scala" % "1.5.16" // MIT License
   val enumeratum         = dep("com.beachape" %%% "enumeratum" % "1.5.13") //MIT License
   val `case-app`         = "com.github.alexarchambault" %% "case-app" % "2.0.0-M9"
   val `silencer-plugin`  = compilerPlugin("com.github.ghik" %% "silencer-plugin" % SilencerVersion)
