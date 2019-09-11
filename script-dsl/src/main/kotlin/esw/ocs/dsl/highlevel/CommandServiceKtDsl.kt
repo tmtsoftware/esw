64989f0337e89b8cc2e0da12c2335ec7abcb63db
Pritam Kadam <kpritam@thoughtworks.com>
2019-09-11 22:10:07 +0530

    Add command service APIs

--- script-dsl/src/main/kotlin/esw/ocs/dsl/highlevel/CommandServiceKtDsl.kt
+++ script-dsl/src/main/kotlin/esw/ocs/dsl/highlevel/CommandServiceKtDsl.kt
@@ -1,8 +1,7 @@
 package esw.ocs.dsl.highlevel
 
 import csw.params.commands.CommandName
-import csw.params.commands.CommandResponse.OnewayResponse
-import csw.params.commands.CommandResponse.SubmitResponse
+import csw.params.commands.CommandResponse.*
 import csw.params.commands.ControlCommand
 import csw.params.commands.Observe
 import csw.params.commands.Setup
@@ -22,18 +21,31 @@ interface CommandServiceKtDsl {
     fun observe(prefix: String, commandName: String, obsId: String?) =
         Observe(Prefix(prefix), CommandName(commandName), obsId.toOptionalObsId())
 
+    /************* Assembly *************/
+    suspend fun validateAssemblyCommand(assemblyName: String, command: ControlCommand): ValidateResponse =
+        cswServices.validateAssemblyCommand(assemblyName, command).await()
+
     suspend fun submitCommandToAssembly(assemblyName: String, command: ControlCommand): SubmitResponse =
         cswServices.submitCommandToAssembly(assemblyName, command).await()
+
+    suspend fun submitAndWaitCommandToAssembly(assemblyName: String, command: ControlCommand): SubmitResponse =
+        cswServices.submitAndWaitCommandToAssembly(assemblyName, command).await()
+
+    suspend fun oneWayCommandToAssembly(assemblyName: String, command: ControlCommand): OnewayResponse =
+        cswServices.oneWayCommandToAssembly(assemblyName, command).await()
+
+    /************* HCD *************/
+    suspend fun validateHcdCommand(assemblyName: String, command: ControlCommand): ValidateResponse =
+        cswServices.validateHcdCommand(assemblyName, command).await()
 
     suspend fun submitCommandToHcd(hcdName: String, command: ControlCommand): SubmitResponse =
         cswServices.submitCommandToHcd(hcdName, command).await()
 
-    suspend fun oneWayToAssembly(assemblyName: String, command: ControlCommand): OnewayResponse =
-        cswServices.oneWayToAssembly(assemblyName, command).await()
+    suspend fun submitAndWaitCommandToHcd(hcdName: String, command: ControlCommand): SubmitResponse =
+        cswServices.submitAndWaitCommandToHcd(hcdName, command).await()
 
     suspend fun oneWayCommandToHcd(hcdName: String, command: ControlCommand): OnewayResponse =
         cswServices.oneWayCommandToHcd(hcdName, command).await()
-
 
     private fun String?.toOptionalObsId() = Optional.ofNullable(this?.let { ObsId(it) })
 }
--- script-dsl/src/main/kotlin/esw/ocs/dsl/params/Commands.kt
+++ script-dsl/src/main/kotlin/esw/ocs/dsl/params/Commands.kt
@@ -3,4 +3,4 @@ package esw.ocs.dsl.params
 import csw.params.core.generics.Parameter
 import csw.params.core.generics.ParameterSetType
 
-fun <P : Parameter<*>, T : ParameterSetType<T>> ParameterSetType<T>.madd(vararg values: P) = jMadd(values.toSet())
+fun <P : Parameter<*>, T : ParameterSetType<T>> ParameterSetType<T>.madd(vararg values: P): T = jMadd(values.toSet())
