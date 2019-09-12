a6efaae2ac9c414cd5699385e42d9dfa77d2c232
Shubham Jaybhaye <shubhamj7997@gmail.com>
2019-09-12 10:39:25 +0530

    Make task suspendable in scheduling api

--- script-dsl/src/main/kotlin/esw/ocs/dsl/highlevel/TimeServiceKtDsl.kt
+++ script-dsl/src/main/kotlin/esw/ocs/dsl/highlevel/TimeServiceKtDsl.kt
@@ -2,9 +2,12 @@ package esw.ocs.dsl.highlevel
 
 import csw.time.core.models.TMTTime
 import csw.time.scheduler.api.Cancellable
+import esw.highlevel.dsl.javadsl.Callback
 import esw.ocs.impl.dsl.CswServices
 import esw.ocs.macros.StrandEc
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.future.future
+import java.util.concurrent.CompletionStage
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 import kotlin.time.toJavaDuration
@@ -13,8 +16,16 @@ interface TimeServiceKtDsl : CoroutineScope {
     val cswServices: CswServices
     fun strandEc(): StrandEc
 
-    fun scheduleOnce(startTime: TMTTime, task: () -> Unit): Cancellable =
-        cswServices.scheduleOnce(startTime, task, strandEc().ec())
+    private fun (suspend () -> Unit).toJavaFuture(): CompletionStage<Void> =
+        this.let {
+            return future {
+                it()
+            }.thenAccept { }
+        }
+
+    //todo : Verify task works fine this way
+    suspend fun scheduleOnce(startTime: TMTTime, task: suspend () -> Unit): Cancellable =
+        cswServices.scheduleOnce(startTime, Callback { task.toJavaFuture() }, strandEc().ec())
 
     @ExperimentalTime
     fun schedulePeriodically(
