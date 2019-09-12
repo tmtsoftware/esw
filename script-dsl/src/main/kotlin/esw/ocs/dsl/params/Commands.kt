dafa8abe65148ee5ccb024a9602eda329411d762
Pritam Kadam <kpritam@thoughtworks.com>
2019-09-12 12:22:39 +0530

    Add some more extensions for ease of use from scripts

--- script-dsl/src/main/kotlin/esw/ocs/dsl/params/Commands.kt
+++ script-dsl/src/main/kotlin/esw/ocs/dsl/params/Commands.kt
@@ -1,6 +1,23 @@
 package esw.ocs.dsl.params
 
+import csw.params.commands.Command
 import csw.params.core.generics.Parameter
 import csw.params.core.generics.ParameterSetType
+import esw.ocs.dsl.nullable
 
-fun <P : Parameter<*>, T : ParameterSetType<T>> ParameterSetType<T>.madd(vararg values: P): T = jMadd(values.toSet())
+fun <A : ParameterSetType<A>, B : Parameter<*>> ParameterSetType<A>.madd(vararg values: B): A = jMadd(values.toSet())
+
+val Command.obsId: String?
+    get() {
+        return jMaybeObsId().map { it.obsId() }.nullable()
+    }
+
+// throws exception
+operator fun <A : ParameterSetType<A>, B> ParameterSetType<A>.invoke(keyHolder: KeyHolder<B>): Parameter<B> {
+    return jGet(keyHolder.key).get()
+}
+
+// throws exception
+operator fun <A> Parameter<A>.invoke(index: Int): A {
+    return jGet(index).get()
+}
--- script-dsl/src/main/kotlin/esw/ocs/dsl/params/Keys.kt
+++ script-dsl/src/main/kotlin/esw/ocs/dsl/params/Keys.kt
@@ -1,5 +1,6 @@
 package esw.ocs.dsl.params
 
+import csw.params.core.generics.GChoiceKey
 import csw.params.core.generics.Key
 import csw.params.core.generics.KeyType
 import csw.params.core.generics.Parameter
@@ -9,6 +10,8 @@ import csw.params.events.SystemEvent
 import csw.params.javadsl.JKeyType.*
 import csw.params.javadsl.JUnits.NoUnits
 import csw.params.javadsl.JUnits.lightyear
+import csw.time.core.models.TAITime
+import csw.time.core.models.UTCTime
 
 // this is present just to hide java methods on Keys which are not user friendly
 data class KeyHolder<T>(val key: Key<T>) {
@@ -19,51 +22,52 @@ data class KeyHolder<T>(val key: Key<T>) {
 }
 
 // ============= Misc Keys ===========
-fun choiceKey(name: String, choices: Choices) = ChoiceKey().make(name, choices)
+fun choiceKey(name: String, choices: Choices): GChoiceKey = ChoiceKey().make(name, choices)
 
-fun choiceKey(name: String, vararg choices: Choice) = ChoiceKey().make(name, Choices.fromChoices(choices.toSet()))
+fun choiceKey(name: String, vararg choices: Choice): GChoiceKey =
+    ChoiceKey().make(name, Choices.fromChoices(choices.toSet()))
 
 fun raDecKey(name: String) = KeyHolder(RaDecKey().make(name))
 
-fun eqCoordKey(name: String) = KeyHolder(EqCoordKey().make(name))
-fun solarSystemCoordKey(name: String) = KeyHolder(SolarSystemCoordKey().make(name))
-fun minorPlanetCoordKey(name: String) = KeyHolder(MinorPlanetCoordKey().make(name))
-fun cometCoordKey(name: String) = KeyHolder(CometCoordKey().make(name))
-fun altAzCoordKey(name: String) = KeyHolder(AltAzCoordKey().make(name))
-fun coordKey(name: String) = KeyHolder(CoordKey().make(name))
-fun stringKey(name: String) = KeyHolder(StringKey().make(name))
-fun structKey(name: String) = KeyHolder(StructKey().make(name))
-fun utcTimeKey(name: String) = KeyHolder(UTCTimeKey().make(name))
-fun taiTimeKey(name: String) = KeyHolder(TAITimeKey().make(name))
+fun eqCoordKey(name: String): KeyHolder<Coords.EqCoord> = KeyHolder(EqCoordKey().make(name))
+fun solarSystemCoordKey(name: String): KeyHolder<Coords.SolarSystemCoord> = KeyHolder(SolarSystemCoordKey().make(name))
+fun minorPlanetCoordKey(name: String): KeyHolder<Coords.MinorPlanetCoord> = KeyHolder(MinorPlanetCoordKey().make(name))
+fun cometCoordKey(name: String): KeyHolder<Coords.CometCoord> = KeyHolder(CometCoordKey().make(name))
+fun altAzCoordKey(name: String): KeyHolder<Coords.AltAzCoord> = KeyHolder(AltAzCoordKey().make(name))
+fun coordKey(name: String): KeyHolder<Coords.Coord> = KeyHolder(CoordKey().make(name))
+fun stringKey(name: String): KeyHolder<String> = KeyHolder(StringKey().make(name))
+fun structKey(name: String): KeyHolder<Struct> = KeyHolder(StructKey().make(name))
+fun utcTimeKey(name: String): KeyHolder<UTCTime> = KeyHolder(UTCTimeKey().make(name))
+fun taiTimeKey(name: String): KeyHolder<TAITime> = KeyHolder(TAITimeKey().make(name))
 
 // ============= Simple Keys ===========
-fun booleanKey(name: String) = KeyHolder(BooleanKey().make(name))
+fun booleanKey(name: String): KeyHolder<Boolean> = KeyHolder(BooleanKey().make(name))
 
-fun charKey(name: String) = KeyHolder(CharKey().make(name))
-fun byteKey(name: String) = KeyHolder(ByteKey().make(name))
-fun shortKey(name: String) = KeyHolder(ShortKey().make(name))
+fun charKey(name: String): KeyHolder<Char> = KeyHolder(CharKey().make(name))
+fun byteKey(name: String): KeyHolder<Byte> = KeyHolder(ByteKey().make(name))
+fun shortKey(name: String): KeyHolder<Short> = KeyHolder(ShortKey().make(name))
 fun longKey(name: String) = KeyHolder(LongKey().make(name))
-fun intKey(name: String) = KeyHolder(IntKey().make(name))
-fun floatKey(name: String) = KeyHolder(FloatKey().make(name))
-fun doubleKey(name: String) = KeyHolder(DoubleKey().make(name))
+fun intKey(name: String): KeyHolder<Int> = KeyHolder(IntKey().make(name))
+fun floatKey(name: String): KeyHolder<Float> = KeyHolder(FloatKey().make(name))
+fun doubleKey(name: String): KeyHolder<Double> = KeyHolder(DoubleKey().make(name))
 
 // ============= Array Keys ===========
-fun byteArrayKey(name: String) = KeyHolder(ByteArrayKey().make(name))
+fun byteArrayKey(name: String): KeyHolder<ArrayData<Byte>> = KeyHolder(ByteArrayKey().make(name))
 
-fun shortArrayKey(name: String) = KeyHolder(ShortArrayKey().make(name))
+fun shortArrayKey(name: String): KeyHolder<ArrayData<Short>> = KeyHolder(ShortArrayKey().make(name))
 fun longArrayKey(name: String) = KeyHolder(LongArrayKey().make(name))
-fun intArrayKey(name: String) = KeyHolder(IntArrayKey().make(name))
-fun floatArrayKey(name: String) = KeyHolder(FloatArrayKey().make(name))
-fun doubleArrayKey(name: String) = KeyHolder(DoubleArrayKey().make(name))
+fun intArrayKey(name: String): KeyHolder<ArrayData<Int>> = KeyHolder(IntArrayKey().make(name))
+fun floatArrayKey(name: String): KeyHolder<ArrayData<Float>> = KeyHolder(FloatArrayKey().make(name))
+fun doubleArrayKey(name: String): KeyHolder<ArrayData<Double>> = KeyHolder(DoubleArrayKey().make(name))
 
 // ============= Matrix Keys ===========
-fun byteMatrixKey(name: String) = KeyHolder(ByteMatrixKey().make(name))
+fun byteMatrixKey(name: String): KeyHolder<MatrixData<Byte>> = KeyHolder(ByteMatrixKey().make(name))
 
-fun shortMatrixKey(name: String) = KeyHolder(ShortMatrixKey().make(name))
-fun longMatrixKey(name: String) = KeyHolder(LongMatrixKey().make(name))
-fun intMatrixKey(name: String) = KeyHolder(IntMatrixKey().make(name))
-fun floatMatrixKey(name: String) = KeyHolder(FloatMatrixKey().make(name))
-fun doubleMatrixKey(name: String) = KeyHolder(DoubleMatrixKey().make(name))
+fun shortMatrixKey(name: String): KeyHolder<MatrixData<Short>> = KeyHolder(ShortMatrixKey().make(name))
+fun longMatrixKey(name: String): KeyHolder<MatrixData<Long>> = KeyHolder(LongMatrixKey().make(name))
+fun intMatrixKey(name: String): KeyHolder<MatrixData<Int>> = KeyHolder(IntMatrixKey().make(name))
+fun floatMatrixKey(name: String): KeyHolder<MatrixData<Float>> = KeyHolder(FloatMatrixKey().make(name))
+fun doubleMatrixKey(name: String): KeyHolder<MatrixData<Double>> = KeyHolder(DoubleMatrixKey().make(name))
 
 fun <T> arrayData(elms: Array<T>): ArrayData<T> = ArrayData.fromJavaArray(elms)
 inline fun <reified T> matrixData(elms: Array<Array<T>>): MatrixData<T> = MatrixData.fromJavaArrays(T::class.java, elms)
