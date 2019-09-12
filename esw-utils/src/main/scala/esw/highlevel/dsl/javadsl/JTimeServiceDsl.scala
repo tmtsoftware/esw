500ca149988c42a6dbd7735770159c02b2517731
Shubham Jaybhaye <shubhamj7997@gmail.com>
2019-09-12 10:41:55 +0530

    #ESW-00 : Pritam | Shubham: Make schedule api compatible for kotlin

--- esw-utils/src/main/scala/esw/highlevel/dsl/javadsl/JTimeServiceDsl.scala
+++ esw-utils/src/main/scala/esw/highlevel/dsl/javadsl/JTimeServiceDsl.scala
@@ -1,6 +1,7 @@
 package esw.highlevel.dsl.javadsl
 
 import java.time.Duration
+import java.util.concurrent.CompletionStage
 
 import csw.time.core.models.TMTTime
 import csw.time.scheduler.api.Cancellable
@@ -8,12 +9,19 @@ import esw.highlevel.dsl.TimeServiceDsl
 
 import scala.concurrent.ExecutionContext
 
+case class Callback(cb: () => CompletionStage[Void])
+
 trait JTimeServiceDsl { self: TimeServiceDsl =>
 
-  def scheduleOnce(startTime: TMTTime, task: Runnable, ec: ExecutionContext): Cancellable =
-    timeServiceSchedulerFactory.make()(ec).scheduleOnce(startTime, task)
+  // fixme : test does task maps to Kotlin's suspendable?
+  def scheduleOnce(startTime: TMTTime, task: Callback, ec: ExecutionContext): Cancellable = {
+    val block: Runnable = () => task.cb()
+    timeServiceSchedulerFactory.make()(ec).scheduleOnce(startTime, block)
+  }
 
-  def schedulePeriodically(startTime: TMTTime, interval: Duration, task: Runnable, ec: ExecutionContext): Cancellable =
-    timeServiceSchedulerFactory.make()(ec).schedulePeriodically(startTime, interval, task)
+  def schedulePeriodically(startTime: TMTTime, interval: Duration, task: Callback, ec: ExecutionContext): Cancellable = {
+    val block: Runnable = () => task.cb()
+    timeServiceSchedulerFactory.make()(ec).schedulePeriodically(startTime, interval, block)
+  }
 
 }
