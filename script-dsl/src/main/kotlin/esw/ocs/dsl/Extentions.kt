d137326b6296175fd08082db8d87f50a5ddfc37e
Pritam Kadam <kpritam@thoughtworks.com>
2019-09-13 16:27:03 +0530

    Add suspendable extensions for IEventSubsctiption

--- script-dsl/src/main/kotlin/esw/ocs/dsl/Extentions.kt
+++ script-dsl/src/main/kotlin/esw/ocs/dsl/Extentions.kt
@@ -1,5 +1,11 @@
 package esw.ocs.dsl
 
+import akka.Done
+import csw.event.api.javadsl.IEventSubscription
+import kotlinx.coroutines.future.await
 import java.util.*
 
 fun <T> Optional<T>.nullable(): T? = orElse(null)
+
+suspend fun IEventSubscription.cancel(): Done = unsubscribe().await()
+suspend fun IEventSubscription.completed(): Done = ready().await()
