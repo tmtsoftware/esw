dbc7859cbbeee3d95123eae896eacf5feda3784f
Pritam Kadam <kpritam@thoughtworks.com>
2019-09-13 11:23:41 +0530

    provide submitSequence java API

--- esw-ocs/esw-ocs-impl/src/main/scala/esw/ocs/impl/dsl/CswServices.scala
+++ esw-ocs/esw-ocs-impl/src/main/scala/esw/ocs/impl/dsl/CswServices.scala
@@ -1,14 +1,20 @@
 package esw.ocs.impl.dsl
 
+import java.util.concurrent.CompletionStage
+
 import akka.actor.typed.ActorSystem
 import csw.command.client.CommandResponseManager
 import csw.event.api.scaladsl.EventService
 import csw.location.api.scaladsl.LocationService
+import csw.params.commands.CommandResponse.SubmitResponse
+import csw.params.commands.Sequence
 import csw.time.scheduler.TimeServiceSchedulerFactory
 import esw.highlevel.dsl.javadsl.{JCommandServiceDsl, JEventServiceDsl, JLocationServiceDsl, JTimeServiceDsl}
 import esw.highlevel.dsl.{EventServiceDsl, LocationServiceDsl, TimeServiceDsl}
 import esw.ocs.impl.core.SequenceOperator
 import esw.ocs.impl.internal.SequencerCommandServiceDsl
+
+import scala.compat.java8.FutureConverters.FutureOps
 
 class CswServices(
     private[ocs] val sequenceOperatorFactory: () => SequenceOperator,
@@ -24,4 +30,12 @@ class CswServices(
     with JEventServiceDsl
     with TimeServiceDsl
     with JTimeServiceDsl
-    with JCommandServiceDsl
+    with JCommandServiceDsl {
+
+  // fixme: move it to appropriate place
+  def jSubmitSequence(sequencerId: String, observingMode: String, sequence: Sequence): CompletionStage[SubmitResponse] =
+    resolveSequencer(sequencerId, observingMode).flatMap { loc =>
+      super.submitSequence(loc, sequence)
+    }.toJava
+
+}
