10806b7bf909562e9ce3c5d30510a2b67f280f6e
Pritam Kadam <kpritam@thoughtworks.com>
2019-09-14 12:26:35 +0530

    Add bgLoop

--- script-dsl/src/main/kotlin/esw/ocs/dsl/core/utils/Loop.kt
+++ script-dsl/src/main/kotlin/esw/ocs/dsl/core/utils/Loop.kt
@@ -45,15 +45,43 @@ suspend fun loop(minimumInterval: Duration, block: suspend Loop.StopIf.() -> Uni
     Loop().run { loop(minimumInterval) { block(StopIf()) } }
 }
 
+@ExperimentalTime
+fun CoroutineScope.bgLoop(block: suspend Loop.StopIf.() -> Unit) = launch {
+    Loop().run { loop { block(StopIf()) } }
+}
+
+@ExperimentalTime
+fun CoroutineScope.bgLoop(minimumInterval: Duration, block: suspend Loop.StopIf.() -> Unit) = async {
+    Loop().run { loop(minimumInterval) { block(StopIf()) } }
+}
+
 // =========== Sample Usage ===========
 @ExperimentalTime
 fun main() = runBlocking {
     var counter = 0
+    var counter2 = 0
+
+
+    bgLoop(1.seconds) {
+        counter2 += 1
+        println("[Bg Loop1] before stopIf, counter=$counter2")
+        stopIf(counter2 == 20)
+        println("[Bg Loop1] after stopIf, counter=$counter2")
+    }
 
     loop(1.seconds) {
         counter += 1
-        println("Before stopIf, counter=$counter")
+        println("[Loop1] before stopIf, counter=$counter")
         stopIf(counter == 5)
-        println("After stopIf, counter=$counter")
+        println("[Loop1] after stopIf, counter=$counter")
     }
+
+    loop(1.seconds) {
+        counter += 1
+        println("[Loop2] before stopIf, counter=$counter")
+        stopIf(counter == 15)
+        println("[Loop2] after stopIf, counter=$counter")
+    }
+
+    println("==============")
 }
