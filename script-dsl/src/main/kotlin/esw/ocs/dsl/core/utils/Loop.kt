f03419f96f171e63804893c0a3a3975ca1848759
Pritam Kadam <kpritam@thoughtworks.com>
2019-09-14 19:09:44 +0530

    Simplify loop

--- script-dsl/src/main/kotlin/esw/ocs/dsl/core/utils/Loop.kt
+++ script-dsl/src/main/kotlin/esw/ocs/dsl/core/utils/Loop.kt
@@ -1,58 +1,49 @@
 package esw.ocs.dsl.core.utils
 
 import kotlinx.coroutines.*
-import kotlin.time.*
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+import kotlin.time.milliseconds
+import kotlin.time.seconds
 
-@UseExperimental(ExperimentalTime::class)
-class Loop {
-    private val loopInterval = 50.milliseconds
+@ExperimentalTime
+private val loopInterval = 50.milliseconds
 
-    inner class StopIf {
-        suspend fun stopIf(condition: Boolean): Unit = coroutineScope {
-            suspendCancellableCoroutine<Unit> {
-                if (condition) it.cancel() else it.resumeWith(Result.success(Unit))
-            }
+@ExperimentalTime
+suspend fun loop(block: suspend LoopDsl.() -> Unit): Job = loop(loopInterval, block)
+
+@ExperimentalTime
+suspend fun loop(minimumInterval: Duration, block: suspend LoopDsl.() -> Unit): Job = coroutineScope {
+    launch {
+        loopWithoutDelay { delayedResult(maxOf(minimumInterval, loopInterval), block) }
+    }
+}
+
+@ExperimentalTime
+fun CoroutineScope.bgLoop(block: suspend LoopDsl.() -> Unit): Deferred<Job> = async { loop(loopInterval, block) }
+
+@ExperimentalTime
+fun CoroutineScope.bgLoop(minimumInterval: Duration, block: suspend LoopDsl.() -> Unit): Deferred<Job> =
+    async { loop(minimumInterval, block) }
+
+private suspend fun loopWithoutDelay(block: suspend () -> Unit) {
+    block()
+    loopWithoutDelay(block)
+}
+
+@ExperimentalTime
+private suspend fun <T> delayedResult(minDelay: Duration, block: suspend LoopDsl.() -> T): T = coroutineScope {
+    val futureValue = async { block(LoopDsl) }
+    delay(minDelay.toLongMilliseconds())
+    futureValue.await()
+}
+
+object LoopDsl {
+    suspend fun stopIf(condition: Boolean): Unit = coroutineScope {
+        suspendCancellableCoroutine<Unit> {
+            if (condition) it.cancel() else it.resumeWith(Result.success(Unit))
         }
     }
-
-    suspend fun loop(block: suspend () -> Unit): Job = loop(loopInterval, block)
-
-    suspend fun loop(minimumInterval: Duration, block: suspend () -> Unit): Job = coroutineScope {
-        launch {
-            loopWithoutDelay { delayedResult(maxOf(minimumInterval, loopInterval), block) }
-        }
-    }
-
-    private suspend fun loopWithoutDelay(block: suspend () -> Unit) {
-        block()
-        loopWithoutDelay(block)
-    }
-
-    private suspend fun <T> delayedResult(minDelay: Duration, block: suspend () -> T): T = coroutineScope {
-        val futureValue = async { block() }
-        delay(minDelay.toLongMilliseconds())
-        futureValue.await()
-    }
-}
-
-@ExperimentalTime
-suspend fun loop(block: suspend Loop.StopIf.() -> Unit) {
-    Loop().run { loop { block(StopIf()) } }
-}
-
-@ExperimentalTime
-suspend fun loop(minimumInterval: Duration, block: suspend Loop.StopIf.() -> Unit) {
-    Loop().run { loop(minimumInterval) { block(StopIf()) } }
-}
-
-@ExperimentalTime
-fun CoroutineScope.bgLoop(block: suspend Loop.StopIf.() -> Unit) = launch {
-    Loop().run { loop { block(StopIf()) } }
-}
-
-@ExperimentalTime
-fun CoroutineScope.bgLoop(minimumInterval: Duration, block: suspend Loop.StopIf.() -> Unit) = async {
-    Loop().run { loop(minimumInterval) { block(StopIf()) } }
 }
 
 // =========== Sample Usage ===========
@@ -61,11 +52,10 @@ fun main() = runBlocking {
     var counter = 0
     var counter2 = 0
 
-
     bgLoop(1.seconds) {
         counter2 += 1
         println("[Bg Loop1] before stopIf, counter=$counter2")
-        stopIf(counter2 == 20)
+        stopIf(counter2 == 10)
         println("[Bg Loop1] after stopIf, counter=$counter2")
     }
 
@@ -79,7 +69,7 @@ fun main() = runBlocking {
     loop(1.seconds) {
         counter += 1
         println("[Loop2] before stopIf, counter=$counter")
-        stopIf(counter == 15)
+        stopIf(counter == 10)
         println("[Loop2] after stopIf, counter=$counter")
     }
 
