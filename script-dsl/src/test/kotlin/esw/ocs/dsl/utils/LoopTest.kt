53c5a26f6d91a4b91c6dff71d6582eaed43bc86f
Pritam Kadam <kpritam@thoughtworks.com>
2019-09-19 12:00:17 +0530

    Use WordSpec

--- script-dsl/src/test/kotlin/esw/ocs/dsl/utils/LoopTest.kt
+++ script-dsl/src/test/kotlin/esw/ocs/dsl/utils/LoopTest.kt
@@ -3,57 +3,59 @@ package esw.ocs.dsl.utils
 import esw.ocs.dsl.core.utils.loop
 import io.kotlintest.matchers.numerics.shouldBeInRange
 import io.kotlintest.shouldBe
-import io.kotlintest.specs.StringSpec
+import io.kotlintest.specs.WordSpec
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.system.measureTimeMillis
 import kotlin.time.milliseconds
 
-class LoopTest : StringSpec({
-    "loop must run till condition becomes true when interval is default | ESW-89" {
-        val counter = AtomicInteger(0)
+class LoopTest : WordSpec({
+    "loop" should {
+        "run till condition becomes true when interval is default | ESW-89" {
+            val counter = AtomicInteger(0)
 
-        val loopTime = measureTimeMillis {
+            val loopTime = measureTimeMillis {
+                loop {
+                    counter.getAndUpdate { it + 1 }
+                    stopWhen(counter.get() == 5)
+                }
+            }
+
+            counter.get() shouldBe 5
+            // default interval is 50ms, loop should run 5 times which means it should take around 50*5=250ms
+            loopTime shouldBeInRange 250L..300L
+        }
+
+        "run till condition becomes true when interval is custom | ESW-89" {
+            val counter = AtomicInteger(0)
+
+            val loopTime = measureTimeMillis {
+                loop(300.milliseconds) {
+                    counter.getAndUpdate { it + 1 }
+                    stopWhen(counter.get() == 3)
+                }
+            }
+
+            counter.get() shouldBe 3
+            // custom loop interval is 300ms, loop should run 3 times which means it should take around 300*3=900ms
+            loopTime shouldBeInRange 900L..1000L
+        }
+
+        "allow stopWhen conditions to be specified any number of times and anywhere in the loop body | ESW-89" {
+            val counter1 = AtomicInteger(0)
+            val counter2 = AtomicInteger(0)
+
             loop {
-                counter.getAndUpdate { it + 1 }
-                stopWhen(counter.get() == 5)
+                counter1.getAndUpdate { it + 1 }
+                stopWhen(counter1.get() == 5)
+
+                counter2.getAndUpdate { it + 2 }
+                stopWhen(counter2.get() == 10)
             }
+
+            // on 5th iteration, counter1 becomes 5 and first stopWhen matches
+            // loop gets terminated there and does not execute rest of the body
+            counter1.get() shouldBe 5
+            counter2.get() shouldBe 8
         }
-
-        counter.get() shouldBe 5
-        // default interval is 50ms, loop should run 5 times which means it should take around 50*5=250ms
-        loopTime shouldBeInRange 250L..300L
-    }
-
-    "loop must run till condition becomes true when interval is custom | ESW-89" {
-        val counter = AtomicInteger(0)
-
-        val loopTime = measureTimeMillis {
-            loop(300.milliseconds) {
-                counter.getAndUpdate { it + 1 }
-                stopWhen(counter.get() == 3)
-            }
-        }
-
-        counter.get() shouldBe 3
-        // custom loop interval is 300ms, loop should run 3 times which means it should take around 300*3=900ms
-        loopTime shouldBeInRange 900L..1000L
-    }
-
-    "loop must allow stopWhen conditions to be specified any number of times and anywhere in the loop body | ESW-89" {
-        val counter1 = AtomicInteger(0)
-        val counter2 = AtomicInteger(0)
-
-        loop {
-            counter1.getAndUpdate { it + 1 }
-            stopWhen(counter1.get() == 5)
-
-            counter2.getAndUpdate { it + 2 }
-            stopWhen(counter2.get() == 10)
-        }
-
-        // on 5th iteration, counter1 becomes 5 and first stopWhen matches
-        // loop gets terminated there and does not execute rest of the body
-        counter1.get() shouldBe 5
-        counter2.get() shouldBe 8
     }
 })
