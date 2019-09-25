6d418ef1e6f241c62a5394f6d6644fdffd2ce2c4
Pritam Kadam <kpritam@thoughtworks.com>
2019-09-25 20:27:05 +0530

    Remove internal accessor from scriptDsl. Internal accessor generates method name to getScriptDsl$script_dsl when compiled to java. We need to access this variable from scala using reflection

--- script-dsl/src/main/kotlin/esw/ocs/dsl/core/Script.kt
+++ script-dsl/src/main/kotlin/esw/ocs/dsl/core/Script.kt
@@ -38,7 +38,7 @@ sealed class ScriptDslKt : CoroutineScope, CswHighLevelDsl {
     }
 
     // this needs to be lazy otherwise handlers does not get loaded properly
-    internal val scriptDsl: ScriptDsl by lazy { ScriptDslFactory.make(cswServices, strandEc()) }
+    val scriptDsl: ScriptDsl by lazy { ScriptDslFactory.make(cswServices, strandEc()) }
 
     fun initialize(block: suspend () -> Unit) = launch { block() }
 
