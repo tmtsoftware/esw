ee7e976787ffe31ed564378c5541803d3739cfd1
Pritam Kadam <kpritam@thoughtworks.com>
2019-10-01 14:10:24 +0530

    #ESW-134: example usage of reusable script in TestScript2

--- examples/src/main/kotlin/esw/ocs/scripts/examples/testData/TestScript2.kts
+++ examples/src/main/kotlin/esw/ocs/scripts/examples/testData/TestScript2.kts
@@ -2,9 +2,44 @@ package esw.ocs.scripts.examples.testData
 
 import csw.params.commands.CommandResponse.Completed
 import csw.params.commands.CommandResponse.Error
+import esw.ocs.dsl.core.reusableScript
 import esw.ocs.dsl.core.script
 import esw.ocs.dsl.utils.booleanKey
 import kotlinx.coroutines.delay
+
+// ESW-134: Reuse code by ability to import logic from one script into another
+val onlineOfflineHandlers = reusableScript {
+    handleGoOffline {
+        // do some actions to go offline
+        val param = booleanKey("offline").set(true)
+        val event = systemEvent("TCS.test", "offline").add(param)
+        publishEvent(event)
+    }
+
+    handleGoOnline {
+        // do some actions to go online
+        val param = booleanKey("online").set(true)
+        val event = systemEvent("TCS.test", "online").add(param)
+        publishEvent(event)
+    }
+}
+
+// ESW-134: Reuse code by ability to import logic from one script into another
+val operationsAndDiagModeHandlers = reusableScript {
+    handleDiagnosticMode { startTime, hint ->
+        // do some actions to go to diagnostic mode based on hint
+        diagnosticModeForSequencer(
+            "testSequencerId6", "testObservingMode6",
+            startTime,
+            hint
+        )
+    }
+
+    handleOperationsMode {
+        // do some actions to go to operations mode
+        operationsModeForSequencer("testSequencerId6", "testObservingMode6")
+    }
+}
 
 script {
     handleSetup("command-1") { command ->
@@ -29,32 +64,9 @@ script {
         addOrUpdateCommand(Error(command.runId, command.commandName().name()))
     }
 
-    handleGoOffline {
-
-        // do some actions to go offline
-        val param = booleanKey("offline").set(true)
-        val event = systemEvent("TCS.test", "offline").add(param)
-        publishEvent(event)
-    }
-
-    handleGoOnline {
-        // do some actions to go online
-        val param = booleanKey("online").set(true)
-        val event = systemEvent("TCS.test", "online").add(param)
-        publishEvent(event)
-    }
-
-    handleDiagnosticMode { startTime, hint ->
-        // do some actions to go to diagnostic mode based on hint
-        diagnosticModeForSequencer(
-            "testSequencerId6", "testObservingMode6",
-            startTime,
-            hint
-        )
-    }
-
-    handleOperationsMode {
-        // do some actions to go to operations mode
-        operationsModeForSequencer("testSequencerId6", "testObservingMode6")
-    }
+    // ESW-134: Reuse code by ability to import logic from one script into another
+    loadScripts(
+        onlineOfflineHandlers,
+        operationsAndDiagModeHandlers
+    )
 }
