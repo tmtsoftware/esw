71c078c009d5c2d144e38ea80099f3ef11c0b06f
Pritam Kadam <kpritam@thoughtworks.com>
2019-10-01 12:30:24 +0530

    Use Try and if-else condition to parse arguments instead of try-catch block

--- esw-ocs/esw-ocs-app/src/main/scala/esw/ocs/app/SequencerAppCommand.scala
+++ esw-ocs/esw-ocs-app/src/main/scala/esw/ocs/app/SequencerAppCommand.scala
@@ -4,33 +4,25 @@ import caseapp.core.Error
 import caseapp.core.argparser.SimpleArgParser
 import caseapp.{CommandName, HelpMessage}
 import csw.params.core.models.Subsystem
-import esw.ocs.impl.internal.NameValidator
 
-import scala.util.control.NonFatal
+import scala.util.Try
 
 sealed trait SequencerAppCommand
 
 object SequencerAppCommand {
 
-  implicit val subsystemParser: SimpleArgParser[Subsystem] = {
+  implicit val subsystemParser: SimpleArgParser[Subsystem] =
     SimpleArgParser.from[Subsystem]("subsystem") { subsystemStr =>
-      try Right(Subsystem.withNameInsensitive(subsystemStr))
-      catch {
-        case NonFatal(_) => Left(Error.Other(s"Subsystem [$subsystemStr] is invalid"))
-      }
+      Try(Right(Subsystem.withNameInsensitive(subsystemStr)))
+        .getOrElse(Left(Error.Other(s"Subsystem [$subsystemStr] is invalid")))
     }
-  }
 
-  implicit val stringParser: SimpleArgParser[String] = {
+  implicit val stringParser: SimpleArgParser[String] =
     SimpleArgParser.from[String](description = "string field") { str =>
-      try {
-        NameValidator.validate(str)
-        Right(str)
-      } catch {
-        case NonFatal(_) => Left(Error.Other(s"[$str] is invalid"))
-      }
+      val invalidSymbol = "@"
+      if (str.contains(invalidSymbol)) Left(Error.Other(s"[$str] is invalid"))
+      else Right(str)
     }
-  }
 
   @CommandName("seqcomp")
   final case class SequenceComponent(
--- esw-ocs/esw-ocs-impl/src/main/scala/esw/ocs/impl/internal/NameValidator.scala
+++
@@ -1,8 +0,0 @@
-package esw.ocs.impl.internal
-
-object NameValidator {
-  def validate(name: String): Unit = {
-    val invalidSymbol = "@"
-    require(!name.contains(invalidSymbol))
-  }
-}
