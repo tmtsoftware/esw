1aefc95bca5f8c3690409b1c680efadd063e8552
Saloni Vithalani <skvithalani@yahoo.com>
2019-10-04 12:30:58 +0530

    #ESW-230: Saloni: Migrate an integration test to resolve sequencer and send sequence from scala to kotlin

--- integration-test/build.gradle.kts
+++ integration-test/build.gradle.kts
@@ -4,7 +4,11 @@ tasks.withType<KotlinCompile>().all {
     kotlinOptions.freeCompilerArgs += "-Xuse-experimental=kotlin.time.ExperimentalTime"
 }
 
+val scalaVersion = "2.13"
+val akkaVersion = "2.5.25"
 dependencies {
-    testImplementation("com.github.tmtsoftware.csw:csw-testkit_2.13:65fc8ae")
+    testImplementation("com.github.tmtsoftware.csw:csw-testkit_${scalaVersion}:65fc8ae")
+    testImplementation("com.typesafe.akka:akka-testkit_${scalaVersion}:${akkaVersion}")
+    testImplementation("com.typesafe.akka:akka-actor-testkit-typed_${scalaVersion}:${akkaVersion}")
     testImplementation(project(":examples"))
 }
--- integration-test/src/test/kotlin/esw/ocs/script/ScriptIntegrationTest.kt
+++ integration-test/src/test/kotlin/esw/ocs/script/ScriptIntegrationTest.kt
@@ -1,6 +1,7 @@
 package esw.ocs.script
 
 import akka.actor.Scheduler
+import akka.actor.testkit.typed.javadsl.TestProbe
 import akka.actor.typed.*
 import akka.actor.typed.javadsl.Behaviors
 import akka.stream.typed.javadsl.ActorMaterializerFactory
@@ -15,22 +16,26 @@ import csw.location.models.AkkaRegistration
 import csw.location.models.ComponentId
 import csw.location.models.Connection
 import csw.logging.client.commons.`AkkaTypedExtension$`
-import csw.params.commands.CommandResponse
-import csw.params.commands.Sequence
+import csw.params.commands.*
+import csw.params.commands.CommandResponse.SubmitResponse
+import csw.params.core.generics.Parameter
+import csw.params.core.models.Id
+import csw.params.core.models.ObsId
 import csw.params.core.models.Prefix
 import csw.testkit.javadsl.FrameworkTestKitJunitResource
 import esw.ocs.app.wiring.SequencerWiring
-import io.kotlintest.Spec
-import io.kotlintest.TestCase
-import io.kotlintest.TestResult
+import io.kotlintest.*
 import io.kotlintest.specs.FunSpec
 import java.net.URI
 import java.time.Duration
+import java.util.*
 import java.util.concurrent.TimeUnit
 import scala.Option
+import scala.collection.immutable.Set
 import scala.concurrent.Await
 import scala.concurrent.Future
 import scala.concurrent.duration.FiniteDuration
+import scala.jdk.javaapi.CollectionConverters
 import scala.util.Either
 
 class ScriptIntegrationTest : FunSpec() {
@@ -68,6 +73,9 @@ class ScriptIntegrationTest : FunSpec() {
     private fun <T> ActorSystem<SpawnProtocol>.spawn(behavior: Behavior<T>, name: String, props: Props): ActorRef<T> =
         `AkkaTypedExtension$`.`MODULE$`.UserActorFactory(this).spawn(behavior, name, props)
 
+    private fun <T> List<T>.toScala() = CollectionConverters.asScala<T>(this).toSeq()
+    private fun <T> kotlin.collections.Set<T>.toScala() = CollectionConverters.asScala<T>(this).toSet<T>()
+
     private fun beh(): Behavior<SequencerMsg> = Behaviors.receiveMessage<SequencerMsg> {
         when (it) {
             is SubmitSequenceAndWait -> {
@@ -97,5 +105,34 @@ class ScriptIntegrationTest : FunSpec() {
     }
 
     init {
+        test("CswServices must be able to send sequence to other Sequencer by resolving location through TestScript | ESW-195, ESW-119") {
+            val obsId = ObsId("obsId")
+            val command = Setup(Prefix("TCS.test"), CommandName("command-4"), Optional.of(obsId))
+            val submitResponseProbe = TestProbe.create<SubmitResponse>(actorSystem)
+            val sequenceId = Id.apply()
+            val sequence = Sequence(sequenceId, listOf(command).toScala())
+
+            ocsSequencer.tell(SubmitSequenceAndWait(sequence, submitResponseProbe.ref))
+
+            // This has to match with sequence created in TestScript -> handleSetupCommand("command-4")
+            val assertableCommand =
+                Setup(
+                    Id("testCommandIdString123"),
+                    Prefix("TCS.test"),
+                    CommandName("command-to-assert-on"),
+                    Option.empty(),
+                    setOf<Parameter<*>>().toScala()
+                )
+
+            val assertableSequence = Sequence(Id("testSequenceIdString123"), listOf(assertableCommand).toScala())
+
+            // response received by irisSequencer
+            submitResponseProbe.expectMessage(CommandResponse.Completed(sequenceId))
+
+            // sequence sent to tcsSequencer by irisSequencer script
+            eventually(Duration.ofSeconds(10)) {
+                sequenceReceivedByTCSProbe shouldBe assertableSequence
+            }
+        }
     }
 }
