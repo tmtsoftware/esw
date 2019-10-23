c7cfe8311c43e14b655dcf7a340cf3801ac043ec
kpritam <kpritam@thoughtworks.com>
2019-10-23 11:56:11 +0530

    #ESW-125: Pritam: Uncomment AlarmServiceDslTest

--- esw-ocs/esw-ocs-dsl-kt/src/main/kotlin/esw/ocs/dsl/highlevel/AlarmServiceDsl.kt
+++ esw-ocs/esw-ocs-dsl-kt/src/main/kotlin/esw/ocs/dsl/highlevel/AlarmServiceDsl.kt
@@ -1,8 +1,24 @@
 package esw.ocs.dsl.highlevel
 
+import csw.alarm.api.javadsl.IAlarmService
 import csw.alarm.models.AlarmSeverity
 import csw.alarm.models.Key.AlarmKey
+import kotlinx.coroutines.CoroutineScope
+import kotlin.time.seconds
 
 interface AlarmServiceDsl {
-    fun setSeverity(alarmKey: AlarmKey, _severity: AlarmSeverity)
+    fun setSeverity(alarmKey: AlarmKey, severity: AlarmSeverity)
+}
+
+class AlarmServiceDslImpl(private val alarmService: IAlarmService, override val coroutineScope: CoroutineScope) : AlarmServiceDsl, LoopDsl {
+    private val map: HashMap<AlarmKey, AlarmSeverity> = HashMap()
+
+    private fun startSetSeverity() = bgLoop(5.seconds) {
+        map.keys.forEach { key -> alarmService.setSeverity(key, map[key]) }
+    }
+
+    override fun setSeverity(alarmKey: AlarmKey, severity: AlarmSeverity) {
+        if (map.size == 0) startSetSeverity()
+        map[alarmKey] = severity
+    }
 }
--- esw-ocs/esw-ocs-dsl-kt/src/main/kotlin/esw/ocs/dsl/highlevel/CswHighLevelDsl.kt
+++ esw-ocs/esw-ocs-dsl-kt/src/main/kotlin/esw/ocs/dsl/highlevel/CswHighLevelDsl.kt
@@ -2,7 +2,7 @@ package esw.ocs.dsl.highlevel
 
 import akka.actor.typed.ActorSystem
 import csw.alarm.models.AlarmSeverity
-import csw.alarm.models.Key
+import csw.alarm.models.Key.AlarmKey
 import csw.command.client.CommandResponseManager
 import csw.config.api.javadsl.IConfigClientService
 import csw.event.api.javadsl.IEventPublisher
@@ -14,7 +14,6 @@ import esw.ocs.dsl.script.StrandEc
 import esw.ocs.dsl.script.utils.LockUnlockUtil
 import esw.ocs.dsl.sequence_manager.LocationServiceUtil
 import kotlinx.coroutines.CoroutineScope
-import kotlin.time.seconds
 
 abstract class CswHighLevelDsl(private val cswServices: CswServices) : EventServiceDsl, TimeServiceDsl, CommandServiceDsl, CrmDsl, DiagnosticDsl,
     LockUnlockDsl, OnlineOfflineDsl, AbortSequenceDsl, StopDsl, ConfigServiceDsl,
@@ -31,24 +30,11 @@ abstract class CswHighLevelDsl(private val cswServices: CswServices) : EventServ
     final override val commonUtils: CommonUtils = CommonUtils(cswServices.sequencerAdminFactory(), LocationServiceUtil(locationService.asScala(), actorSystem))
     final override val lockUnlockUtil: LockUnlockUtil = cswServices.lockUnlockUtil()
 
-    final override val timeServiceScheduler: TimeServiceScheduler by lazy {
-        cswServices.timeServiceSchedulerFactory().make(strandEc.ec())
-    }
+    final override val timeServiceScheduler: TimeServiceScheduler by lazy { cswServices.timeServiceSchedulerFactory().make(strandEc.ec()) }
+    final override val configClient: IConfigClientService by lazy { cswServices.configClientService() }
 
-    final override val configClient: IConfigClientService by lazy {
-        cswServices.configClientService()
-    }
+    /***** AlarmServiceDSl impl *****/
+    private val alarmServiceDslImpl by lazy { AlarmServiceDslImpl(cswServices.alarmService(), coroutineScope) }
 
-    // fixme: move to appropriate place (moved it here b'cuse it used bgLoop construct which needs top level coroutine scope)
-    /******* alarm service dsl impl *********/
-    private val map: HashMap<Key.AlarmKey, AlarmSeverity> = HashMap()
-
-    private fun startSetSeverity() = bgLoop(5.seconds) {
-        map.keys.forEach { key -> cswServices.alarmService().setSeverity(key, map[key]) }
-    }
-
-    override fun setSeverity(alarmKey: Key.AlarmKey, _severity: AlarmSeverity) {
-        if (map.size == 0) startSetSeverity()
-        map[alarmKey] = _severity
-    }
+    override fun setSeverity(alarmKey: AlarmKey, severity: AlarmSeverity) = alarmServiceDslImpl.setSeverity(alarmKey, severity)
 }
