a9eaa741cc2bd44695c7df6412ac80c3aba78400
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-11-04 12:50:00 +0530

    #ESW-241: Bilal | Puneet : Uncommented tests

--- examples/src/main/kotlin/esw/ocs/scripts/examples/script_based/IrisDarkNight.kts
+++ examples/src/main/kotlin/esw/ocs/scripts/examples/script_based/IrisDarkNight.kts
@@ -1,49 +1,49 @@
-//package esw.ocs.scripts.examples.script_based
-//
-//import csw.params.commands.CommandResponse
-//import esw.ocs.dsl.core.script
-//import kotlin.time.seconds
-//import kotlinx.coroutines.future.await
-//
-//script { _ ->
-//
-//    val publishStream = publishEvent(10.seconds) {
-//        systemEvent("iris.test", "system")
-//    }
-//
-//    val subscriptionStream = onEvent("iris.test.system") {
-//        log("------------------> received-event for iris on key: ${it.eventKey()}")
-//    }
-//
-//    handleSetup("setup-iris") { command ->
-//        log("[Iris] Received command: ${command.commandName()}")
-//
-//        val command1 = setup("esw.test-commandA1", "commandA1", "test-obsId")
-//        val command2 = setup("esw.test-commandA2", "commandA2", "test-obsId")
-//
-//        val maybeCommandB = nextIf { it.commandName().name() == "setup-iris" }
-//
-//        maybeCommandB?.let { commandB ->
-//            val commandB1 = setup("esw.test-commandB1", "setup-iris", "test-obsId")
-//            val commandB2 = setup("esw.test-commandB2", "setup-iris", "test-obsId")
-//
-//            val assemblyResponse3 = submitCommandToAssembly("Sample1Assembly", commandB1)
-//            if (assemblyResponse3 is CommandResponse.Error) finishWithError(assemblyResponse3.message())
-//
-//            val assemblyResponse4 = submitCommandToAssembly("Sample1Assembly", commandB2)
-//            if (assemblyResponse4 is CommandResponse.Error) finishWithError(assemblyResponse4.message())
-//        }
-//
-//        val assemblyResponse1 = submitCommandToAssembly("Sample1Assembly", command1)
-//        if (assemblyResponse1 is CommandResponse.Error) finishWithError(assemblyResponse1.message())
-//
-//        val assemblyResponse2 = submitCommandToAssembly("Sample1Assembly", command2)
-//        if (assemblyResponse2 is CommandResponse.Error) finishWithError(assemblyResponse2.message())
-//    }
-//
-//    handleShutdown {
-//        publishStream.cancel()
-//        subscriptionStream.unsubscribe().await()
-//        log("shutdown iris")
-//    }
-//}
+package esw.ocs.scripts.examples.script_based
+
+import csw.params.commands.CommandResponse
+import esw.ocs.dsl.core.script
+import kotlin.time.seconds
+import kotlinx.coroutines.future.await
+
+script { _ ->
+
+    val publishStream = publishEvent(10.seconds) {
+        systemEvent("iris.test", "system")
+    }
+
+    val subscriptionStream = onEvent("iris.test.system") {
+        log("------------------> received-event for iris on key: ${it.eventKey()}")
+    }
+
+    handleSetup("setup-iris") { command ->
+        log("[Iris] Received command: ${command.commandName()}")
+
+        val command1 = setup("esw.test-commandA1", "commandA1", "test-obsId")
+        val command2 = setup("esw.test-commandA2", "commandA2", "test-obsId")
+
+        val maybeCommandB = nextIf { it.commandName().name() == "setup-iris" }
+
+        maybeCommandB?.let { commandB ->
+            val commandB1 = setup("esw.test-commandB1", "setup-iris", "test-obsId")
+            val commandB2 = setup("esw.test-commandB2", "setup-iris", "test-obsId")
+
+            val assemblyResponse3 = submitCommandToAssembly("Sample1Assembly", commandB1)
+            if (assemblyResponse3 is CommandResponse.Error) finishWithError(assemblyResponse3.message())
+
+            val assemblyResponse4 = submitCommandToAssembly("Sample1Assembly", commandB2)
+            if (assemblyResponse4 is CommandResponse.Error) finishWithError(assemblyResponse4.message())
+        }
+
+        val assemblyResponse1 = submitCommandToAssembly("Sample1Assembly", command1)
+        if (assemblyResponse1 is CommandResponse.Error) finishWithError(assemblyResponse1.message())
+
+        val assemblyResponse2 = submitCommandToAssembly("Sample1Assembly", command2)
+        if (assemblyResponse2 is CommandResponse.Error) finishWithError(assemblyResponse2.message())
+    }
+
+    handleShutdown {
+        publishStream.cancel()
+        subscriptionStream.unsubscribe().await()
+        log("shutdown iris")
+    }
+}
