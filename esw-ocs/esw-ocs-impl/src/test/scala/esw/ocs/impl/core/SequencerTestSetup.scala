e1d3f8488471e042786668519b09dd75a94a4527
Pritam Kadam <kpritam@thoughtworks.com>
2019-11-06 14:54:27 +0530

    Fix macro expansion failure

--- esw-ocs/esw-ocs-impl/src/test/scala/esw/ocs/impl/core/SequencerTestSetup.scala
+++ esw-ocs/esw-ocs-impl/src/test/scala/esw/ocs/impl/core/SequencerTestSetup.scala
@@ -167,7 +167,7 @@ class SequencerTestSetup(sequence: Sequence)(implicit system: ActorSystem[_]) {
     expectedState match {
       case Idle                            => stepList shouldNot be(None)
       case InProgress                      => stepList shouldNot be(None)
-      case x: SequencerState[SequencerMsg] => assert(condition = false, s"$x is not valid state after AbortSequence")
+      case x: SequencerState[SequencerMsg] => assert(false, s"$x is not valid state after AbortSequence")
     }
     eventually(verify(script).executeStop())
     probe
