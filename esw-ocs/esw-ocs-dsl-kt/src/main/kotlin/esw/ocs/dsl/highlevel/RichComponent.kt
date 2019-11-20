6cefaff1b79f7a60a984feb67ae27b03cf0d3062
Shubham Chauhan <shubham25may1998@gmail.com>
2019-11-20 15:44:52 +0530

    #ESW-121: Chauhan : Fix bug in api

--- esw-ocs/esw-ocs-dsl-kt/src/main/kotlin/esw/ocs/dsl/highlevel/RichComponent.kt
+++ esw-ocs/esw-ocs-dsl-kt/src/main/kotlin/esw/ocs/dsl/highlevel/RichComponent.kt
@@ -15,6 +15,7 @@ import csw.location.models.ComponentType
 import csw.params.commands.CommandResponse.*
 import csw.params.commands.ControlCommand
 import csw.params.core.models.Prefix
+import csw.params.core.states.CurrentState
 import csw.params.core.states.StateName
 import csw.time.core.models.UTCTime
 import esw.ocs.dsl.Timeouts
@@ -41,8 +42,8 @@ class RichComponent(
     suspend fun oneway(command: ControlCommand): OnewayResponse = commandService().oneway(command, timeout).await()
     suspend fun submit(command: ControlCommand): SubmitResponse = commandService().submit(command, timeout).await()
     suspend fun submitAndWait(command: ControlCommand): SubmitResponse = commandService().submitAndWait(command, timeout).await()
-    suspend fun subscribeCurrentState(stateNames: Set<StateName>, callback: suspend CoroutineScope.() -> Unit): CurrentStateSubscription =
-            commandService().subscribeCurrentState(stateNames) { callback.toJava() }
+    suspend fun subscribeCurrentState(stateNames: Set<StateName>, callback: suspend CoroutineScope.(CurrentState) -> Unit): CurrentStateSubscription =
+            commandService().subscribeCurrentState(stateNames) { callback.toJava(it) }
 
     suspend fun diagnosticMode(startTime: UTCTime, hint: String): Unit = componentRef().tell(DiagnosticDataMessage.DiagnosticMode(startTime, hint))
     suspend fun operationsMode(): Unit = componentRef().tell(DiagnosticDataMessage.`OperationsMode$`.`MODULE$`)
