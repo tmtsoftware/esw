c5cb8732c3b3a38ae9f149e179e692371ebfdb8c
Saloni Vithalani <skvithalani@yahoo.com>
2019-11-22 17:45:11 +0530

    Introduce compileAll command to include scalafmtCheck, scalastyleCheck, test:compile, multi-jvm:compile excluding makesite

--- project/Settings.scala
+++ project/Settings.scala
@@ -4,7 +4,11 @@ object Settings {
   def addAliases: Seq[Setting[_]] = {
     addCommandAlias(
       "buildAll",
-      ";set every enableFatalWarnings := true; scalafmtCheck; clean; makeSite; test:compile; set every enableFatalWarnings := false;"
+      ";set every enableFatalWarnings := true; scalafmtCheck; clean; makeSite; test:compile; set every enableFatalWarnings := false"
+    ) ++
+    addCommandAlias(
+      "compileAll",
+      ";set every enableFatalWarnings := true; scalafmtCheck; scalastyle; test:compile; multi-jvm:compile; set every enableFatalWarnings := false;"
     )
   }
 }
