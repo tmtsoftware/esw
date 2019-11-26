d6279eaf58e5905315710b06e022a1bcb03f16c0
Pritam Kadam <kpritam@thoughtworks.com>
2019-11-26 16:22:28 +0530

    #ESW-142: Shubham| Pritam : Make currentScript optional in FSMScriptDsl and throw exception when handler is not present in the map for provided state
    Co-authored-by: Shubham Jaybhaye <34642890+shubhamJay@users.noreply.github.com>

--- esw-ocs/esw-ocs-dsl/src/main/scala/esw/ocs/dsl/script/FSMScriptDsl.scala
+++ esw-ocs/esw-ocs-dsl/src/main/scala/esw/ocs/dsl/script/FSMScriptDsl.scala
@@ -10,40 +10,42 @@ import csw.time.core.models.UTCTime
 import scala.concurrent.Future
 
 private[esw] class FSMScriptDsl(override val csw: CswServices, val strandEc: StrandEc) extends ScriptDsl(csw, strandEc) {
-  protected var currentState: String = "UN_INITIALIZED"
-  // fixme : should not be null.
-  protected var currentStateDsl: ScriptDsl = _
-  protected var stateMap                   = Map.empty[String, Supplier[ScriptDsl]]
+  protected var currentState                          = "UN_INITIALIZED"
+  protected var maybeCurrentScript: Option[ScriptDsl] = None
+  protected var stateMap                              = Map.empty[String, Supplier[ScriptDsl]]
 
   def become(nextState: String): Unit =
     if (currentState != nextState) {
-      currentStateDsl = stateMap(nextState).get()
+      maybeCurrentScript = Some(getScript(nextState))
       currentState = nextState
     }
 
   def add(state: String, script: Supplier[ScriptDsl]): Unit = stateMap += (state -> script)
 
-  override def execute(command: SequenceCommand): Future[Unit] = stateMap.get(currentState) match {
-    case Some(_) => currentStateDsl.execute(command)
-    case None    => Future.failed(new RuntimeException(s"Invalid state = $currentState"))
-  }
+  override def execute(command: SequenceCommand): Future[Unit] = currentScript.execute(command)
 
-  override def executeGoOnline(): Future[Done] = currentStateDsl.executeGoOnline().flatMap(_ => super.executeGoOnline())
+  override def executeGoOnline(): Future[Done] = currentScript.executeGoOnline().flatMap(_ => super.executeGoOnline())
 
-  override def executeGoOffline(): Future[Done] = currentStateDsl.executeGoOffline().flatMap(_ => super.executeGoOffline())
+  override def executeGoOffline(): Future[Done] = currentScript.executeGoOffline().flatMap(_ => super.executeGoOffline())
 
-  override def executeShutdown(): Future[Done] = currentStateDsl.executeShutdown().flatMap(_ => super.executeShutdown())
+  override def executeShutdown(): Future[Done] = currentScript.executeShutdown().flatMap(_ => super.executeShutdown())
 
-  override def executeAbort(): Future[Done] = currentStateDsl.executeAbort().flatMap(_ => super.executeAbort())
+  override def executeAbort(): Future[Done] = currentScript.executeAbort().flatMap(_ => super.executeAbort())
 
-  override def executeStop(): Future[Done] = currentStateDsl.executeStop().flatMap(_ => super.executeStop())
+  override def executeStop(): Future[Done] = currentScript.executeStop().flatMap(_ => super.executeStop())
 
   override def executeDiagnosticMode(startTime: UTCTime, hint: String): Future[Done] =
-    currentStateDsl.executeDiagnosticMode(startTime, hint).flatMap(_ => super.executeDiagnosticMode(startTime, hint))
+    currentScript.executeDiagnosticMode(startTime, hint).flatMap(_ => super.executeDiagnosticMode(startTime, hint))
 
   override def executeOperationsMode(): Future[Done] =
-    currentStateDsl.executeOperationsMode().flatMap(_ => super.executeOperationsMode())
+    currentScript.executeOperationsMode().flatMap(_ => super.executeOperationsMode())
 
   override def executeExceptionHandlers(ex: Throwable): CompletionStage[Void] =
-    currentStateDsl.executeExceptionHandlers(ex).thenAccept(_ => super.executeExceptionHandlers(ex))
+    currentScript.executeExceptionHandlers(ex).thenAccept(_ => super.executeExceptionHandlers(ex))
+
+  private def currentScript =
+    maybeCurrentScript.getOrElse(throw new RuntimeException("Current script handler is not initialized"))
+
+  private def getScript(state: String) =
+    stateMap.getOrElse(state, throw new RuntimeException(s"No command handlers found for state: $state")).get()
 }
