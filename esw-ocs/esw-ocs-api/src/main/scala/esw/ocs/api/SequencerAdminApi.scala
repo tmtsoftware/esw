f4896e8cd55115513cd72b51a3559a21c7e7f1fa
Dollyg <dollyg@thoughtworks.com>
2019-11-27 10:29:06 +0530

    #000 : Dolly : Remove redundant SequencerCommandApi and add additional apis to AdminApi

--- esw-ocs/esw-ocs-api/src/main/scala/esw/ocs/api/SequencerAdminApi.scala
+++ esw-ocs/esw-ocs-api/src/main/scala/esw/ocs/api/SequencerAdminApi.scala
@@ -1,3 +1,18 @@
 package esw.ocs.api
 
-trait SequencerAdminApi extends SequencerEditorApi with SequencerCommandApi
+import csw.command.api.scaladsl.SequencerCommandService
+import csw.params.commands.CommandResponse.SubmitResponse
+import csw.params.commands.Sequence
+import csw.time.core.models.UTCTime
+import esw.ocs.api.protocol._
+
+import scala.concurrent.Future
+
+trait SequencerAdminApi extends SequencerEditorApi with SequencerCommandService {
+  def loadSequence(sequence: Sequence): Future[OkOrUnhandledResponse]
+  def startSequence(): Future[SubmitResponse]
+  def goOnline(): Future[GoOnlineResponse]
+  def goOffline(): Future[GoOfflineResponse]
+  def diagnosticMode(startTime: UTCTime, hint: String): Future[DiagnosticModeResponse]
+  def operationsMode(): Future[OperationsModeResponse]
+}
