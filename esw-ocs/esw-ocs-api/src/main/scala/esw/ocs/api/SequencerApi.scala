18327a9fb02f883504f1fb987e7de234dc55cccd
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-11-28 10:28:32 +0530

    re-arrange api methods and messages in consistent and logical order

--- esw-ocs/esw-ocs-api/src/main/scala/esw/ocs/api/SequencerApi.scala
+++ esw-ocs/esw-ocs-api/src/main/scala/esw/ocs/api/SequencerApi.scala
@@ -12,28 +12,29 @@ import scala.concurrent.Future
 
 trait SequencerApi extends SequencerCommandService {
 
-  //Editor API
+  def loadSequence(sequence: Sequence): Future[OkOrUnhandledResponse]
+  def startSequence(): Future[SubmitResponse]
+
   def getSequence: Future[Option[StepList]]
-  def isAvailable: Future[Boolean]
-  def isOnline: Future[Boolean]
   def add(commands: List[SequenceCommand]): Future[OkOrUnhandledResponse]
   def prepend(commands: List[SequenceCommand]): Future[OkOrUnhandledResponse]
   def replace(id: Id, commands: List[SequenceCommand]): Future[GenericResponse]
   def insertAfter(id: Id, commands: List[SequenceCommand]): Future[GenericResponse]
   def delete(id: Id): Future[GenericResponse]
-  def pause: Future[PauseResponse]
-  def resume: Future[OkOrUnhandledResponse]
   def addBreakpoint(id: Id): Future[GenericResponse]
   def removeBreakpoint(id: Id): Future[RemoveBreakpointResponse]
   def reset(): Future[OkOrUnhandledResponse]
-  def abortSequence(): Future[OkOrUnhandledResponse]
-  def stop(): Future[OkOrUnhandledResponse]
-  def loadSequence(sequence: Sequence): Future[OkOrUnhandledResponse]
-  def startSequence(): Future[SubmitResponse]
+  def pause: Future[PauseResponse]
+  def resume: Future[OkOrUnhandledResponse]
 
-  //Lifecycle API
+  def isAvailable: Future[Boolean]
+  def isOnline: Future[Boolean]
   def goOnline(): Future[GoOnlineResponse]
   def goOffline(): Future[GoOfflineResponse]
+  def abortSequence(): Future[OkOrUnhandledResponse]
+  def stop(): Future[OkOrUnhandledResponse]
+
   def diagnosticMode(startTime: UTCTime, hint: String): Future[DiagnosticModeResponse]
   def operationsMode(): Future[OperationsModeResponse]
+
 }
--- esw-ocs/esw-ocs-api/src/main/scala/esw/ocs/api/protocol/SequencerWebsocketRequest.scala
+++ esw-ocs/esw-ocs-api/src/main/scala/esw/ocs/api/protocol/SequencerWebsocketRequest.scala
@@ -6,5 +6,6 @@ import csw.params.core.models.Id
 sealed trait SequencerWebsocketRequest
 
 private[ocs] object SequencerWebsocketRequest {
+  // Sequencer Command Protocol
   case class QueryFinal(runId: Id, timeout: Timeout) extends SequencerWebsocketRequest
 }
