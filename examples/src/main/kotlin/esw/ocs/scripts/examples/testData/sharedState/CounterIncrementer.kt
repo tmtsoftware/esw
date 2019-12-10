caf24b53beb4605ccacfb0959544d095544757e7
Pritam Kadam <kpritam@thoughtworks.com>
2019-12-10 11:34:06 +0530

    #ESW-183: Pritam: Update ThreadSafeScript to show usage of shared mutable state between main and reusable script

---
+++ examples/src/main/kotlin/esw/ocs/scripts/examples/testData/sharedState/CounterIncrementer.kt
@@ -0,0 +1,16 @@
+package esw.ocs.scripts.examples.testData.sharedState
+
+import esw.ocs.dsl.core.reusableScript
+import kotlinx.coroutines.launch
+
+val counterIncrementer = reusableScript {
+    // keep incrementing counter 100_000 times in the background while processing commands
+    val job = coroutineScope.launch {
+        repeat(100_000) { counter++ }
+    }
+
+    onSetup("increment") {
+        repeat(100_000) { counter++ }
+        job.join()
+    }
+}
--- examples/src/main/kotlin/esw/ocs/scripts/examples/testData/sharedState/ThreadSafeScript.kts
+++ examples/src/main/kotlin/esw/ocs/scripts/examples/testData/sharedState/ThreadSafeScript.kts
@@ -1,4 +1,4 @@
-package esw.ocs.scripts.examples.testData
+package esw.ocs.scripts.examples.testData.sharedState
 
 import esw.ocs.dsl.core.script
 import esw.ocs.dsl.params.intKey
@@ -6,16 +6,12 @@ import esw.ocs.dsl.params.set
 import kotlinx.coroutines.launch
 
 script {
-    var counter = 0
-
     // keep incrementing counter 100_000 times in the background while processing commands
     val job = coroutineScope.launch {
         repeat(100_000) { counter++ }
     }
 
-    onSetup("increment") {
-        repeat(100_000) { counter++ }
-    }
+    loadScripts(counterIncrementer)
 
     onObserve("get-counter") {
         job.join()
