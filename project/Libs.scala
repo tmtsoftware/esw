07649b79e209559fc7d27adeedc4e73a666784bd
Scala Steward <me@scala-steward.org>
2019-12-15 13:50:43 +0100

    Update enumeratum to 1.5.14

--- project/Libs.scala
+++ project/Libs.scala
@@ -10,7 +10,7 @@ object Libs {
   val `silencer-lib`    = "com.github.ghik" % "silencer-lib"    % SilencerVersion cross CrossVersion.full
 
   val `case-app`           = "com.github.alexarchambault" %% "case-app" % "2.0.0-M9"
-  val enumeratum           = dep("com.beachape" %%% "enumeratum" % "1.5.13") //MIT License
+  val enumeratum           = dep("com.beachape" %%% "enumeratum" % "1.5.14") //MIT License
   val `mockito-scala`      = "org.mockito" %% "mockito-scala" % "1.7.1" // MIT License
   val `scala-async`        = "org.scala-lang.modules" %% "scala-async" % "0.10.0" //BSD 3-clause "New" or "Revised" License
   val scalatest            = "org.scalatest" %% "scalatest" % "3.0.8" //Apache License 2.0
