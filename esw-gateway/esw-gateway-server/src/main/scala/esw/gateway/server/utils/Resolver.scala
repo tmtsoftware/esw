83d8cd3e6844b017be5dc87937ffb2c15c81fd76
Poorva Gokhale <poorvasgokhale@gmail.com>
2019-12-18 16:07:54 +0530

    #ESW-258 : Poorva : Add ability to resolve HTTPLocation is Akka location is not found for component and sequencer

--- esw-gateway/esw-gateway-server/src/main/scala/esw/gateway/server/utils/Resolver.scala
+++ esw-gateway/esw-gateway-server/src/main/scala/esw/gateway/server/utils/Resolver.scala
@@ -4,13 +4,12 @@ import akka.actor.typed.ActorSystem
 import akka.util.Timeout
 import csw.command.api.scaladsl.CommandService
 import csw.command.client.CommandServiceFactory
-import csw.command.client.extensions.AkkaLocationExt.RichAkkaLocation
 import csw.location.api.scaladsl.LocationService
-import csw.location.models.ComponentId
-import csw.location.models.Connection.AkkaConnection
+import csw.location.models.Connection.{AkkaConnection, HttpConnection}
+import csw.location.models.{ComponentId, Location}
 import esw.gateway.api.protocol.InvalidComponent
 import esw.ocs.api.SequencerApi
-import esw.ocs.impl.SequencerActorProxy
+import esw.ocs.impl.SequencerApiFactory
 
 import scala.concurrent.Future
 import scala.concurrent.duration.DurationLong
@@ -20,15 +19,20 @@ class Resolver(locationService: LocationService)(implicit typedSystem: ActorSyst
   import typedSystem.executionContext
   private implicit val timeout: Timeout = 5.seconds
 
-  def resolveComponent(componentId: ComponentId): Future[CommandService] =
+  private def resolveLocation(componentId: ComponentId): Future[Location] =
     locationService
       .resolve(AkkaConnection(componentId), timeout.duration)
-      .map(_.getOrElse(throw InvalidComponent(s"No component is registered with id $componentId")))
-      .map(CommandServiceFactory.make)
+      .flatMap {
+        case Some(akkaLocation) => Future.successful(akkaLocation)
+        case None =>
+          locationService
+            .resolve(HttpConnection(componentId), timeout.duration)
+            .map(_.getOrElse(throw InvalidComponent(s"No component is registered with id $componentId")))
+      }
+
+  def resolveComponent(componentId: ComponentId): Future[CommandService] =
+    resolveLocation(componentId).map(CommandServiceFactory.make)
 
   def resolveSequencer(componentId: ComponentId): Future[SequencerApi] =
-    locationService
-      .resolve(AkkaConnection(componentId), timeout.duration)
-      .map(_.getOrElse(throw InvalidComponent(s"No sequencer is registered with id $componentId")))
-      .map(loc => new SequencerActorProxy(loc.sequencerRef))
+    resolveLocation(componentId).map(SequencerApiFactory.make)
 }
