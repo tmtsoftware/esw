46089497aae946b344c7a18ada15649939b3f43f
Dollyg <dollyg@thoughtworks.com>
2019-12-18 17:36:17 +0530

    #ESW-237 : Bilal | Dolly : Small refactorings
    Co-authored-by: Bilal Fazlani <bilal.m.fazlani@gmail.com>

--- agent/src/main/scala/agent/AgentActor.scala
+++ agent/src/main/scala/agent/AgentActor.scala
@@ -2,49 +2,45 @@ package agent
 
 import java.io.InputStream
 
-import akka.NotUsed
+import akka.Done
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorSystem, Behavior}
-import akka.stream.IOResult
 import akka.stream.scaladsl.{Framing, Keep, Source, StreamConverters}
 import akka.util.ByteString
 import csw.prefix.models.Subsystem
 
 import scala.concurrent.Future
 
-sealed trait AgentMsg
-case class SpawnSequenceComponent(subsystem: Subsystem, compName: String) extends AgentMsg {
-  def executablePath = "/Users/dollygyanchandani/Projects/tmt/esw/target/universal/stage/bin/esw-ocs-app"
+sealed trait AgentCommand {
+  val strings: List[String]
+}
+
+case class SpawnSequenceComponent(subsystem: Subsystem, name: String) extends AgentCommand {
+  private val executablePath = "/Users/dollygyanchandani/Projects/tmt/esw/target/universal/stage/bin/esw-ocs-app"
+  override val strings       = List(executablePath, "seqcomp", "-s", subsystem.toString, "-n", name)
 }
 
 object AgentActor {
 
-  def runCommand(command: String*)(implicit system: ActorSystem[_]): Unit = {
-    val processBuilder = new ProcessBuilder(command: _*)
-    val process = processBuilder.start()
-    println("PID=" + process.pid())
-
-    def printStreams(is: (() => InputStream)*) = {
-      val value: Source[ByteString, NotUsed] =
-        is.foldLeft(Source.empty[ByteString])((a, b) => {
-          val value1: Source[ByteString, Future[IOResult]] = StreamConverters.fromInputStream(b)
-          a.mergeMat(value1)(Keep.left)
-        })
-      value
-        .via(Framing.delimiter(ByteString("\n"), 256, allowTruncation = true))
-        .map(_.utf8String)
-        .runForeach(println)
-    }
-
-    printStreams(process.getInputStream, process.getErrorStream)
+  def behavior: Behavior[AgentCommand] = Behaviors.receive { (ctx, command) =>
+    import ctx.system
+    runCommand(command)(system)
+    Behaviors.same
   }
 
-  def behavior: Behavior[AgentMsg] = Behaviors.receive { (ctx, msg) =>
-    import ctx.system
-    msg match {
-      case c @ SpawnSequenceComponent(ss, cN) =>
-        runCommand(c.executablePath, "seqcomp", "-s", ss.toString, "-n", cN)(system)
-        Behaviors.same
-    }
+  private def printStreams(inputStreams: (() => InputStream)*)(implicit system: ActorSystem[_]): Future[Done] =
+    inputStreams
+      .foldLeft(Source.empty[ByteString])((source, inputStream) =>
+        source.mergeMat(StreamConverters.fromInputStream(inputStream))(Keep.left)
+      )
+      .via(Framing.delimiter(ByteString("\n"), 256, allowTruncation = true))
+      .map(_.utf8String)
+      .runForeach(println)
+
+  private def runCommand(agentCommand: AgentCommand)(implicit system: ActorSystem[_]): Unit = {
+    val processBuilder = new ProcessBuilder(agentCommand.strings: _*)
+    val process        = processBuilder.start()
+    println("PID=" + process.pid())
+    printStreams(process.getInputStream, process.getErrorStream)
   }
 }
--- agent/src/main/scala/agent/Main.scala
+++ agent/src/main/scala/agent/Main.scala
@@ -2,21 +2,21 @@ package agent
 
 import akka.actor.typed.SpawnProtocol.Spawn
 import akka.actor.typed.scaladsl.AskPattern.Askable
-import akka.actor.typed.{ActorRef, ActorSystem, Props, Scheduler, SpawnProtocol}
+import akka.actor.typed._
 import akka.util.Timeout
 import csw.location.client.ActorSystemFactory
 import csw.prefix.models.Subsystem
 
-import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
 
 object Main extends App {
 
   lazy val actorSystem: ActorSystem[SpawnProtocol.Command] = ActorSystemFactory.remote(SpawnProtocol(), "esw-system")
 
-  implicit val timeout: Timeout     = Timeout(10.seconds)
-  implicit val scheduler: Scheduler = actorSystem.scheduler
-  val d: Future[ActorRef[AgentMsg]] = actorSystem ? (Spawn(AgentActor.behavior, "agent-actor", Props.empty, _))
+  implicit val timeout: Timeout         = Timeout(10.seconds)
+  implicit val scheduler: Scheduler     = actorSystem.scheduler
+  val d: Future[ActorRef[AgentCommand]] = actorSystem ? (Spawn(AgentActor.behavior, "agent-actor", Props.empty, _))
 
   Await.result(d, 5.seconds) ! SpawnSequenceComponent(Subsystem.ESW, "primary")
 }
