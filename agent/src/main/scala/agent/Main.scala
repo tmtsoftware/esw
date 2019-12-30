c3f022103bec89ae5dd65210b0561ce563c643c5
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-12-30 19:57:27 +0530

    #ESW-237: remove unused imports

--- agent/src/main/scala/agent/Main.scala
+++ agent/src/main/scala/agent/Main.scala
@@ -3,15 +3,11 @@ package agent
 import agent.AgentActor.AgentState
 import agent.AgentCliCommand.StartCommand
 import agent.AgentCommand.SpawnCommand.SpawnSequenceComponent
-import agent.utils.ProcessOutput
-import akka.actor.typed.SpawnProtocol.Spawn
-import akka.actor.typed._
 import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.util.Timeout
 import caseapp.core.RemainingArgs
 import caseapp.core.app.CommandApp
 import csw.location.api.extensions.ActorExtension.RichActor
-import csw.location.client.scaladsl.HttpLocationServiceFactory
 import csw.location.models.Connection.AkkaConnection
 import csw.location.models.{AkkaRegistration, ComponentId, ComponentType}
 import csw.prefix.models.{Prefix, Subsystem}
@@ -24,7 +20,6 @@ import scala.concurrent.{Await, Future}
 // todo: options: clusterPort, auth, interface, devMode
 // todo: devmode kills all processes before dying
 // todo: try moving this module to csw by merging with location-server
-
 object Main extends CommandApp[AgentCliCommand] {
   override def appName: String    = getClass.getSimpleName.dropRight(1) // remove $ from class name
   override def appVersion: String = BuildInfo.version
@@ -53,7 +48,5 @@ object Main extends CommandApp[AgentCliCommand] {
     val response2: Future[Response] = agentRef ? SpawnSequenceComponent(Prefix(Subsystem.ESW, "secondary"))
     println("primary Response=" + Await.result(response, 10.seconds))
     println("secondary Response=" + Await.result(response2, 10.seconds))
-//      val killedResponse: Future[Response] = agentRef ? KillAllProcesses
-//      println("Killed All processes:" + Await.result(killedResponse, 10.seconds))
   }
 }
