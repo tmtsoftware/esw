e1fe0d47d52dfb8a456bddd4e1fe5be0a27d596b
weiss <weiss.personal@gmail.com>
2020-01-24 11:50:12 -0800

    #ESW-000: Jason : Changed to submitAndWait calls

--- examples/src/main/kotlin/esw/ocs/scripts/examples/paradox/MiscExample.kts
+++ examples/src/main/kotlin/esw/ocs/scripts/examples/paradox/MiscExample.kts
@@ -11,7 +11,7 @@ import kotlin.time.minutes
 script {
 
     //#par
-    val prefix = "ocs.primary"
+    val prefix = "OCS.IRIS_darkMode"
     val hcd1 = Hcd(IRIS, "filter.wheel1", 10.minutes)
     val hcd2 = Hcd(IRIS, "filter.wheel2", 10.minutes)
     val hcd3 = Hcd(IRIS, "filter.wheel3", 10.minutes)
@@ -20,9 +20,9 @@ script {
         // send 3 setup commands to 3 HCD's in parallel
         val responses: List<SubmitResponse> =
                 par(
-                        { hcd1.submit(Setup(prefix, "move-10")) },
-                        { hcd2.submit(Setup(prefix, "move-10")) },
-                        { hcd3.submit(Setup(prefix, "move-10")) }
+                        { hcd1.submitAndWait(Setup(prefix, "move-10")) },
+                        { hcd2.submitAndWait(Setup(prefix, "move-10")) },
+                        { hcd3.submitAndWait(Setup(prefix, "move-10")) }
                 )
     }
     //#par
