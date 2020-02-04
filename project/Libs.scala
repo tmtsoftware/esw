c8a3923a83a547073eaa1684fbc55d092b036162
Saloni Vithalani <skvithalani@yahoo.com>
2020-02-04 15:04:06 +0530

    #000 : Saloni: Prepare for 0.1.0-RC1 release (use csw 1.1.0-RC1)

--- project/Libs.scala
+++ project/Libs.scala
@@ -23,7 +23,7 @@ object Libs {
 
 object Csw {
   private val Org     = "com.github.tmtsoftware.csw"
-  private val Version = "e134a87" //change this to 0.1.0-SNAPSHOT to test with local csw changes (after publishLocal)
+  private val Version = "1.1.0-RC1" //change this to 0.1.0-SNAPSHOT to test with local csw changes (after publishLocal)
 
   val `csw-admin-api`                 = dep(Org %%% "csw-admin-api" % Version)
   val `csw-admin-impl`                = Org %% "csw-admin-impl" % Version
