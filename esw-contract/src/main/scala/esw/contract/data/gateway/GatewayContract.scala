0a3563c8e1aa7ae9ae05366b911512083c9c4853
mausamyede <mausam.yede1@gmail.com>
2020-02-06 15:01:27 +0530

    #ESW-278: Manish | Mausam : Add error types to gateway service docs

--- esw-contract/src/main/scala/esw/contract/data/gateway/GatewayContract.scala
+++ esw-contract/src/main/scala/esw/contract/data/gateway/GatewayContract.scala
@@ -9,9 +9,27 @@ import csw.params.commands.CommandResponse.{SubmitResponse, ValidateResponse}
 import csw.params.events.Event
 import csw.prefix.models.Subsystem
 import esw.gateway.api.codecs.GatewayCodecs
-import esw.gateway.api.protocol.PostRequest.{ComponentCommand, GetEvent, GetLogMetadata, Log, PublishEvent, SequencerCommand, SetAlarmSeverity, SetLogLevel}
+import esw.gateway.api.protocol.PostRequest.{
+  ComponentCommand,
+  GetEvent,
+  GetLogMetadata,
+  Log,
+  PublishEvent,
+  SequencerCommand,
+  SetAlarmSeverity,
+  SetLogLevel
+}
 import esw.gateway.api.protocol.WebsocketRequest.{Subscribe, SubscribeWithPattern}
-import esw.gateway.api.protocol.{PostRequest, WebsocketRequest}
+import esw.gateway.api.protocol.{
+  EmptyEventKeys,
+  EventServerUnavailable,
+  GatewayException,
+  InvalidComponent,
+  InvalidMaxFrequency,
+  PostRequest,
+  SetAlarmSeverityFailure,
+  WebsocketRequest
+}
 import esw.ocs.api.protocol.OkOrUnhandledResponse
 import io.bullet.borer.Encoder
 
@@ -23,7 +41,14 @@ object GatewayContract extends GatewayCodecs with GatewayData {
     ModelType(eventKey),
     ModelType(componentId),
     ModelType(logMetadata),
-    ModelType(Subsystem)
+    ModelType(Subsystem),
+    ModelType[GatewayException](
+      invalidComponent,
+      emptyEventKeys,
+      eventServerUnavailable,
+      invalidMaxFrequency,
+      setAlarmSeverityFailure
+    )
   )
 
   implicit def httpEnc[Sub <: PostRequest]: Encoder[Sub]           = SubTypeCodec.encoder(postRequestValue)
@@ -51,20 +76,22 @@ object GatewayContract extends GatewayCodecs with GatewayData {
     Endpoint(
       name[ComponentCommand],
       name[ValidateResponse],
-      description = Some(
+      List(name[InvalidComponent]),
+      Some(
         "Response type will depend on the command passed to componentCommand request. For all possible request and response type mappings refer to HTTP endpoint documentation of command service in CSW."
       )
     ),
     Endpoint(
       name[SequencerCommand],
       name[OkOrUnhandledResponse],
-      description = Some(
+      List(name[InvalidComponent]),
+      Some(
         "Response type will depend on the command passed to sequencerCommand request. For all possible request and response type mappings refer to HTTP endpoint documentation of sequencer service in ESW."
       )
     ),
-    Endpoint(name[PublishEvent], name[Done]),
-    Endpoint(name[GetEvent], arrayName[Event]),
-    Endpoint(name[SetAlarmSeverity], name[Done]),
+    Endpoint(name[PublishEvent], name[Done], List(name[EventServerUnavailable])),
+    Endpoint(name[GetEvent], arrayName[Event], List(name[EmptyEventKeys], name[EventServerUnavailable])),
+    Endpoint(name[SetAlarmSeverity], name[Done], List(name[SetAlarmSeverityFailure])),
     Endpoint(name[Log], name[Done]),
     Endpoint(name[SetLogLevel], name[Unit]),
     Endpoint(name[GetLogMetadata], name[LogMetadata])
@@ -85,8 +112,8 @@ object GatewayContract extends GatewayCodecs with GatewayData {
         "Response type will depend on the command passed to sequencerCommand request. For all possible request and response type mappings refer to websocket endpoint documentation of sequencer service in ESW."
       )
     ),
-    Endpoint(name[Subscribe], name[Event]),
-    Endpoint(name[SubscribeWithPattern], name[Event])
+    Endpoint(name[Subscribe], name[Event], List(name[EmptyEventKeys], name[InvalidMaxFrequency])),
+    Endpoint(name[SubscribeWithPattern], name[Event], List(name[InvalidMaxFrequency]))
   )
 
   val service: Service = Service(
--- esw-contract/src/main/scala/esw/contract/data/gateway/GatewayData.scala
+++ esw-contract/src/main/scala/esw/contract/data/gateway/GatewayData.scala
@@ -10,7 +10,15 @@ import csw.prefix.models.Subsystem
 import esw.contract.data.sequencer.SequencerData
 import esw.gateway.api.protocol.PostRequest._
 import esw.gateway.api.protocol.WebsocketRequest.{Subscribe, SubscribeWithPattern}
-import esw.gateway.api.protocol.{PostRequest, WebsocketRequest}
+import esw.gateway.api.protocol.{
+  EmptyEventKeys,
+  EventServerUnavailable,
+  InvalidComponent,
+  InvalidMaxFrequency,
+  PostRequest,
+  SetAlarmSeverityFailure,
+  WebsocketRequest
+}
 
 trait GatewayData extends CommandData with SequencerData {
   val componentId: ComponentId = ComponentId(prefix, ComponentType.HCD)
@@ -18,21 +26,28 @@ trait GatewayData extends CommandData with SequencerData {
   val eventName: EventName       = EventName("offline")
   val observeEvent: ObserveEvent = ObserveEvent(prefix, eventName)
   val systemEvent: SystemEvent   = SystemEvent(prefix, eventName)
-  val eventKey: EventKey = EventKey(prefix, eventName)
+  val eventKey: EventKey         = EventKey(prefix, eventName)
 
-  val logMetadata= LogMetadata(Level.INFO, Level.DEBUG, Level.INFO, Level.ERROR)
+  val logMetadata = LogMetadata(Level.INFO, Level.DEBUG, Level.INFO, Level.ERROR)
 
   val postComponentCommand: PostRequest.ComponentCommand = PostRequest.ComponentCommand(componentId, observeValidate)
   val postSequencerCommand: PostRequest.SequencerCommand = PostRequest.SequencerCommand(componentId, prepend)
-  val publishEvent: PublishEvent             = PublishEvent(observeEvent)
-  val getEvent: GetEvent = GetEvent(Set(eventKey))
-  val setAlarmSeverity: SetAlarmSeverity = SetAlarmSeverity(AlarmKey(prefix, "someAlarm"), AlarmSeverity.Okay)
-  val log: Log = Log(prefix, Level.DEBUG, "message", Map("additional-info" -> 45))
-  val setLogLevel: SetLogLevel = SetLogLevel(componentId, Level.ERROR)
-  val getLogMetadata: GetLogMetadata = GetLogMetadata(componentId)
+  val publishEvent: PublishEvent                         = PublishEvent(observeEvent)
+  val getEvent: GetEvent                                 = GetEvent(Set(eventKey))
+  val setAlarmSeverity: SetAlarmSeverity                 = SetAlarmSeverity(AlarmKey(prefix, "someAlarm"), AlarmSeverity.Okay)
+  val log: Log                                           = Log(prefix, Level.DEBUG, "message", Map("additional-info" -> 45))
+  val setLogLevel: SetLogLevel                           = SetLogLevel(componentId, Level.ERROR)
+  val getLogMetadata: GetLogMetadata                     = GetLogMetadata(componentId)
 
   val websocketComponentCommand: WebsocketRequest.ComponentCommand = WebsocketRequest.ComponentCommand(componentId, queryFinal)
-  val websocketSequencerCommand: WebsocketRequest.SequencerCommand = WebsocketRequest.SequencerCommand(componentId, sequencerQueryFinal)
-  val subscribe: Subscribe = Subscribe(Set(eventKey), Some(10))
+  val websocketSequencerCommand: WebsocketRequest.SequencerCommand =
+    WebsocketRequest.SequencerCommand(componentId, sequencerQueryFinal)
+  val subscribe: Subscribe                       = Subscribe(Set(eventKey), Some(10))
   val subscribeWithPattern: SubscribeWithPattern = SubscribeWithPattern(Subsystem.CSW, Some(10), "[a-b]*")
+
+  val invalidComponent: InvalidComponent               = InvalidComponent("invalid component")
+  val emptyEventKeys: EmptyEventKeys                   = EmptyEventKeys()
+  val eventServerUnavailable: EventServerUnavailable   = EventServerUnavailable()
+  val invalidMaxFrequency: InvalidMaxFrequency         = InvalidMaxFrequency()
+  val setAlarmSeverityFailure: SetAlarmSeverityFailure = SetAlarmSeverityFailure("alarm fail")
 }
