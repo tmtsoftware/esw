f12d229ac1f0d3f7776ad951706d0818e9e8c7be
Pritam Kadam <kpritam@thoughtworks.com>
2020-02-19 01:04:31 +0530

    #ESW-197: Pritam: Add static imports

--- esw-gateway/esw-gateway-server/src/main/scala/esw/gateway/server/metrics/PostHandlerMetrics.scala
+++ esw-gateway/esw-gateway-server/src/main/scala/esw/gateway/server/metrics/PostHandlerMetrics.scala
@@ -1,9 +1,10 @@
 package esw.gateway.server.metrics
 
 import akka.http.scaladsl.server.Route
-import esw.gateway.api.protocol.PostRequest
-import msocket.impl.post.HttpPostHandler
 import esw.gateway.api.codecs.GatewayCodecs._
+import esw.gateway.api.protocol.PostRequest
+import esw.gateway.api.protocol.PostRequest.{ComponentCommand, GetEvent, PublishEvent}
+import msocket.impl.post.HttpPostHandler
 
 class PostHandlerMetrics(postHandler: HttpPostHandler[PostRequest]) extends HttpPostHandler[PostRequest] {
   import CommandMetrics._
@@ -11,10 +12,10 @@ class PostHandlerMetrics(postHandler: HttpPostHandler[PostRequest]) extends Http
 
   override def handle(request: PostRequest): Route = {
     request match {
-      case PostRequest.ComponentCommand(_, command) => incCommandCounter(command)
-      case _: PostRequest.PublishEvent              => incEventCounter(publishEventLabel)
-      case _: PostRequest.GetEvent                  => incEventCounter(getEventLabel)
-      case _                                        =>
+      case ComponentCommand(_, command) => incCommandCounter(command)
+      case _: PublishEvent              => incEventCounter(publishEventLabel)
+      case _: GetEvent                  => incEventCounter(getEventLabel)
+      case _                            =>
     }
     postHandler.handle(request)
   }
