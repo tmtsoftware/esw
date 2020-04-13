671d6f9d59a2301e78989ab689fdc3f7a96466da
Shubham Jaybhaye <shubhamj7997@gmail.com>
2020-04-13 19:56:23 +0530

    #ESW-178 : Shubham : Fix SequencerUtil to accommodate changes in SequenceComponentUtil. Wire up SequencerUtil  in SM behavior

--- esw-sm/src/main/scala/esw/sm/messages/SequenceManagerMsg.scala
+++ esw-sm/src/main/scala/esw/sm/messages/SequenceManagerMsg.scala
@@ -3,14 +3,15 @@ package esw.sm.messages
 import akka.Done
 import akka.actor.typed.ActorRef
 import csw.location.api.models.HttpLocation
-import esw.ocs.api.protocol.ScriptError
 
 trait SequenceManagerMsg
 
 object SequenceManagerMsg {
-  case class Configure(observingMode: String, replyTo: ActorRef[ConfigureResponse]) extends SequenceManagerMsg
-  case class Cleanup(observingMode: String, replyTo: ActorRef[Done])                extends SequenceManagerMsg
-  case class GetRunningObsModes(replyTo: ActorRef[Set[String]])                     extends SequenceManagerMsg
+  case class Configure(obsMode: String, replyTo: ActorRef[ConfigureResponse]) extends SequenceManagerMsg
+  case class Cleanup(obsMode: String, replyTo: ActorRef[Done])                extends SequenceManagerMsg
+  case class GetRunningObsModes(replyTo: ActorRef[Set[String]])               extends SequenceManagerMsg
+
+  private[sm] case class ConfigurationCompleted(res: ConfigureResponse) extends SequenceManagerMsg
 }
 
 sealed trait ConfigureResponse
@@ -18,9 +19,8 @@ sealed trait ConfigureResponse
 object ConfigureResponse {
   case class Success(sequencerLocation: HttpLocation) extends ConfigureResponse
 
-  sealed trait Failure                                           extends ConfigureResponse
-  case object ConflictingResourcesWithRunningObsMode             extends Failure
-  case class ConfigurationFailure(msg: String)                   extends Failure
-  case class FailedToStartSequencers(reasons: List[ScriptError]) extends Failure
-
+  sealed trait Failure                                      extends ConfigureResponse
+  case object ConflictingResourcesWithRunningObsMode        extends Failure // todo : add conflicting obs mode
+  case class FailedToStartSequencers(reasons: List[String]) extends Failure
+  case class ConfigurationFailure(msg: String)              extends Failure
 }
