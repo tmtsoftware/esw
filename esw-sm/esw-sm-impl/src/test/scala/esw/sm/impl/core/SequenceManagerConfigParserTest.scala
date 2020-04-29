30176c0d4a076e44467945a46b6caacb4e7be012
mausamyede <mausam.yede1@gmail.com>
2020-04-29 16:35:44 +0530

    #ESW-162: Poorva | Mausam : Add test for error scenarios of config parsing Co-authored-by: Poorva Gokhale <13188905+Poorva17@users.noreply.github.com>

--- esw-sm/esw-sm-impl/src/test/scala/esw/sm/impl/core/SequenceManagerConfigParserTest.scala
+++ esw-sm/esw-sm-impl/src/test/scala/esw/sm/impl/core/SequenceManagerConfigParserTest.scala
@@ -3,11 +3,12 @@ package esw.sm.impl.core
 import java.nio.file.Paths
 
 import akka.actor.typed.{ActorSystem, SpawnProtocol}
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{ConfigException, ConfigFactory}
 import csw.config.client.commons.ConfigUtils
 import csw.prefix.models.Subsystem._
 import esw.commons.BaseTestSuite
 import esw.sm.api.models.{ObsModeConfig, Resources, SequenceManagerConfig, Sequencers}
+import io.bullet.borer.Borer.Error.InvalidInputData
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -16,14 +17,14 @@ class SequenceManagerConfigParserTest extends BaseTestSuite {
   implicit private val ec: ExecutionContext = actorSystem.executionContext
 
   "read" must {
-    "read config from local file" in {
+    "read config from local file | ESW-162" in {
       val configUtils                     = mock[ConfigUtils]
       val path                            = Paths.get("testConfig.conf")
       val sequenceManagerConfigParser     = new SequenceManagerConfigParser(configUtils)
       val darknightSequencers: Sequencers = Sequencers(IRIS, ESW, TCS, AOESW)
       val calSequencers: Sequencers       = Sequencers(IRIS, ESW, AOESW)
       val testConfig                      = ConfigFactory.parseResources("testConfig.conf")
-      when(configUtils.getConfig(inputFilePath = path, isLocal = true)).thenReturn(Future.successful(testConfig))
+      when(configUtils.getConfig(isLocal = true, Some(path), None)).thenReturn(Future.successful(testConfig))
 
       val config = sequenceManagerConfigParser.read(configFilePath = path, isLocal = true)
 
@@ -34,6 +35,44 @@ class SequenceManagerConfigParserTest extends BaseTestSuite {
         )
       )
       config.awaitResult shouldBe expectedConfig
+    }
+
+    "throw exception if config file has missing obsMode key | ESW-162" in {
+      val configUtils                 = mock[ConfigUtils]
+      val path                        = Paths.get("missingTestConfig.conf")
+      val sequenceManagerConfigParser = new SequenceManagerConfigParser(configUtils)
+      val testConfig                  = ConfigFactory.parseResources("missingTestConfig.conf")
+      when(configUtils.getConfig(isLocal = true, Some(path), None)).thenReturn(Future.successful(testConfig))
+
+      intercept[ConfigException.Missing](
+        sequenceManagerConfigParser.read(isLocal = true, configFilePath = path).awaitResult
+      )
+    }
+
+    "throw exception if config file has invalid config structure | ESW-162" in {
+      val configUtils                 = mock[ConfigUtils]
+      val path                        = Paths.get("invalidTestConfig.conf")
+      val sequenceManagerConfigParser = new SequenceManagerConfigParser(configUtils)
+      val testConfig                  = ConfigFactory.parseResources("invalidTestConfig.conf")
+      when(configUtils.getConfig(isLocal = true, Some(path), None)).thenReturn(Future.successful(testConfig))
+
+      intercept[InvalidInputData[Any]](
+        sequenceManagerConfigParser.read(isLocal = true, configFilePath = path).awaitResult
+      )
+    }
+
+    "throw exception if it fails to read config | ESW-162" in {
+      val configUtils                 = mock[ConfigUtils]
+      val path                        = Paths.get("testConfig.conf")
+      val sequenceManagerConfigParser = new SequenceManagerConfigParser(configUtils)
+      val expectedException           = new RuntimeException("Failed to read config")
+      when(configUtils.getConfig(isLocal = true, Some(path), None)).thenReturn(Future.failed(expectedException))
+
+      val exception = intercept[RuntimeException](
+        sequenceManagerConfigParser.read(isLocal = true, configFilePath = path).awaitResult
+      )
+
+      exception shouldBe expectedException
     }
   }
 }
