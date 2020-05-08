848d17fc512cbb7c36ff33aa61cdd93d1d991643
Poorva Gokhale <poorvasgokhale@gmail.com>
2020-05-08 12:28:44 +0530

    #ESW-166: Poorva: Refactor sm integration test

--- esw-integration-test/src/test/scala/esw/sm/app/SequenceManagerIntegrationTest.scala
+++ esw-integration-test/src/test/scala/esw/sm/app/SequenceManagerIntegrationTest.scala
@@ -5,18 +5,17 @@ import java.nio.file.Paths
 import csw.location.api.models.ComponentType.Sequencer
 import csw.prefix.models.Subsystem.{AOESW, ESW, IRIS}
 import csw.prefix.models.{Prefix, Subsystem}
+import esw.ocs.api.actor.client.SequenceComponentImpl
 import esw.ocs.app.SequencerApp
 import esw.ocs.testkit.EswTestKit
 import esw.sm.api.SequenceManagerApi
 import esw.sm.api.models.{CleanupResponse, ConfigureResponse}
 
-import scala.concurrent.Future
-
 class SequenceManagerIntegrationTest extends EswTestKit {
 
   override def afterEach(): Unit = locationService.unregisterAll()
 
-  "configure for provided observation mode | ESW-162" in {
+  "configure and cleanup for provided observation mode | ESW-162, ESW-166" in {
     // Setup Sequence components for ESW, IRIS, AOESW
     TestSetup.setupSeqComponent(ESW, IRIS, AOESW)
 
@@ -24,10 +23,9 @@ class SequenceManagerIntegrationTest extends EswTestKit {
     val configFilePath  = Paths.get(ClassLoader.getSystemResource("sequence_manager.conf").toURI)
     val wiring          = new SequenceManagerWiring(configFilePath)
 
-    // Start Sequence Manager
     val sequenceManager: SequenceManagerApi = wiring.start
 
-    // Configure for observing mode
+    // ************ Configure for observing mode ************************
     val configureResponse = sequenceManager.configure(obsMode).futureValue
 
     // assert for Successful Configuration
@@ -38,55 +36,40 @@ class SequenceManagerIntegrationTest extends EswTestKit {
     resolveSequencerLocation(Prefix(IRIS, obsMode))
     resolveSequencerLocation(Prefix(AOESW, obsMode))
 
-    // verify configure response returns master sequencer http location
+    // assert that sequence components are busy
+    TestSetup.assertSeqCompAvailability(isSeqCompAvailable = false, ESW, IRIS, AOESW)
+
+    // ESW-162 verify configure response returns master sequencer http location
     val masterSequencerLocation = resolveHTTPLocation(Prefix(ESW, obsMode), Sequencer)
     configureResponse shouldBe ConfigureResponse.Success(masterSequencerLocation)
-  }
 
-  "cleanup for provided observation mode | ESW-166" in {
-    // Setup Sequence components for ESW, IRIS, AOESW
-    TestSetup.setupSeqComponent(ESW, IRIS, AOESW)
-
-    val obsMode: String = "IRIS_Cal"
-    val configFilePath  = Paths.get(ClassLoader.getSystemResource("sequence_manager.conf").toURI)
-    val wiring          = new SequenceManagerWiring(configFilePath)
-
-    // Start Sequence Manager
-    val sequenceManager: SequenceManagerApi = wiring.start
-
-    // Configure for observing mode
-    val configureResponse = sequenceManager.configure(obsMode).futureValue
-
-    // assert for Successful Configuration
-    configureResponse shouldBe a[ConfigureResponse.Success]
-
-    // Cleanup for observing mode
+    // *************** Cleanup for observing mode ********************
     val cleanupResponse = sequenceManager.cleanup(obsMode).futureValue
 
     // assert for Successful Cleanup
     cleanupResponse shouldBe CleanupResponse.Success
 
-    // ESW-166 (verify all sequencers are stopped for the observing mode)
-    val eswEventualLocation = Future {
-      resolveSequencerLocation(Prefix(ESW, obsMode))
-    }
-    val irisEventualLocation = Future {
-      resolveSequencerLocation(Prefix(IRIS, obsMode))
-    }
-    val aoeswEventualLocation = Future {
-      resolveSequencerLocation(Prefix(AOESW, obsMode))
-    }
-    Future
-      .traverse(List(eswEventualLocation, irisEventualLocation, aoeswEventualLocation)) { x =>
-        Future.successful(intercept[Exception](x.futureValue))
-      }
-      .futureValue
+    // ESW-166 verify all sequencers are stopped for the observing mode and seq comps are available
+    TestSetup.assertSeqCompAvailability(isSeqCompAvailable = true, ESW, IRIS, AOESW)
   }
 
   object TestSetup {
+    val seqCompName = "primary"
+
     def setupSeqComponent(subsystems: Subsystem*): Unit = {
       // Setup Sequence components for subsystems
-      subsystems.foreach(subsystem => SequencerApp.main(Array("seqcomp", "-s", subsystem.name, "-n", "primary")))
+      subsystems.foreach(subsystem => {
+        SequencerApp.main(Array("seqcomp", "-s", subsystem.name, "-n", seqCompName))
+      })
+    }
+
+    def assertSeqCompAvailability(isSeqCompAvailable: Boolean, subsystem: Subsystem*): Unit = {
+      subsystem.foreach(subsystem => {
+        val seqCompStatus =
+          new SequenceComponentImpl(resolveSequenceComponentLocation(Prefix(subsystem, seqCompName))).status.futureValue
+        if (isSeqCompAvailable) seqCompStatus.response shouldBe None // assert sequence components are available
+        else seqCompStatus.response.isDefined shouldBe true          // assert sequence components are busy
+      })
     }
   }
 }
