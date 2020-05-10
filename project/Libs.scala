ffbf41204402aea3b7d5f4d9ab307d3a17d094d4
Scala Steward <me@scala-steward.org>
2020-05-10 18:56:50 +0200

    Update scalatest to 3.1.2

--- project/Libs.scala
+++ project/Libs.scala
@@ -9,7 +9,7 @@ object Libs {
   val enumeratum           = dep("com.beachape" %%% "enumeratum" % "1.6.0") //MIT License
   val `mockito-scala`      = "org.mockito"                    %% "mockito-scala"      % "1.13.4" // MIT License
   val `scala-async`        = "org.scala-lang.modules"         %% "scala-async"        % "0.10.0" //BSD 3-clause "New" or "Revised" License
-  val scalatest            = "org.scalatest"                  %% "scalatest"          % "3.1.1"  //Apache License 2.0
+  val scalatest            = "org.scalatest"                  %% "scalatest"          % "3.1.2"  //Apache License 2.0
   val `scala-java8-compat` = "org.scala-lang.modules"         %% "scala-java8-compat" % "0.9.1"  //BSD 3-clause "New" or "Revised" License
   val `msocket-api`        = dep("com.github.tmtsoftware.msocket" %%% "msocket-api" % MSocketVersion)
   val `msocket-impl-jvm`   = "com.github.tmtsoftware.msocket" %% "msocket-impl"       % MSocketVersion
