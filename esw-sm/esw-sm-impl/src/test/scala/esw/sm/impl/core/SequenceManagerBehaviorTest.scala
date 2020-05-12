b2b5e1e61c6ec57063fac65acf4489bbf6dff524
Pritam Kadam <phkadam2008@gmail.com>
2020-05-12 21:41:37 +0530

    #ESW164: Pritam: Remove duplicate cleanup test from SequenceManagerBehaviorTest

--- esw-sm/esw-sm-impl/src/test/scala/esw/sm/impl/core/SequenceManagerBehaviorTest.scala
+++ esw-sm/esw-sm-impl/src/test/scala/esw/sm/impl/core/SequenceManagerBehaviorTest.scala
@@ -26,13 +26,13 @@ import scala.concurrent.duration.DurationLong
 
 class SequenceManagerBehaviorTest extends ScalaTestWithActorTestKit with BaseTestSuite {
 
-  private val DARKNIGHT                        = "darknight"
-  private val CLEARSKIES                       = "clearskies"
+  private val Darknight                        = "darknight"
+  private val Clearskies                       = "clearskies"
   private val darknightSequencers: Sequencers  = Sequencers(ESW, TCS)
   private val clearskiesSequencers: Sequencers = Sequencers(ESW)
   private val config = Map(
-    DARKNIGHT  -> ObsModeConfig(Resources("r1", "r2"), darknightSequencers),
-    CLEARSKIES -> ObsModeConfig(Resources("r2", "r3"), clearskiesSequencers)
+    Darknight  -> ObsModeConfig(Resources("r1", "r2"), darknightSequencers),
+    Clearskies -> ObsModeConfig(Resources("r2", "r3"), clearskiesSequencers)
   )
   private val locationServiceUtil: LocationServiceUtil = mock[LocationServiceUtil]
   private val sequencerUtil: SequencerUtil             = mock[SequencerUtil]
@@ -44,83 +44,76 @@ class SequenceManagerBehaviorTest extends ScalaTestWithActorTestKit with BaseTes
 
   "Configure" must {
 
-    "transition sm from IDLE -> ConfigurationInProcess -> Idle state and return location of master sequencer| ESW-178, ESW-164" in {
-      val httpLocation   = HttpLocation(HttpConnection(ComponentId(Prefix(ESW, DARKNIGHT), Sequencer)), new URI("uri"))
+    "transition sm from IDLE -> ConfigurationInProcess -> Idle state and return location of master sequencer | ESW-178, ESW-164" in {
+      val httpLocation   = HttpLocation(HttpConnection(ComponentId(Prefix(ESW, Darknight), Sequencer)), new URI("uri"))
       val configResponse = Success(httpLocation)
       when(locationServiceUtil.listAkkaLocationsBy(ESW, Sequencer)).thenReturn(future(1.seconds, Right(List.empty)))
-      when(sequencerUtil.startSequencers(DARKNIGHT, darknightSequencers)).thenReturn(Future.successful(configResponse))
+      when(sequencerUtil.startSequencers(Darknight, darknightSequencers)).thenReturn(Future.successful(configResponse))
       val configureProbe = createTestProbe[ConfigureResponse]
 
       // STATE TRANSITION: IDLE -> Configure() -> ConfigurationInProcess -> Idle
       assertState(Idle)
-      smRef ! Configure(DARKNIGHT, configureProbe.ref)
+      smRef ! Configure(Darknight, configureProbe.ref)
       assertState(ConfigurationInProcess)
       assertState(Idle)
 
       configureProbe.expectMessage(configResponse)
       verify(locationServiceUtil).listAkkaLocationsBy(ESW, Sequencer)
-      verify(sequencerUtil).startSequencers(DARKNIGHT, darknightSequencers)
+      verify(sequencerUtil).startSequencers(Darknight, darknightSequencers)
     }
 
-    "return ConfigurationFailure if location service fails to return running observation mode | ESW-178" in {
+    "return LocationServiceError if location service fails to return running observation mode | ESW-178" in {
       when(locationServiceUtil.listAkkaLocationsBy(ESW, Sequencer))
         .thenReturn(Future.successful(Left(RegistrationListingFailed("Sequencer"))))
 
       val probe = createTestProbe[ConfigureResponse]
-      smRef ! Configure(DARKNIGHT, probe.ref)
-      probe.expectMessage(LocationServiceError("Sequencer"))
+      smRef ! Configure(Darknight, probe.ref)
 
+      probe.expectMessage(LocationServiceError("Sequencer"))
       verify(locationServiceUtil).listAkkaLocationsBy(ESW, Sequencer)
     }
 
     "return ConflictingResourcesWithRunningObsMode when required resources are already in use | ESW-169" in {
-      val akkaLocation = AkkaLocation(AkkaConnection(ComponentId(Prefix(ESW, CLEARSKIES), Sequencer)), new URI("uri"))
-      when(locationServiceUtil.listAkkaLocationsBy(ESW, Sequencer))
-        .thenReturn(Future.successful(Right(List(akkaLocation))))
+      // this simulates that Clearskies observation is running
+      val akkaLocation = AkkaLocation(AkkaConnection(ComponentId(Prefix(ESW, Clearskies), Sequencer)), new URI("uri"))
+      when(locationServiceUtil.listAkkaLocationsBy(ESW, Sequencer)).thenReturn(Future.successful(Right(List(akkaLocation))))
       val probe = createTestProbe[ConfigureResponse]
 
-      smRef ! Configure("darknight", probe.ref)
+      // r2 is a conflicting resource between Darknight and Clearskies observations
+      smRef ! Configure(Darknight, probe.ref)
 
-      probe.expectMessage(ConflictingResourcesWithRunningObsMode(Set(CLEARSKIES)))
+      probe.expectMessage(ConflictingResourcesWithRunningObsMode(Set(Clearskies)))
       verify(locationServiceUtil).listAkkaLocationsBy(ESW, Sequencer)
-      verify(sequencerUtil, times(0)).startSequencers(DARKNIGHT, darknightSequencers)
+      verify(sequencerUtil, times(0)).startSequencers(Darknight, darknightSequencers)
     }
   }
 
   "Cleanup" must {
 
-    "transition sequence manager to CleaningInProcess state | ESW-166" in {
-      when(sequencerUtil.stopSequencers(darknightSequencers, DARKNIGHT))
-        .thenReturn(future(1.seconds, Right(Done)))
+    "transition sm from IDLE -> CleaningInProcess -> Idle state and stop all the sequencer for given obs mode | ESW-166" in {
+      when(sequencerUtil.stopSequencers(darknightSequencers, Darknight)).thenReturn(future(1.seconds, Right(Done)))
 
       val cleanupProbe = createTestProbe[CleanupResponse]
-      smRef ! Cleanup(DARKNIGHT, cleanupProbe.ref)
 
+      assertState(Idle)
+      smRef ! Cleanup(Darknight, cleanupProbe.ref)
       assertState(CleaningInProcess)
       assertState(Idle)
-    }
 
-    "stop all the sequencers of the given observation mode | ESW-166" in {
-      when(sequencerUtil.stopSequencers(darknightSequencers, DARKNIGHT))
-        .thenReturn(Future.successful(Right(Done)))
-
-      val probe = createTestProbe[CleanupResponse]
-      smRef ! Cleanup(DARKNIGHT, probe.ref)
-
-      probe.expectMessage(CleanupResponse.Success)
-      verify(sequencerUtil).stopSequencers(darknightSequencers, DARKNIGHT)
+      cleanupProbe.expectMessage(CleanupResponse.Success)
+      verify(sequencerUtil).stopSequencers(darknightSequencers, Darknight)
     }
 
     "return fail if there is failure while stopping sequencers | ESW-166" in {
       val failureMsg = "location service error"
-      when(sequencerUtil.stopSequencers(darknightSequencers, DARKNIGHT))
+      when(sequencerUtil.stopSequencers(darknightSequencers, Darknight))
         .thenReturn(Future.successful(Left(RegistrationListingFailed(failureMsg))))
 
       val probe = createTestProbe[CleanupResponse]
-      smRef ! Cleanup(DARKNIGHT, probe.ref)
+      smRef ! Cleanup(Darknight, probe.ref)
 
       probe.expectMessage(CleanupResponse.Failed(failureMsg))
-      verify(sequencerUtil).stopSequencers(darknightSequencers, DARKNIGHT)
+      verify(sequencerUtil).stopSequencers(darknightSequencers, Darknight)
     }
   }
 
