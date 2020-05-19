a62f92dfac5ad95cd4776e21886970db224d2a60
Scala Steward <me@scala-steward.org>
2020-05-19 21:05:58 +0200

    Update kotlinx-coroutines-jdk8 to 1.3.7

--- project/Libs.scala
+++ project/Libs.scala
@@ -89,7 +89,7 @@ object Borer {
 
 object Kotlin {
   val `stdlib-jdk8`     = "org.jetbrains.kotlin"  % "kotlin-stdlib-jdk8"      % "1.3.72"
-  val `coroutines-jdk8` = "org.jetbrains.kotlinx" % "kotlinx-coroutines-jdk8" % "1.3.6"
+  val `coroutines-jdk8` = "org.jetbrains.kotlinx" % "kotlinx-coroutines-jdk8" % "1.3.7"
   val kotlintest        = "io.kotlintest"         % "kotlintest-core"         % "3.4.2"
   val mockk             = "io.mockk"              % "mockk"                   % "1.10.0"
 }
