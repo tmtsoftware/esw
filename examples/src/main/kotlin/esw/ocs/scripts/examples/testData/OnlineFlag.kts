1c0e966e8df5c5ca2198037a2dfe3b242db8d3a2
mausamyede <mausam.yede1@gmail.com>
2020-06-17 14:04:16 +0530

    #ESW-287 : Chauhan | Mausam : Add integration test for sequencer goOnline/goOffline handler invocation scenario

--- examples/src/main/kotlin/esw/ocs/scripts/examples/testData/OnlineFlag.kts
+++ examples/src/main/kotlin/esw/ocs/scripts/examples/testData/OnlineFlag.kts
@@ -2,14 +2,26 @@ package esw.ocs.scripts.examples.testData
 
 import esw.ocs.dsl.core.script
 import esw.ocs.dsl.params.booleanKey
+import esw.ocs.dsl.params.intKey
 import kotlin.time.milliseconds
 
 script {
 
     val onlineFlagKey = booleanKey("online-flag")
+    var goOnlineCount = 0
+    var goOfflineCount = 0
 
     loopAsync(100.milliseconds) {
         publishEvent(SystemEvent("TCS.filter.wheel", "online-flag", onlineFlagKey.set(isOnline)))
     }
 
+    onGoOffline {
+        goOfflineCount += 1
+        publishEvent(SystemEvent("TCS.filter.wheel", "go-offline-handler", intKey("offline-key").set(goOfflineCount)))
+    }
+
+    onGoOnline {
+        goOnlineCount += 1
+        publishEvent(SystemEvent("TCS.filter.wheel", "go-online-handler", intKey("online-key").set(goOnlineCount)))
+    }
 }
