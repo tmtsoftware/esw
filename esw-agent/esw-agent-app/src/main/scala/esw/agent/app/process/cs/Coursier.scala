8a151ac23aeffa8d30e7c06a14de4c67f4883032
Poorva Gokhale <poorvasgokhale@gmail.com>
2020-06-23 16:46:37 +0530

    #000: Poorva: Install cs native jar for running agent integration tests. (#174)
    * #000: Poorva: Install cs native jar for running agent integration tests.
    
    This reduces agent spawning time by 4-5 seconds
    
    * #000: Poorva: Reduce agent timeout to 10 seconds
    
    * #000: Refactoring dev.yml

--- esw-agent/esw-agent-app/src/main/scala/esw/agent/app/process/cs/Coursier.scala
+++ esw-agent/esw-agent-app/src/main/scala/esw/agent/app/process/cs/Coursier.scala
@@ -1,13 +1,7 @@
 package esw.agent.app.process.cs
 
-import esw.agent.app.process.ProcessUtils
-
 object Coursier {
-  private def csNative             = "cs"
-  private def csLauncher           = getClass.getResource("/coursier").getPath
-  private def csInstalled: Boolean = ProcessUtils.isInstalled(csNative)
-
-  lazy val cs: String = if (csInstalled) csNative else csLauncher
+  lazy val cs: String = "cs"
 
   def ocsApp(version: Option[String]): CoursierLaunch = CoursierLaunch("ocs-app", version)
 }
