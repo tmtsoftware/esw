9fddf70a247346fb08638a9edabeb3732cf62eb9
Shubham Jaybhaye <shubhamj7997@gmail.com>
2020-07-03 16:28:43 +0530

    #ESW-328 : Shubham: Remove unnecessary Resource type in sm config models

--- esw-sm/esw-sm-impl/src/main/scala/esw/sm/impl/config/models.scala
+++ esw-sm/esw-sm-impl/src/main/scala/esw/sm/impl/config/models.scala
@@ -3,14 +3,12 @@ package esw.sm.impl.config
 import csw.prefix.models.Subsystem
 import esw.ocs.api.models.ObsMode
 
-case class Resource(subsystem: Subsystem)
-
-case class Resources(resources: Set[Resource]) {
+case class Resources(resources: Set[Subsystem]) {
   private def conflictsWith(other: Resources): Boolean  = this.resources.exists(other.resources.contains)
   def conflictsWithAny(others: Set[Resources]): Boolean = others.exists(conflictsWith)
 }
 object Resources {
-  def apply(resources: Resource*): Resources = new Resources(resources.toSet)
+  def apply(resources: Subsystem*): Resources = new Resources(resources.toSet)
 }
 
 case class Sequencers(subsystems: List[Subsystem])
