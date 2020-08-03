1461cfdb4e781e07a41c7d804d7c6ac77a51dc7c
Pritam Kadam <phkadam2008@gmail.com>
2020-08-03 15:30:25 +0530

    #000: Pritam: Remove duplicate code from GetComponentStatusTest

--- esw-agent/esw-agent-app/src/test/scala/esw/agent/app/GetComponentStatusTest.scala
+++ esw-agent/esw-agent-app/src/test/scala/esw/agent/app/GetComponentStatusTest.scala
@@ -1,11 +1,7 @@
 package esw.agent.app
 
-import java.net.URI
-
 import akka.actor.testkit.typed.scaladsl.TestProbe
 import akka.actor.typed.ActorRef
-import csw.location.api.models.ComponentType.{SequenceComponent, Service}
-import csw.location.api.models.Connection.{AkkaConnection, TcpConnection}
 import csw.location.api.models._
 import csw.location.api.scaladsl.RegistrationResult
 import esw.agent.api.AgentCommand.GetComponentStatus
@@ -15,136 +11,66 @@ import esw.agent.api.ComponentStatus.{Initializing, NotAvailable, Running}
 import esw.agent.api._
 import org.mockito.ArgumentMatchers.any
 import org.scalatest.matchers.must.Matchers.convertToStringMustWrapper
+import org.scalatest.prop.TableDrivenPropertyChecks._
 
+import scala.concurrent.Future
 import scala.concurrent.duration.{DurationLong, FiniteDuration}
 
 class GetComponentStatusTest extends AgentSetup {
+  "GetComponentStatus" must {
+    val spawnRedis: ActorRef[SpawnResponse] => SpawnRedis                    = SpawnRedis(_, prefix, 6548, List.empty)
+    val spawnSeqComponent: ActorRef[SpawnResponse] => SpawnSequenceComponent = SpawnSequenceComponent(_, seqCompPrefix)
 
-  "GetComponentStatus (manually registered)" must {
-    val componentId                    = ComponentId(prefix, Service)
-    val getStatus                      = GetComponentStatus(_, componentId)
-    val connection                     = TcpConnection(componentId)
-    implicit val location: TcpLocation = TcpLocation(connection, new URI("uri"))
-    implicit val registrationResult: RegistrationResult =
-      RegistrationResult.from(location, con => locationService.unregister(con))
-    val spawnComponent = SpawnRedis(_, prefix, 6548, List.empty)
+    Table(
+      ("name", "SpawnCommand", "ComponentID"),
+      ("SpawnRedis", spawnRedis, componentId),
+      ("SpawnSequenceComponent", spawnSeqComponent, seqCompComponentId)
+    ).foreach {
+      case (name, spawnComponent, compId) =>
+        s"reply 'NotAvailable' when given component is not present on machine [$name] | ESW-286" in {
+          withAgentSetup(name + "1") { (agentRef, compStatusProbe, _) =>
+            agentRef ! GetComponentStatus(compStatusProbe.ref, compId)
+            compStatusProbe.expectMessage(NotAvailable)
+          }
+        }
 
-    "reply 'NotAvailable' when given component is not present on machine | ESW-286" in {
-      val agentActorRef = spawnAgentActor(name = "test-actor1")
-      val probe         = TestProbe[ComponentStatus]()
-      agentActorRef ! getStatus(probe.ref)
-      probe.expectMessage(NotAvailable)
+        s"reply 'Initializing' when component is not spawned [$name] | ESW-286" in {
+          withAgentSetup(name + "2") { (agentRef, compStatusProbe, spawnResProbe) =>
+            agentRef ! spawnComponent(spawnResProbe.ref)
+            agentRef ! GetComponentStatus(compStatusProbe.ref, compId)
+            compStatusProbe.expectMessage(Initializing)
+          }
+        }
+
+        s"reply 'Running' when process is running and registered [$name] | ESW-286" in {
+          withAgentSetup(name + "3", spawnComponentAfter = 0.seconds) { (agentRef, compStatusProbe, spawnResProbe) =>
+            agentRef ! spawnComponent(spawnResProbe.ref)
+            spawnResProbe.expectMessage(Spawned)
+            agentRef ! GetComponentStatus(compStatusProbe.ref, compId)
+            compStatusProbe.expectMessage(Running)
+          }
+        }
     }
-
-    "reply 'Initializing' when registration is being checked for given component before spawning process | ESW-286" in {
-      val agentActorRef = spawnAgentActor(name = "test-actor2")
-      val spawner       = TestProbe[SpawnResponse]()
-      val probe         = TestProbe[ComponentStatus]()
-
-      mockSuccessfulProcess(2.seconds)
-      mockLocationService(checkDuration = 2.seconds)
-
-      agentActorRef ! spawnComponent(spawner.ref)
-      agentActorRef ! getStatus(probe.ref)
-      probe.expectMessage(Initializing)
-    }
-
-    "reply 'Initializing' when registration is being performed for given component after spawning process | ESW-286" in {
-      val agentActorRef = spawnAgentActor(name = "test-actor3")
-      val spawner       = TestProbe[SpawnResponse]()
-      val probe         = TestProbe[ComponentStatus]()
-
-      mockSuccessfulProcess(2.seconds)
-      mockLocationService(registrationDuration = 2.seconds)
-
-      agentActorRef ! spawnComponent(spawner.ref)
-      agentActorRef ! getStatus(probe.ref)
-      probe.expectMessage(Initializing)
-    }
-
-    "reply 'Running' when process is running and registered | ESW-286" in {
-      val agentActorRef = spawnAgentActor(name = "test-actor4")
-      val spawner       = TestProbe[SpawnResponse]()
-      val probe         = TestProbe[ComponentStatus]()
-
-      mockSuccessfulProcess(5.seconds)
-      mockLocationService()
-
-      agentActorRef ! spawnComponent(spawner.ref)
-      Thread.sleep(200)
-      agentActorRef ! getStatus(probe.ref)
-      probe.expectMessage(Running)
-    }
-
   }
 
-  "GetComponentStatus (self registered)" must {
+  private def withAgentSetup(name: String, spawnComponentAfter: FiniteDuration = 2.seconds)(
+      testCode: (ActorRef[AgentCommand], TestProbe[ComponentStatus], TestProbe[SpawnResponse]) => Unit
+  ): Unit = {
+    val agentActorRef = spawnAgentActor(name = name)
+    val probe         = TestProbe[ComponentStatus]()
+    val spawner       = TestProbe[SpawnResponse]()
 
-    val componentId                     = ComponentId(prefix, SequenceComponent)
-    val getStatus                       = GetComponentStatus(_, componentId)
-    val connection                      = AkkaConnection(componentId)
-    implicit val location: AkkaLocation = AkkaLocation(connection, new URI("uri"))
-    implicit val registrationResult: RegistrationResult =
-      RegistrationResult.from(location, con => locationService.unregister(con))
-    val spawnComponent: ActorRef[SpawnResponse] => SpawnSequenceComponent = SpawnSequenceComponent(_, prefix)
+    mockSuccessfulProcess(5.seconds) // do not let process die
+    mockLocationService(spawnComponentAfter)
 
-    "reply 'NotAvailable' when given component is not present on machine | ESW-286" in {
-      val agentActorRef = spawnAgentActor(name = "test-actor6")
-      val probe         = TestProbe[ComponentStatus]()
-      agentActorRef ! getStatus(probe.ref)
-      probe.expectMessage(NotAvailable)
-    }
-
-    "reply 'Initializing' when registration is being checked for given component before spawning process | ESW-286" in {
-      val agentActorRef = spawnAgentActor(name = "test-actor7")
-      val spawner       = TestProbe[SpawnResponse]()
-      val probe         = TestProbe[ComponentStatus]()
-
-      mockSuccessfulProcess(2.seconds)
-      mockLocationService(checkDuration = 2.seconds)
-
-      agentActorRef ! spawnComponent(spawner.ref)
-      agentActorRef ! getStatus(probe.ref)
-      probe.expectMessage(Initializing)
-    }
-
-    "reply 'Initializing' when registration is being validated for given component after spawning process | ESW-286" in {
-      val agentActorRef = spawnAgentActor(name = "test-actor8")
-      val spawner       = TestProbe[SpawnResponse]()
-      val probe         = TestProbe[ComponentStatus]()
-
-      mockSuccessfulProcess(2.seconds)
-      mockLocationService(validationDuration = 2.seconds)
-
-      agentActorRef ! spawnComponent(spawner.ref)
-      agentActorRef ! getStatus(probe.ref)
-      probe.expectMessage(Initializing)
-    }
-
-    "reply 'Running' when process is running and registered | ESW-286" in {
-      val agentActorRef = spawnAgentActor(name = "test-actor9")
-      val spawner       = TestProbe[SpawnResponse]()
-      val probe         = TestProbe[ComponentStatus]()
-
-      mockSuccessfulProcess(5.seconds)
-      mockLocationService()
-
-      agentActorRef ! spawnComponent(spawner.ref)
-      Thread.sleep(200)
-      agentActorRef ! getStatus(probe.ref)
-      probe.expectMessage(Running)
-    }
-
+    testCode(agentActorRef, probe, spawner)
   }
 
-  private def mockLocationService(
-      checkDuration: FiniteDuration = 0.seconds,
-      registrationDuration: FiniteDuration = 0.seconds,
-      validationDuration: FiniteDuration = 0.seconds
-  )(implicit location: Location, registrationResult: RegistrationResult): Unit = {
+  private def mockLocationService(checkDuration: FiniteDuration = 0.seconds): Unit = {
+    val location           = mock[Location]
+    val registrationResult = mock[RegistrationResult]
     when(locationService.resolve(any[TypedConnection[Location]], any[FiniteDuration]))
-      .thenReturn(delayedFuture(None, checkDuration), delayedFuture(Some(location), validationDuration))
-    when(locationService.register(any[Registration]))
-      .thenReturn(delayedFuture(registrationResult, registrationDuration))
+      .thenReturn(delayedFuture(None, checkDuration), Future.successful(Some(location)))
+    when(locationService.register(any[Registration])).thenReturn(Future.successful(registrationResult))
   }
 }
