44c1febb3807f3c765c26a48f86208ca2bf8b2e7
Pritam Kadam <phkadam2008@gmail.com>
2020-08-12 14:27:21 +0530

    #000: Pritam: Fix config key in AgentClient

--- esw-agent-akka/esw-agent-akka-client/src/main/scala/esw/agent/akka/client/AgentClient.scala
+++ esw-agent-akka/esw-agent-akka-client/src/main/scala/esw/agent/akka/client/AgentClient.scala
@@ -21,7 +21,7 @@ import scala.jdk.DurationConverters.JavaDurationOps
 
 class AgentClient(akkaLocation: AkkaLocation)(implicit actorSystem: ActorSystem[_]) {
   private implicit val timeout: Timeout = Timeout(
-    actorSystem.settings.config.getDuration("esw.agent.akka.client.askTimeout").toScala
+    actorSystem.settings.config.getDuration("agent.akka.client.askTimeout").toScala
   )
   private val agentRef: ActorRef[AgentCommand] = akkaLocation.uri.toActorRef.unsafeUpcast[AgentCommand]
   private val agentPrefixStr                   = akkaLocation.prefix.toString()
