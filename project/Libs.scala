8dae7d1a9a4b41e23164eb876e7712fa803f7d27
Shubham Chauhan <shubham25may1998@gmail.com>
2020-09-07 09:31:20 +0530

    upgrade csw

--- project/Libs.scala
+++ project/Libs.scala
@@ -26,7 +26,7 @@ object Libs {
 object Csw {
   private val Org = "com.github.tmtsoftware.csw"
 
-  private val Version = "1d54709" // Change this to 0.1.0-SNAPSHOT to test with local csw changes (after publishLocal)
+  private val Version = "f4c30d8b1d" // Change this to 0.1.0-SNAPSHOT to test with local csw changes (after publishLocal)
 
   val `csw-aas-http`        = Org %% "csw-aas-http"        % Version
   val `csw-alarm-api`       = Org %% "csw-alarm-api"       % Version
