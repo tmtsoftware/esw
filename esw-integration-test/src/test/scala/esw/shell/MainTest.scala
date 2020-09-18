b79249fea85fa81787b1c6da58593049d1896f06
Pritam Kadam <phkadam2008@gmail.com>
2020-09-18 22:59:22 +0530

    #000: Pritam: Update esw-shell test

--- esw-integration-test/src/test/scala/esw/shell/MainTest.scala
+++ esw-integration-test/src/test/scala/esw/shell/MainTest.scala
@@ -4,21 +4,20 @@ import esw.testcommons.BaseTestSuite
 
 class MainTest extends BaseTestSuite {
 
-  private var process: Option[Process] = None
-
-  "Main test should check imports" in {
-
+  "verify esw-shell compiles and starts successfully" in {
     val channel        = "https://raw.githubusercontent.com/tmtsoftware/osw-apps/master/apps.json"
     val version        = "0.1.0-SNAPSHOT"
     val commands       = List("cs", "launch", "--channel", channel, s"esw-shell:$version")
     val processBuilder = new ProcessBuilder(commands: _*)
-    process = Some(processBuilder.start())
+    val process        = processBuilder.inheritIO().start()
     Thread.sleep(10000)
-    process.get.isAlive shouldBe true
+    assert(process.isAlive, "esw-shell failed to start!")
+
+    process.descendants().map(_.destroyForcibly())
+    process.destroyForcibly()
   }
 
-  override protected def afterAll(): Unit = {
-    process.foreach(_.destroy())
-    super.afterAll()
-  }
+  // just to verify if this test halts github runner
+  override protected def afterAll(): Unit = println("========== ESW-SHELL TERMINATED ==========")
+
 }
