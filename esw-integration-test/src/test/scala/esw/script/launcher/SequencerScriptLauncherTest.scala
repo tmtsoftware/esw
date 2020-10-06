0cd2105acf8a78e85a83780f9d3786666f6c61af
mausamyede <mausam.yede1@gmail.com>
2020-10-06 18:43:05 +0530

    #ESW-000 : Mausam : Enable logging and try increased timeout for failing test

--- esw-integration-test/src/test/scala/esw/script/launcher/SequencerScriptLauncherTest.scala
+++ esw-integration-test/src/test/scala/esw/script/launcher/SequencerScriptLauncherTest.scala
@@ -5,6 +5,7 @@ import java.util
 import csw.location.api.models.ComponentId
 import csw.location.api.models.ComponentType.Sequencer
 import csw.location.api.models.Connection.AkkaConnection
+import csw.logging.client.scaladsl.LoggingSystemFactory
 import csw.prefix.models.Prefix
 import csw.prefix.models.Subsystem.ESW
 import esw.agent.akka.app.ext.ProcessExt.ProcessOps
@@ -26,7 +27,7 @@ class SequencerScriptLauncherTest extends EswTestKit {
   private val scriptLauncher   = (projectRootPath / "scripts" / "script-launcher" / "launchSequencer.sh").toString()
 
   var process: Process = _
-
+  LoggingSystemFactory.forTestingOnly()
   "launch sequencer script should compile given sequencer script and start sequencer with it| ESW-150" in {
     val builder = new ProcessBuilder(scriptLauncher, "-f", sampleScriptPath, "-v", ocsAppVersion).inheritIO()
 
@@ -41,7 +42,7 @@ class SequencerScriptLauncherTest extends EswTestKit {
 
     // check sequencer is registered in location service
     val prefix         = Prefix(ESW, className)
-    val resolveTimeout = 20.seconds // this timeout includes time taken by the process to start
+    val resolveTimeout = 30.seconds // this timeout includes time taken by the process to start
     val locationF =
       locationService.resolve(AkkaConnection(ComponentId(prefix, Sequencer)), resolveTimeout)
     locationF.futureValue(Timeout(resolveTimeout)).value.prefix shouldBe prefix
