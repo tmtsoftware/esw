62733bb1e1d79c29713cbcde7a76b5ba2f6d7880
mausamyede <mausam.yede1@gmail.com>
2020-10-07 17:53:15 +0530

    #ESW-000 : Mausam : Disable logging and restore test timeout

--- esw-integration-test/src/test/scala/esw/script/launcher/SequencerScriptLauncherTest.scala
+++ esw-integration-test/src/test/scala/esw/script/launcher/SequencerScriptLauncherTest.scala
@@ -27,7 +27,6 @@ class SequencerScriptLauncherTest extends EswTestKit {
   private val scriptLauncher   = (projectRootPath / "scripts" / "script-launcher" / "launchSequencer.sh").toString()
 
   var process: Process = _
-  LoggingSystemFactory.forTestingOnly()
   "launch sequencer script should compile given sequencer script and start sequencer with it| ESW-150" in {
     val builder = new ProcessBuilder(scriptLauncher, "-f", sampleScriptPath, "-v", ocsAppVersion).inheritIO()
 
@@ -42,7 +41,7 @@ class SequencerScriptLauncherTest extends EswTestKit {
 
     // check sequencer is registered in location service
     val prefix         = Prefix(ESW, className)
-    val resolveTimeout = 30.seconds // this timeout includes time taken by the process to start
+    val resolveTimeout = 20.seconds // this timeout includes time taken by the process to start
     val locationF =
       locationService.resolve(AkkaConnection(ComponentId(prefix, Sequencer)), resolveTimeout)
     locationF.futureValue(Timeout(resolveTimeout)).value.prefix shouldBe prefix
