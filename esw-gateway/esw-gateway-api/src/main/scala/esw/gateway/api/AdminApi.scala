53388c6f453f3f3940d2c13a975a321a97ad8309
Shubham Chauhan <shubham25may1998@gmail.com>
2020-12-31 10:26:31 +0530

    #ESW-378: Chauhan: Add api docs

--- esw-gateway/esw-gateway-api/src/main/scala/esw/gateway/api/AdminApi.scala
+++ esw-gateway/esw-gateway-api/src/main/scala/esw/gateway/api/AdminApi.scala
@@ -10,12 +10,52 @@ import scala.concurrent.Future
 
 trait AdminApi {
 
+  /**
+   * Shuts down the given component(HCD, Assembly or Container)
+   *
+   * @param componentId the [[csw.location.api.models.ComponentId]] of the component which needs to be shut down
+   * @return a Done as a Future value
+   */
   def shutdown(componentId: ComponentId): Future[Done]
+
+  /**
+   * Restarts the given component(HCD, Assembly or Container)
+   *
+   * @param componentId the [[csw.location.api.models.ComponentId]] of the component which needs to be restarted
+   * @return a Done as a Future value
+   */
   def restart(componentId: ComponentId): Future[Done]
+
+  /**
+   * Sets the current Lifecycle state of the given component(HCD, Assembly or Container) to Offline
+   *
+   * @param componentId the [[csw.location.api.models.ComponentId]] of the component which needs to be offline
+   * @return a Done as a Future value
+   */
   def goOffline(componentId: ComponentId): Future[Done]
+
+  /**
+   * Sets the current Lifecycle state of the given component(HCD, Assembly or Container) to Online
+   *
+   * @param componentId the [[csw.location.api.models.ComponentId]] of the component which needs to be online
+   * @return a Done as a Future value
+   */
   def goOnline(componentId: ComponentId): Future[Done]
 
+  /**
+   * Fetches the current Lifecycle state of the given container
+   *
+   * @param prefix the [[csw.prefix.models.Prefix]] of the container whose lifecycle state needs to be fetched
+   * @return a ContainerLifecycleState as a Future value
+   */
   def getContainerLifecycleState(prefix: Prefix): Future[ContainerLifecycleState]
+
+  /**
+   * Fetches the current Lifecycle state of the given Assembly or HCD
+   *
+   * @param componentId the [[csw.location.api.models.ComponentId]] of the component whose lifecycle state needs to be fetched
+   * @return a SupervisorLifecycleState as a Future value
+   */
   def getComponentLifecycleState(componentId: ComponentId): Future[SupervisorLifecycleState]
 
   /**
