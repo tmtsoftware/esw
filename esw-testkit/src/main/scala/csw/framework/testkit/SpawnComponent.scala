0051b8db709b66cf1189b4263514fb5279d43272
Vinay Mahamuni <vinaymahamuni22@gmail.com>
2021-01-17 21:06:32 +0530

    #ESW-130: Vinay: Refactor and rearrange the private method

--- esw-testkit/src/main/scala/csw/framework/testkit/SpawnComponent.scala
+++ esw-testkit/src/main/scala/csw/framework/testkit/SpawnComponent.scala
@@ -11,34 +11,12 @@ import csw.location.api.models.{ComponentType, Connection}
 import csw.prefix.models.Prefix
 
 import scala.async.Async.{async, await}
-import scala.concurrent.Future
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import scala.concurrent.{ExecutionContextExecutor, Future}
 
 trait SpawnComponent {
 
   val wiring: FrameworkWiring
-
-  private def spawnComponent(
-      prefix: Prefix,
-      componentType: ComponentType,
-      behaviorFactory: ComponentBehaviorFactory,
-      locationServiceUsage: LocationServiceUsage,
-      connections: Set[Connection],
-      initializeTimeout: FiniteDuration
-  ): Future[ActorRef[ComponentMessage]] = {
-    import wiring._
-    import actorRuntime._
-
-    val componentInfo =
-      ComponentInfo(prefix, componentType, "", locationServiceUsage, connections, initializeTimeout)
-
-    val richSystem = new CswFrameworkSystem(actorRuntime.actorSystem)
-    async {
-      val cswCtxF            = CswContext.make(locationService, eventServiceFactory, alarmServiceFactory, componentInfo)(richSystem)
-      val supervisorBehavior = SupervisorBehaviorFactory.make(None, registrationFactory, behaviorFactory, await(cswCtxF))
-      await(richSystem.spawnTyped(supervisorBehavior, componentInfo.prefix.toString))
-    }
-  }
 
   def spawnHCD(
       prefix: Prefix,
@@ -57,5 +35,27 @@ trait SpawnComponent {
       initializeTimeout: FiniteDuration = 10.seconds
   ): Future[ActorRef[ComponentMessage]] =
     spawnComponent(prefix, ComponentType.Assembly, behaviorFactory, locationServiceUsage, connections, initializeTimeout)
+
+  private def spawnComponent(
+      prefix: Prefix,
+      componentType: ComponentType,
+      behaviorFactory: ComponentBehaviorFactory,
+      locationServiceUsage: LocationServiceUsage,
+      connections: Set[Connection],
+      initializeTimeout: FiniteDuration
+  ): Future[ActorRef[ComponentMessage]] = {
+    implicit val ec: ExecutionContextExecutor   = wiring.actorRuntime.ec
+    implicit val richSystem: CswFrameworkSystem = new CswFrameworkSystem(wiring.actorRuntime.actorSystem)
+    val componentInfo =
+      ComponentInfo(prefix, componentType, "", locationServiceUsage, connections, initializeTimeout)
+
+    async {
+      val cswCtx =
+        await(CswContext.make(wiring.locationService, wiring.eventServiceFactory, wiring.alarmServiceFactory, componentInfo))
+      val supervisorBehavior = SupervisorBehaviorFactory.make(None, wiring.registrationFactory, behaviorFactory, cswCtx)
+
+      await(richSystem.spawnTyped(supervisorBehavior, componentInfo.prefix.toString))
+    }
+  }
 
 }
