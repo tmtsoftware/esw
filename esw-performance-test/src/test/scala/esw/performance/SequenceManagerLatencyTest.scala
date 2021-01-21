248905b1abc2d408c26bc86f7f8ac26bd1b9d0d7
Shivani Thorat <shivanimt31@gmail.com>
2021-01-21 16:49:21 +0530

    #ESW-175: Shivani |  Poorva: Add warm up before starting actual latency test

--- esw-performance-test/src/test/scala/esw/performance/SequenceManagerLatencyTest.scala
+++ esw-performance-test/src/test/scala/esw/performance/SequenceManagerLatencyTest.scala
@@ -3,6 +3,7 @@ package esw.performance
 import java.io.{File, FileOutputStream, PrintStream}
 
 import akka.actor.typed.{ActorSystem, SpawnProtocol}
+import csw.location.api.models.AkkaLocation
 import csw.location.api.models.ComponentType.Service
 import csw.location.api.scaladsl.LocationService
 import csw.location.client.ActorSystemFactory
@@ -29,15 +30,17 @@ class SequenceManagerLatencyTest extends LocationUtils {
   override implicit def patienceConfig: PatienceConfig                  = PatienceConfig(120.seconds, 50.millis)
 
   LocationServerStatus.requireUpLocally()
-  val loggerFactory = new LoggerFactory(Prefix(ESW, "perf.test"))
-  val log: Logger   = loggerFactory.getLogger
-  val histogram     = new Histogram(3)
+  val loggerFactory                 = new LoggerFactory(Prefix(ESW, "perf.test"))
+  val sequenceManagerPrefix: Prefix = Prefix(ESW, "sequence_manager")
+  val smLocation: AkkaLocation      = resolveAkkaLocation(sequenceManagerPrefix, Service)
+  val smClient: SequenceManagerApi  = SequenceManagerApiFactory.makeAkkaClient(smLocation)
 
+  val log: Logger = loggerFactory.getLogger
   "Sequence Manager" must {
     "configure observing mode with permissible latency | ESW-175" in {
-      val sequenceManagerPrefix = Prefix(ESW, "sequence_manager")
-      val smLocation            = resolveAkkaLocation(sequenceManagerPrefix, Service)
-      val smClient              = SequenceManagerApiFactory.makeAkkaClient(smLocation)
+
+      val histogram       = new Histogram(3)
+      val warmUpHistogram = new Histogram(3)
 
       val provisionConfig = ProvisionConfig(
         List(
@@ -50,84 +53,85 @@ class SequenceManagerLatencyTest extends LocationUtils {
       )
 
       val provisionResponse = smClient.provision(provisionConfig).futureValue
-      println(s"---------> ${provisionResponse}")
       provisionResponse shouldBe a[ProvisionResponse.Success.type]
+
+      val warmUpDeadline = 8.seconds.fromNow
+      while (warmUpDeadline.hasTimeLeft()) {
+        scenario(warmUpHistogram)
+      }
+
+      println("Warmup Latencies------>")
+      printResults(warmUpHistogram)
 
       val deadline = 20.seconds.fromNow
       while (deadline.hasTimeLeft()) {
-        scenario(smClient)
+        scenario(histogram)
       }
 
-      recordResults()
+      recordResults(histogram)
+      println("Actual Latencies------>")
+      printResults(histogram)
 
       actorSystem.terminate()
     }
   }
 
-  private def recordResults(): Unit = {
+  private def printResults(histogram: Histogram): Unit = {
+    println("50 %tile: " + histogram.getValueAtPercentile(50))
+    println("90 %tile: " + histogram.getValueAtPercentile(90))
+    println("99 %tile: " + histogram.getValueAtPercentile(99))
+  }
+
+  private def recordResults(histogram: Histogram): Unit = {
     try {
-      val resultsFile = new File("~/results.txt")
-      val fout        = new FileOutputStream(resultsFile)
+      val resultsFile = new File("results.txt")
+      resultsFile.createNewFile()
+      println(resultsFile.getAbsolutePath)
+      val fout = new FileOutputStream(resultsFile)
       try histogram.outputPercentileDistribution(new PrintStream(fout), 1.0)
       finally if (fout != null) fout.close()
     }
     catch {
-      case e: Exception => log.error("Writing histogram results failed with error", ex = e)
+      case e: Exception =>
+        println("Writing histogram results failed with error " + e.getMessage)
+        log.error("Writing histogram results failed with error", ex = e)
+
     }
   }
 
-  private def scenario(smClient: SequenceManagerApi): Unit = {
+  private def scenario(histogram: Histogram): Unit = {
     val obsmode1 = ObsMode("obsMode1")
     val obsmode2 = ObsMode("obsMode2")
     val obsmode3 = ObsMode("obsMode3")
 
     //-------------------- Configure Obsmode1 -------------------------------
     val configureResponse1 = smClient.configure(obsmode1).futureValue
-    log.info(s"Configured ObsMode ${obsmode1} response ${configureResponse1}")
-    println(s"Configured ObsMode ${obsmode1} response ${configureResponse1}")
+    log.info(s"Configured ObsMode $obsmode1 response $configureResponse1")
+    println(s"Configured ObsMode $obsmode1 response $configureResponse1")
 
-    // To simulate observation
-    Thread.sleep(500) //todo: async delay
+    switchObsMode(obsmode1, obsmode2, histogram)
 
-    val time0 = System.currentTimeMillis()
-    smClient.shutdownObsModeSequencers(obsmode1).futureValue
-    val configureResponse2 = smClient.configure(obsmode2).futureValue
-    val time1              = System.currentTimeMillis()
-    val diff0              = time1 - time0
-    histogram.recordValue(diff0)
-    log.info(s"Configured ObsMode ${obsmode2} response ${configureResponse2}")
-    println(s"Configured ObsMode ${obsmode2} response ${configureResponse2}")
-    println("Shutdown + configure time: " + diff0)
+    switchObsMode(obsmode2, obsmode3, histogram)
 
-    // To simulate observation
-    Thread.sleep(500) //todo: async delay
-
-    val time2 = System.currentTimeMillis()
-    smClient.shutdownObsModeSequencers(obsmode2).futureValue
-    val configureResponse3 = smClient.configure(obsmode3).futureValue
-    val time3              = System.currentTimeMillis()
-    val diff1              = time3 - time2
-    histogram.recordValue(diff1)
-    log.info(s"Configured ObsMode ${obsmode3} response ${configureResponse3}")
-    println(s"Configured ObsMode ${obsmode3} response ${configureResponse3}")
-    println("Shutdown + configure time: " + diff1)
-
-    Thread.sleep(500) //todo: async delay
-
-    val time4 = System.currentTimeMillis()
-    smClient.shutdownObsModeSequencers(obsmode3).futureValue
-    val configureResponse4 = smClient.configure(obsmode1).futureValue
-    val time5              = System.currentTimeMillis()
-    val diff2              = time5 - time4
-    histogram.recordValue(diff2)
-    log.info(s"Configured ObsMode ${obsmode1} response ${configureResponse4}")
-    println(s"Configured ObsMode ${obsmode1} response ${configureResponse4}")
-    println("Shutdown + configure time: " + diff2)
-
-    // To simulate observation
-    Thread.sleep(500) //todo: async delay
+    switchObsMode(obsmode3, obsmode1, histogram)
 
     smClient.shutdownObsModeSequencers(obsmode1).futureValue
 
+  }
+
+  private def switchObsMode(prevObsMode: ObsMode, nextObsMode: ObsMode, histogram: Histogram): Unit = {
+    val beforeSwitch = System.currentTimeMillis()
+    smClient.shutdownObsModeSequencers(prevObsMode).futureValue
+    val configureResponse = smClient.configure(nextObsMode).futureValue
+    val afterSwitch       = System.currentTimeMillis()
+
+    val latency = afterSwitch - beforeSwitch
+    histogram.recordValue(latency)
+    log.info(s"Configured ObsMode $nextObsMode response $configureResponse")
+    println(s"Configured ObsMode $nextObsMode response $configureResponse")
+    println("Shutdown + configure time: " + latency)
+
+    // To simulate observation
+    Thread.sleep(500) //todo: async delay
   }
 }
