1f844acb521206cfe2f8dd04123853f748d9917f
Poorva Gokhale <poorvasgokhale@gmail.com>
2021-01-25 14:32:52 +0530

    #000: Poorva: Add configurable constants in perf test

---
+++ esw-performance-test/src/test/scala/esw/performance/Constants.scala
@@ -0,0 +1,27 @@
+package esw.performance
+
+import csw.prefix.models.Prefix
+import esw.ocs.api.models.ObsMode
+import esw.sm.api.models.{AgentProvisionConfig, ProvisionConfig}
+
+object Constants {
+
+  val obsmode1: ObsMode = ObsMode("obsMode1")
+  val obsmode2: ObsMode = ObsMode("obsMode2")
+  val obsmode3: ObsMode = ObsMode("obsMode3")
+
+  val enableSwitching = false
+
+  val warmupIterations = 5
+  val actualIterations = 10
+
+  val provisionConfig: ProvisionConfig = ProvisionConfig(
+    List(
+      AgentProvisionConfig(Prefix("ESW.machine1"), 1),
+      AgentProvisionConfig(Prefix("IRIS.machine1"), 1),
+      AgentProvisionConfig(Prefix("TCS.machine1"), 1),
+      AgentProvisionConfig(Prefix("AOESW.machine1"), 1),
+      AgentProvisionConfig(Prefix("WFOS.machine1"), 1)
+    )
+  )
+}
--- esw-performance-test/src/test/scala/esw/performance/SequenceManagerLatencyTest.scala
+++ esw-performance-test/src/test/scala/esw/performance/SequenceManagerLatencyTest.scala
@@ -15,9 +15,9 @@ import csw.prefix.models.Prefix
 import csw.prefix.models.Subsystem.ESW
 import esw.ocs.api.models.ObsMode
 import esw.ocs.testkit.utils.LocationUtils
+import esw.performance.Constants._
 import esw.sm.api.SequenceManagerApi
 import esw.sm.api.actor.client.SequenceManagerApiFactory
-import esw.sm.api.models.{AgentProvisionConfig, ProvisionConfig}
 import esw.sm.api.protocol.ProvisionResponse
 import org.HdrHistogram.Histogram
 
@@ -42,29 +42,19 @@ class SequenceManagerLatencyTest extends LocationUtils {
       val histogram       = new Histogram(3)
       val warmUpHistogram = new Histogram(3)
 
-      val provisionConfig = ProvisionConfig(
-        List(
-          AgentProvisionConfig(Prefix("ESW.machine1"), 1),
-          AgentProvisionConfig(Prefix("IRIS.machine1"), 1),
-          AgentProvisionConfig(Prefix("TCS.machine1"), 1),
-          AgentProvisionConfig(Prefix("AOESW.machine1"), 1),
-          AgentProvisionConfig(Prefix("WFOS.machine1"), 1)
-        )
-      )
-
       val provisionResponse = smClient.provision(provisionConfig).futureValue
       provisionResponse shouldBe a[ProvisionResponse.Success.type]
 
-      val warmUpDeadline = 8.seconds.fromNow
-      while (warmUpDeadline.hasTimeLeft()) {
+      (1 to warmupIterations).foreach { iterationNumber =>
+        println(s"Warmup iteration ------> $iterationNumber")
         scenario(warmUpHistogram)
       }
 
       println("Warmup Latencies------>")
       printResults(warmUpHistogram)
 
-      val deadline = 20.seconds.fromNow
-      while (deadline.hasTimeLeft()) {
+      (1 to actualIterations).foreach { iterationNumber =>
+        println(s"Actual iteration ------> $iterationNumber")
         scenario(histogram)
       }
 
@@ -100,20 +90,18 @@ class SequenceManagerLatencyTest extends LocationUtils {
   }
 
   private def scenario(histogram: Histogram): Unit = {
-    val obsmode1 = ObsMode("obsMode1")
-    val obsmode2 = ObsMode("obsMode2")
-    val obsmode3 = ObsMode("obsMode3")
-
     //-------------------- Configure Obsmode1 -------------------------------
     val configureResponse1 = smClient.configure(obsmode1).futureValue
     log.info(s"Configured ObsMode $obsmode1 response $configureResponse1")
     println(s"Configured ObsMode $obsmode1 response $configureResponse1")
 
-    switchObsMode(obsmode1, obsmode2, histogram)
+    Thread.sleep(60 * 1000)
 
-    switchObsMode(obsmode2, obsmode3, histogram)
-
-    switchObsMode(obsmode3, obsmode1, histogram)
+    if (enableSwitching) {
+      switchObsMode(obsmode1, obsmode2, histogram)
+      switchObsMode(obsmode2, obsmode3, histogram)
+      switchObsMode(obsmode3, obsmode1, histogram)
+    }
 
     smClient.shutdownObsModeSequencers(obsmode1).futureValue
 
@@ -132,6 +120,6 @@ class SequenceManagerLatencyTest extends LocationUtils {
     println("Shutdown + configure time: " + latency)
 
     // To simulate observation
-    Thread.sleep(500) //todo: async delay
+    Thread.sleep(60 * 1000) //todo: async delay
   }
 }
