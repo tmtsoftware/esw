fc5e9097d194ea493b4a2275bec6dae9fc078fa4
Poorva Gokhale <poorvasgokhale@gmail.com>
2021-02-15 14:31:40 +0530

    #ESW-421: Poorva: Add tests for ObsId and ExposureId kotlin glue

---
+++ esw-ocs/esw-ocs-dsl-kt/src/test/kotlin/esw/ocs/dsl/highlevel/models/ExposureIdTest.kt
@@ -0,0 +1,20 @@
+package esw.ocs.dsl.highlevel.models
+
+import csw.params.core.models.ExposureId
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class ExposureIdTest {
+    @Test
+    fun `verify ExposureId kotlin glue | ESW-421`() {
+        val exposureId = ExposureId("2021A-011-153-TCS-DET-SCI0-0001")
+        val expectedExposureId = ExposureId.apply("2021A-011-153-TCS-DET-SCI0-0001")
+
+        exposureId shouldBe expectedExposureId
+        exposureId.obsId shouldBe expectedExposureId.obsId()
+        exposureId.det shouldBe expectedExposureId.det()
+        exposureId.typLevel shouldBe expectedExposureId.typLevel()
+        exposureId.subsystem shouldBe expectedExposureId.subsystem()
+        exposureId.exposureNumber shouldBe expectedExposureId.exposureNumber()
+    }
+}
---
+++ esw-ocs/esw-ocs-dsl-kt/src/test/kotlin/esw/ocs/dsl/highlevel/models/ObsIdTest.kt
@@ -0,0 +1,17 @@
+package esw.ocs.dsl.highlevel.models
+
+import csw.params.core.models.ObsId
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class ObsIdTest {
+    @Test
+    fun `verify ObsId kotlin glue | ESW-421`() {
+        val obsId = ObsId("2020A-001-123")
+        val expectedObsId = ObsId.apply("2020A-001-123")
+
+        obsId shouldBe expectedObsId
+        obsId.programId shouldBe expectedObsId.programId()
+        obsId.observationNumber shouldBe expectedObsId.observationNumber()
+    }
+}
