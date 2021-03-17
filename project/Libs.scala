253aed481cfd1fcc4183674d6fcf2c36ab7aef4b
Poorva Gokhale <poorvasgokhale@gmail.com>
2021-03-17 15:01:05 +0530

    #ESW-481: Poorva: Update csw version

--- project/Libs.scala
+++ project/Libs.scala
@@ -29,7 +29,7 @@ object Libs {
 object Csw {
   private val Org = "com.github.tmtsoftware.csw"
 
-  private val Version = "4deda1783d"
+  private val Version = "efe5d22c4"
 
   val `csw-aas-http`        = Org %% "csw-aas-http"        % Version
   val `csw-alarm-api`       = Org %% "csw-alarm-api"       % Version
