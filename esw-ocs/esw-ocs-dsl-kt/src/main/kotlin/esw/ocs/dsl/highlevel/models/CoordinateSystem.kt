1ea12a9c507ae9fdc3c5ff7221223f89d7b578e1
manish <gowardipemanish95@gmail.com>
2022-04-01 11:27:27 +0530

    #000: Manish | Himanshu : Add CoordinateSystem to kt dsl

---
+++ esw-ocs/esw-ocs-dsl-kt/src/main/kotlin/esw/ocs/dsl/highlevel/models/CoordinateSystem.kt
@@ -0,0 +1,8 @@
+package esw.ocs.dsl.highlevel.models
+
+import csw.params.events.CoordinateSystem
+import csw.params.javadsl.JCoordinateSystem
+
+val RADEC: CoordinateSystem = JCoordinateSystem.RADEC
+val XY: CoordinateSystem = JCoordinateSystem.XY
+val ALTAZ: CoordinateSystem = JCoordinateSystem.ALTAZ
