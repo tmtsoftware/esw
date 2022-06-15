b7fa5f2d7ef3736f56c59bc890c0f59c8b43188d
Allan Brighton <Allan.Brighton@t-online.de>
2022-06-15 15:28:40 +0200

    In progress test with kotlin-1.7.0

--- esw-services/src/main/scala/esw/services/EswServicesApp.scala
+++ esw-services/src/main/scala/esw/services/EswServicesApp.scala
@@ -14,7 +14,7 @@ object EswServicesApp extends EswCommandApp[Command] {
   override def appName: String    = getClass.getSimpleName.dropRight(1)
   override def appVersion: String = BuildInfo.version
   override def progName: String   = BuildInfo.name
-  println(s"starting $progName-$appVersion")
+  println(s"starting $progName-$appVersion (XXX 1)")
 
   val hostname: String = Networks().hostname
 
