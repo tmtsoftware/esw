77b933c5918f4cd44104517b89594bd70bb75389
Allan Brighton <Allan.Brighton@t-online.de>
2023-11-06 10:20:41 +0100

    Set kotlin version to 1.9.20, use jvm target 17

--- project/EswKeys.scala
+++ project/EswKeys.scala
@@ -2,7 +2,7 @@ object EswKeys {
   val homepageValue: String = "https://github.com/tmtsoftware/esw"
 //  val scalaVersion: String  = "3.3.1-RC5"
   val scalaVersion: String  = "3.3.0"
-  val kotlinVersion: String = "1.9.0"
+  val kotlinVersion: String = "1.9.20"
   val projectName           = "esw"
   val projectVersion        = "0.1.0-SNAPSHOT"
 }
