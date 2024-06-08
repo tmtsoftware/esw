c8d646dbd712e2e24ed0a9980622ac491dbf9802
Allan Brighton <Allan.Brighton@t-online.de>
2024-06-08 22:37:53 +0200

    Set scala version to 3.3.3 due to esw-shell ammonite dependency

--- project/EswKeys.scala
+++ project/EswKeys.scala
@@ -1,6 +1,7 @@
 object EswKeys {
   val homepageValue: String = "https://github.com/tmtsoftware/esw"
-  val scalaVersion: String  = "3.4.2"
+  //  val scalaVersion: String  = "3.4.2"
+  val scalaVersion: String  = "3.3.3" // Needs to match ammonite scala version
   val kotlinVersion: String = "1.9.22"
   val projectName           = "esw"
   val projectVersion        = "0.1.0-SNAPSHOT"
