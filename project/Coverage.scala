c12efe18756f93b7682a7afdfe8ecdfd06067dc9
Allan Brighton <Allan.Brighton@t-online.de>
2025-03-13 20:27:10 +0100

    Updated Jenkins file

--- project/Coverage.scala
+++ project/Coverage.scala
@@ -8,7 +8,9 @@ object Coverage extends AutoPlugin {
 
   override def projectSettings: Seq[Setting[_]] =
     Seq(
-      coverageEnabled          := false, // XXX TODO FIXME
+      // XXX TODO FIXME: Coverage broken in Scala3? See https://contributors.scala-lang.org/t/coverage-broken-why-does-nobody-care/6262/44
+      // Coverage results for Scala3 show much lower values than for Scala2.
+      coverageEnabled          := false,
       coverageMinimumStmtTotal := 80,
       coverageFailOnMinimum    := true,
       coverageHighlighting     := true,
