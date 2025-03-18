7cf5aca08735dd0503e68c9f8d99717ea6af62b6
Allan Brighton <Allan.Brighton@t-online.de>
2025-03-18 11:54:47 +0100

    Testing Jenkins file

--- scripts/install_kotlin.sh
+++ scripts/install_kotlin.sh
@@ -2,6 +2,6 @@
 
 set -x
 curl -s https://get.sdkman.io | bash
-source "/home/runner/.sdkman/bin/sdkman-init.sh"
+source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install kotlin 2.1.0
 sudo ln -s $(which kotlinc) /usr/local/bin/kotlinc
