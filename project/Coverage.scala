8bf45c64e2e28994fcfc0625984828a59c5707b8
Allan Brighton <Allan.Brighton@t-online.de>
2025-05-15 10:20:48 +0200

    re-enabled coverage

--- project/Coverage.scala
+++ project/Coverage.scala
@@ -10,9 +10,10 @@ object Coverage extends AutoPlugin {
     Seq(
       // XXX TODO FIXME: Coverage broken in Scala3? See https://contributors.scala-lang.org/t/coverage-broken-why-does-nobody-care/6262/44
       // Coverage results for Scala3 show much lower values than for Scala2.
-      coverageEnabled          := false,
+      coverageEnabled          := true,
       coverageMinimumStmtTotal := 80,
-      coverageFailOnMinimum    := true,
+//      coverageFailOnMinimum    := true,
+      coverageFailOnMinimum    := false,
       coverageHighlighting     := true,
       coverageOutputCobertura  := true,
       coverageOutputXML        := true
