e4281611d6f27000c71dd54cf5345882e401d78e
Allan Brighton <Allan.Brighton@t-online.de>
2025-05-22 21:48:02 +0200

    Updated and removed "ignore" keyword.

--- esw-integration-test/src/test/scala/esw/shell/MainTest.scala
+++ esw-integration-test/src/test/scala/esw/shell/MainTest.scala
@@ -6,19 +6,20 @@ import esw.testcommons.BaseTestSuite
 
 class MainTest extends BaseTestSuite {
 
-  "verify esw-shell compiles and starts successfully" ignore {
-    val channel        = "https://raw.githubusercontent.com/tmtsoftware/osw-apps/branch-6.0.x/apps.json"
-    val version        = "0.1.0-SNAPSHOT"
-    val commands       = List("cs", "launch", "--channel", channel, s"esw-shell:$version")
-    val processBuilder = new ProcessBuilder(commands*)
-    val process        = processBuilder.start()
-    Thread.sleep(10000)
-    assert(process.isAlive, "esw-shell failed to start!")
+  "esw-shell" must {
+    "verify, compile and start successfully" in {
+      val channel        = "https://raw.githubusercontent.com/tmtsoftware/osw-apps/branch-6.0.x/apps.json"
+      val version        = "0.1.0-SNAPSHOT"
+      val commands       = List("cs", "launch", "--channel", channel, s"esw-shell:$version")
+      val processBuilder = new ProcessBuilder(commands*)
+      val process        = processBuilder.start()
+      Thread.sleep(10000)
+      assert(process.isAlive, "esw-shell failed to start!")
 
-    process.descendants().map(_.destroyForcibly())
-    process.destroyForcibly().waitFor(30, TimeUnit.SECONDS)
+      process.descendants().map(_.destroyForcibly())
+      process.destroyForcibly().waitFor(30, TimeUnit.SECONDS)
 
-    assert(!process.isAlive, "esw-shell process did not terminate within 10 seconds!")
+      assert(!process.isAlive, "esw-shell process did not terminate within 10 seconds!")
+    }
   }
-
 }
